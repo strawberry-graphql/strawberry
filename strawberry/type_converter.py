39f665a581a6e2916f0d5fcde8cfd9f29f83d753
Patrick Arminio <patrick.arminio@gmail.com>
2019-02-26 16:40:40 +0000

    Add support for lists

--- strawberry/type_converter.py
+++ strawberry/type_converter.py
@@ -4,6 +4,7 @@ from graphql import (
     GraphQLInt,
     GraphQLNonNull,
     GraphQLString,
+    GraphQLList,
     GraphQLUnionType,
 )
 
@@ -23,34 +24,46 @@ def get_graphql_type_for_annotation(
 ):
     # TODO: nice error
 
-    # TODO: this might lead to issues with types that have a
-    # field value
-    if hasattr(annotation, "field"):
-        return annotation.field
-
     is_optional = False
 
-    # checking for optional and union types
-    if hasattr(annotation, "__args__"):
-        types = annotation.__args__
-        non_none_types = [x for x in types if x != type(None)]
-
-        # optionals are represented as Union[type, None]
-        if len(non_none_types) == 1:
-            is_optional = True
-            graphql_type = get_graphql_type_for_annotation(
-                non_none_types[0], field_name, force_optional=True
-            )
-        else:
-            is_optional = type(None) in types
-
-            # TODO: union types don't work with scalar types
-            # so we want to return a nice error
-            # also we want to make sure we have been passed
-            # strawberry types
-            graphql_type = GraphQLUnionType(field_name, [type.field for type in types])
+    # TODO: this might lead to issues with types that have a field value
+    if hasattr(annotation, "field"):
+        graphql_type = annotation.field
     else:
-        graphql_type = TYPE_MAP.get(annotation)
+        annotation_name = getattr(annotation, "_name", None)
+
+        if annotation_name == "List":
+            list_of_type = get_graphql_type_for_annotation(
+                annotation.__args__[0], field_name
+            )
+
+            return GraphQLList(list_of_type)
+
+        # for some reason _name is None for Optional and Union types, so we check if we
+        # have __args__ populated, there might be some edge cases where __args__ is
+        # populated but the type is not an Union, like in the above case with Lists
+        if hasattr(annotation, "__args__"):
+            types = annotation.__args__
+            non_none_types = [x for x in types if x != type(None)]
+
+            # optionals are represented as Union[type, None]
+            if len(non_none_types) == 1:
+                is_optional = True
+                graphql_type = get_graphql_type_for_annotation(
+                    non_none_types[0], field_name, force_optional=True
+                )
+            else:
+                is_optional = type(None) in types
+
+                # TODO: union types don't work with scalar types
+                # so we want to return a nice error
+                # also we want to make sure we have been passed
+                # strawberry types
+                graphql_type = GraphQLUnionType(
+                    field_name, [type.field for type in types]
+                )
+        else:
+            graphql_type = TYPE_MAP.get(annotation)
 
     if not graphql_type:
         raise ValueError(f"Unable to get GraphQL type for {annotation}")
--- tests/test_type_converter.py
+++ tests/test_type_converter.py
@@ -1,6 +1,6 @@
-from typing import Optional, Union
+from typing import Optional, Union, List
 
-from graphql import GraphQLNonNull, GraphQLScalarType, GraphQLObjectType
+from graphql import GraphQLNonNull, GraphQLScalarType, GraphQLObjectType, GraphQLList
 
 import strawberry
 from strawberry.type_converter import get_graphql_type_for_annotation
@@ -41,3 +41,37 @@ def test_optional_object_type():
 
     assert type(field) == GraphQLObjectType
     assert field.name == "A"
+
+
+def test_list_of_scalar():
+    field = get_graphql_type_for_annotation(List[str], "Example")
+
+    assert type(field) == GraphQLList
+    assert type(field.of_type) == GraphQLNonNull
+    assert type(field.of_type.of_type) == GraphQLScalarType
+    assert field.of_type.of_type.name == "String"
+
+    field = get_graphql_type_for_annotation(List[Optional[str]], "Example")
+
+    assert type(field) == GraphQLList
+    assert type(field.of_type) == GraphQLScalarType
+    assert field.of_type.name == "String"
+
+
+def test_list_of_object_types():
+    @strawberry.type
+    class A:
+        x: int
+
+    field = get_graphql_type_for_annotation(List[A], "Example")
+
+    assert type(field) == GraphQLList
+    assert type(field.of_type) == GraphQLNonNull
+    assert type(field.of_type.of_type) == GraphQLObjectType
+    assert field.of_type.of_type.name == "A"
+
+    field = get_graphql_type_for_annotation(List[Optional[A]], "Example")
+
+    assert type(field) == GraphQLList
+    assert type(field.of_type) == GraphQLObjectType
+    assert field.of_type.name == "A"
