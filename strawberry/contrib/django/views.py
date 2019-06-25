fc12dc1214f55c05f32fe857db7d176744f0ce97
Jayden Windle <jaydenwindle@gmail.com>
2019-06-24 17:35:08 -0700

    started on django view

--- strawberry/contrib/django/views.py
+++ strawberry/contrib/django/views.py
@@ -1,5 +1,11 @@
+# from django.http import HttpResponse, HttpResponseNotAllowed
+# from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import View
+
+from graphql.type.schema import GraphQLSchema
 
 
 class GraphQLPlaygroundView(View):
@@ -9,3 +15,17 @@ class GraphQLPlaygroundView(View):
             "graphql/playground.html",
             {"REQUEST_PATH": request.get_full_path()},
         )
+
+
+class GraphQLView(View):
+    def __init__(self, schema=None):
+        assert schema, "You must pass in a schema to GraphQLView"
+        assert isinstance(
+            schema, GraphQLSchema
+        ), "You must pass in a valid schema to GraphQLView"
+
+        self.schema = schema
+
+    @method_decorator(ensure_csrf_cookie)
+    def dispatch(self, request, *args, **kwargs):
+        pass
