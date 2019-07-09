2556d0376bb03c78bcfb8c943c330be1cd2d56e6
Johnbosco Ohia <johnboscoohia@gmail.com>
2019-07-09 18:07:25 +0100

    format permission.py

--- strawberry/permission.py
+++ strawberry/permission.py
@@ -1,7 +1,8 @@
 class BasePermission(object):
     """
-    A base permission class 
+    Base class for creating permissions
     """
+
     def has_permission(self, info):
         raise NotImplementedError(
             "Permission classes should override has_permission method"
