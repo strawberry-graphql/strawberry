1becc41f48c5598adcc868815ad7377ba22d2e1a
Johnbosco Ohia <johnboscoohia@gmail.com>
2019-07-10 16:45:52 +0100

    fix code styles

--- tests/test_permission.py
+++ tests/test_permission.py
@@ -1,29 +1,27 @@
-import pytest
-
-from graphql import graphql, graphql_sync
-
 import strawberry
+from graphql import graphql_sync
 from strawberry.permission import BasePermission
 
 
-def test_raises_graphql_error_when_has_permission_is_not_defined():
-
+def test_raises_graphql_error_when_permission_method_is_missing():
     class IsAuthenticated(BasePermission):
         pass
 
     @strawberry.type
     class Query:
-
         @strawberry.field(permission_classes=[IsAuthenticated])
         def user(self, info) -> str:
-            return 'patrick'
+            return "patrick"
 
     schema = strawberry.Schema(query=Query)
 
     query = "{ user }"
 
     result = graphql_sync(schema, query)
-    assert result.errors[0].message == "Permission classes should override has_permission method"
+    assert (
+        result.errors[0].message
+        == "Permission classes should override has_permission method"
+    )
 
 
 def test_raises_graphql_error_when_permission_is_denied():
@@ -35,10 +33,9 @@ def test_raises_graphql_error_when_permission_is_denied():
 
     @strawberry.type
     class Query:
-
         @strawberry.field(permission_classes=[IsAuthenticated])
         def user(self, info) -> str:
-            return 'patrick'
+            return "patrick"
 
     schema = strawberry.Schema(query=Query)
 
