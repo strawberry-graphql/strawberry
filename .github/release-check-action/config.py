0fd755f2ff8d6580af482aafe719eb609a168dbf
Patrick Arminio <patrick.arminio@gmail.com>
2019-09-05 20:21:29 +0100

    Hard code API_URL
    GitHub actions don't pass any secret in forked PRs

--- .github/release-check-action/config.py
+++ .github/release-check-action/config.py
@@ -5,4 +5,4 @@ RELEASE_FILE_PATH = "RELEASE.md"
 GITHUB_SHA = os.environ["GITHUB_SHA"]
 GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
 GITHUB_WORKSPACE = os.environ["GITHUB_WORKSPACE"]
-API_URL = os.environ["API_URL"]
+API_URL = "https://gentle-beach-72400.herokuapp.com/graphql"
