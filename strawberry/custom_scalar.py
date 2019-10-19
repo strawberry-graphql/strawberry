5588ca8411e4b63b389f615ab80710d9dc00fa5d
Jan Gosmann <jan@hyper-world.de>
2019-10-19 17:21:45 +0200

    Allow parse_literal function for custom scalars
    Addresses #114.
    
    It is unclear to me in what case this function would be called
    and how a test for it could be written.

--- strawberry/custom_scalar.py
+++ strawberry/custom_scalar.py
@@ -5,21 +5,29 @@ from graphql.type.scalars import GraphQLScalarType
 from .type_converter import REGISTRY
 
 
-def _process_scalar(cls, *, serialize, parse_value):
+def _process_scalar(cls, *, serialize, parse_value, parse_literal):
     name = cls.__name__
 
     custom_scalar_type = NewType(name, cls)
 
     REGISTRY[custom_scalar_type] = GraphQLScalarType(
-        name=name, serialize=serialize, parse_value=parse_value
+        name=name,
+        serialize=serialize,
+        parse_value=parse_value,
+        parse_literal=parse_literal,
     )
 
     return custom_scalar_type
 
 
-def scalar(cls=None, *, serialize, parse_value):
+def scalar(cls=None, *, serialize, parse_value, parse_literal=None):
     def wrap(cls):
-        return _process_scalar(cls, serialize=serialize, parse_value=parse_value)
+        return _process_scalar(
+            cls,
+            serialize=serialize,
+            parse_value=parse_value,
+            parse_literal=parse_literal,
+        )
 
     if cls is None:
         return wrap
