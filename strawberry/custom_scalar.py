efd51c919f3477c85071359bad8be58b8b1c19ee
Jan Gosmann <jan@hyper-world.de>
2019-10-19 18:18:25 +0200

    Add documentation for custom scalars
    Addresses #114.

--- strawberry/custom_scalar.py
+++ strawberry/custom_scalar.py
@@ -18,6 +18,29 @@ def _process_scalar(cls, *, description, serialize, parse_value, parse_literal):
 
 
 def scalar(cls=None, *, description=None, serialize, parse_value, parse_literal=None):
+    """Annotates a class or type as a GraphQL custom scalar.
+
+    Example usages:
+
+    >>> strawberry.scalar(
+    >>>     datetime.date,
+    >>>     serialize=lambda value: value.isoformat(),
+    >>>     parse_value=datetime.parse_date)
+
+    >>> Base64Encoded = strawberry.scalar(
+    >>>     NewType("Base64Encoded", bytes),
+    >>>     serialize=base64.b64encode,
+    >>>     parse_value=base64.b64decode)
+
+    >>> @strawberry.scalar(
+    >>>     serialize=lambda value: ",".join(value.items),
+    >>>     parse_value=lambda value: CustomList(value.split(",")))
+    >>> class CustomList:
+    >>>     def __init__(self, items):
+    >>>         self.items = items
+
+    """
+
     def wrap(cls):
         return _process_scalar(
             cls,
