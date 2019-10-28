218f0a441ab726f622a9b7743db1f648722b7e4f
Tiziano Müller <tm@dev-zero.ch>
2019-10-28 14:18:05 +0100

    type: add is_type_of for GraphlQLObjectType (#193)
    * type: add is_type_of for GraphlQLObjectType
    
    fixes #190
    
    * schema: also forward types to GraphQLSchema
    
    * add RELEASE.md
    
    * tests: add new test for is_type_of and manual type registry
    
    * schema: add minimal amount of API documentation

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+Release type: patch
+
+Minimal support for registering types without fields and abstract interface querying.
--- strawberry/schema.py
+++ strawberry/schema.py
@@ -7,13 +7,28 @@ from graphql.utilities.schema_printer import print_schema
 
 
 class Schema(GraphQLSchema):
-    def __init__(self, query, mutation=None, subscription=None, directives=()):
+    def __init__(
+        self, query, mutation=None, subscription=None, directives=(), types=()
+    ):
+        """
+        Wrapper around the GraphQLSchema, but compatible
+        with Strawberry types and directives.
+
+        :param query: the root query to use for the schema
+        :param mutation: the basic mutation type (if any)
+        :param mutation: the subscription type (if any)
+        :param directives: (additional) Strawberry directives
+        :param types: additional Strawberry types to register, return values of fields
+                      are automatically registered while return types for interfaces have
+                      to be manually registered
+        """
         super().__init__(
             query=query.field,
             mutation=mutation.field if mutation else None,
             subscription=subscription.field if subscription else None,
             directives=specified_directives
             + [directive.directive for directive in directives],
+            types=[type.field for type in types],
         )
 
     def __repr__(self):
