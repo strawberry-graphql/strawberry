65e9e709b937a68e8493d2706bb047e16901acf5
Patrick Arminio <patrick.arminio@gmail.com>
2019-10-28 09:59:16 +0100

    Use dev uvloop version

---
+++ tests/asgi/test_async.py
@@ -0,0 +1,31 @@
+import typing
+
+import pytest
+
+import strawberry
+from starlette.testclient import TestClient
+from strawberry.asgi import GraphQL
+
+
+@pytest.fixture
+def async_schema():
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        async def hello(self, info, name: typing.Optional[str] = None) -> str:
+            return f"Hello {name or 'world'}"
+
+    return strawberry.Schema(Query)
+
+
+@pytest.fixture
+def test_client(async_schema):
+    app = GraphQL(async_schema)
+
+    return TestClient(app)
+
+
+def test_simple_query(schema, test_client):
+    response = test_client.post("/", json={"query": "{ hello }"})
+
+    assert response.json() == {"data": {"hello": "Hello world"}}
