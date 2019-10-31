052ef488cefe2cf0534ebd4a4a949fdd5833eb60
Bryan Culver <me@bryanculver.com>
2019-10-31 02:18:34 -0400

    Refactor CLI Folder Structure (#12) (#196)
    * Refactor CLI Folder Structure (#12)
    
    - Creates separate command folder structure
    
    - Refactored cli __init__ to import commands explicitly
    
    * Rename 'cmd_server' to 'server' in cli commands.
    
    * Add simple tests for CLI
    
    * Change mokeypatching to mocking. Add pytest-mock.

--- strawberry/cli/__init__.py
+++ strawberry/cli/__init__.py
@@ -1,17 +1,6 @@
 import click
-import sys
 
-import os
-from starlette.applications import Starlette
-from starlette.middleware.cors import CORSMiddleware
-
-import importlib
-
-import uvicorn
-
-import hupper
-
-from strawberry.asgi import GraphQL
+from .commands.server import server as cmd_server
 
 
 @click.group()
@@ -19,30 +8,4 @@ def run():
     pass
 
 
-@run.command("server")
-@click.argument("module", type=str)
-@click.option("-h", "--host", default="0.0.0.0", type=str)
-@click.option("-p", "--port", default=8000, type=int)
-def server(module, host, port):
-    sys.path.append(os.getcwd())
-
-    reloader = hupper.start_reloader("strawberry.cli.run", verbose=False)
-
-    schema_module = importlib.import_module(module)
-
-    reloader.watch_files([schema_module.__file__])
-
-    app = Starlette(debug=True)
-
-    app.add_middleware(
-        CORSMiddleware, allow_headers=["*"], allow_origins=["*"], allow_methods=["*"]
-    )
-
-    graphql_app = GraphQL(schema_module.schema, debug=True)
-
-    app.add_route("/graphql", graphql_app)
-    app.add_websocket_route("/graphql", graphql_app)
-
-    print(f"Running strawberry on http://{host}:{port}/graphql 🍓")
-
-    uvicorn.run(app, host=host, port=port, log_level="error")
+run.add_command(cmd_server)
---
+++ tests/cli/test_server.py
@@ -0,0 +1,32 @@
+from click.testing import CliRunner
+
+from strawberry.cli.commands.server import server as cmd_server
+
+import uvicorn
+import hupper
+
+
+def test_cli_cmd_server(mocker):
+
+    # Mock of uvicorn.run
+    uvicorn_run_patch = mocker.patch('uvicorn.run')
+    uvicorn_run_patch.return_value = True
+    # Mock to prevent the reloader from kicking in
+    hupper_reloader_patch = mocker.patch('hupper.start_reloader')
+    hupper_reloader_patch.return_value = MockReloader()
+    runner = CliRunner()
+
+    result = runner.invoke(cmd_server, ['tests.cli.helpers.sample_schema'], "")
+    assert result.exit_code == 0
+
+    # We started the reloader
+    assert hupper.start_reloader.call_count == 1
+    assert uvicorn.run.call_count == 1
+
+    assert result.output == 'Running strawberry on http://0.0.0.0:8000/graphql 🍓\n'
+
+
+class MockReloader:
+    @staticmethod
+    def watch_files(*args, **kwargs):
+        return True
