f20739fefb15822ce26e3dacb98d94e5ef23a4e8
Bryan Culver <me@bryanculver.com>
2019-10-30 22:55:52 -0400

    Change mokeypatching to mocking. Add pytest-mock.

--- tests/cli/test_server.py
+++ tests/cli/test_server.py
@@ -5,29 +5,23 @@ from strawberry.cli.commands.server import server as cmd_server
 import uvicorn
 import hupper
 
-from unittest.mock import Mock
 
-
-def test_cli_cmd_server(monkeypatch):
-
-    def mock_uvicorn_run(*args, **kwargs):
-        exit(0)
-
-    def mock_hupper_start_reloader(*args, **kwargs):
-        return MockReloader()
+def test_cli_cmd_server(mocker):
 
     # Mock of uvicorn.run
-    monkeypatch.setattr(uvicorn, "run", Mock(wraps=mock_uvicorn_run))
+    uvicorn_run_patch = mocker.patch('uvicorn.run')
+    uvicorn_run_patch.return_value = True
     # Mock to prevent the reloader from kicking in
-    monkeypatch.setattr(hupper, "start_reloader", Mock(wraps=mock_hupper_start_reloader))
+    hupper_reloader_patch = mocker.patch('hupper.start_reloader')
+    hupper_reloader_patch.return_value = MockReloader()
     runner = CliRunner()
 
     result = runner.invoke(cmd_server, ['tests.cli.helpers.sample_schema'], "")
     assert result.exit_code == 0
 
     # We started the reloader
-    hupper.start_reloader.assert_called_once
-    uvicorn.run.assert_called_once
+    assert hupper.start_reloader.call_count == 1
+    assert uvicorn.run.call_count == 1
 
     assert result.output == 'Running strawberry on http://0.0.0.0:8000/graphql 🍓\n'
 
