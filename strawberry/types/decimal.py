7c13809d2f1666c93cd96170c622d4214c8c187f
A. Coady <coady@users.noreply.github.com>
2020-06-11 01:15:34 -0700

    Decimal description typo fixed. (#348)
    Co-authored-by: Patrick Arminio <patrick.arminio@gmail.com>

--- strawberry/types/decimal.py
+++ strawberry/types/decimal.py
@@ -6,7 +6,7 @@ from ..custom_scalar import scalar
 Decimal = scalar(
     decimal.Decimal,
     name="Decimal",
-    description="Date (isoformat)",
+    description="Decimal (fixed-point)",
     serialize=str,
     parse_value=decimal.Decimal,
 )
