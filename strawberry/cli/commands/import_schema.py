9cd4dc268fa959b6d933ac0f3377314d19986d4d
srbiotik <srbiotik@gmail.com>
2020-06-15 20:13:33 +0200

    Simplified import_schema, added nargs -1 for paths

--- strawberry/cli/commands/import_schema.py
+++ strawberry/cli/commands/import_schema.py
@@ -3,7 +3,6 @@ import os
 from os.path import abspath
 
 import click
-
 from graphql.language import parse
 from graphql.error import GraphQLError
 
@@ -16,41 +15,23 @@ GQL_IMPORT_REGEX = r"#import \"(.*\.gql|.*\.graphql)\""
 GQL_SCHEMA_TYPE = r"(?si)^Schema.*?}"
 
 
-def set_chdir(fn):
-    """ Helps resolve relative imports based on paths passed as arguments """
-
-    def wrapper(*args):
-        path, imported = args
-        previous_chdir = os.getcwd()
-        os.chdir(os.path.dirname(path))
-        paths = fn(path, imported)
-        os.chdir(previous_chdir)
-        return paths
-
-    return wrapper
-
-
 @click.command("import-schema", short_help="Parses SDL file to python dataclasses")
-@click.argument("schema", type=str, required=True)
-@click.option("-O", "--output", default="", type=str)
-@click.option("-R", "--recursive", default=True, type=bool)
-@click.option("-M", "--multifile", default=False, type=bool)
-def import_schema(schema, output, recursive, multifile):
+@click.argument("schema", type=str, required=True, nargs=-1)
+def import_schema(schema):
     """ Parses SDL file to python dataclasses and writes them out """
+    
+    templates = []  # Put rendered templates here
     try:
-        # Find paths and eliminate duplicates
-        paths = tuple(set(get_paths(schema, recursive)))
-        ast = get_ast(paths)
-        templates = []
+        ast = get_ast(schema)  # Get abstract syntax tree
         for definition in ast.definitions:
+            # Parse and render specific ast definitions
             templates.append(render_template(definition))
-
-        if not multifile:
-            imports = "import strawberry"
-            schema_string = "\n\n".join(templates)
-            imports += "\nimport typing\n\n" if "typing" in schema_string else ""
-            schema_string = imports + schema_string
-            write_strawberry_schema(schema_string, output, schema)
+        
+        imports = "import strawberry"
+        schema_string = "\n\n".join(templates)
+        imports += "\nimport typing\n\n" if "typing" in schema_string else ""
+        schema_string = imports + schema_string
+        click.echo(schema_string)
 
     except FileNotFoundError:
         print(f"File or files not found on schema path: {schema}")
@@ -63,8 +44,8 @@ def import_schema(schema, output, recursive, multifile):
 def get_ast(paths):
     """ Parse the content of files to AST """
     gql_strings = []
+    # Looping through the passed argument paths 
     for path in paths:
-        # TODO: Option to print multiple files or single file
         gql_strings += gql_file_to_string(path)
     # Join read file strings and parse them to ast
     concatenation = "\n".join(gql_strings)
@@ -72,31 +53,6 @@ def get_ast(paths):
     concatenation = re.sub(GQL_SCHEMA_TYPE, "", concatenation)
     ast = parse(concatenation)
     return ast
-
-
-def get_paths(schema, recursive):
-    """ Locate import statements and return them """
-    paths = [schema]
-    if recursive:
-        paths += get_imports(schema, [])
-
-    return paths
-
-
-@set_chdir
-def get_imports(path: str, imported: [str]) -> [str]:
-    """ Extract imprort statements and make them path-like """
-    paths = [path]
-    if path in imported:
-        return paths
-    with open(path, "r") as file:
-        schema_string = file.read()
-        imports = re.findall(GQL_IMPORT_REGEX, schema_string)
-        abs_imports = [abspath(imp) for imp in imports if abspath(imp) not in imported]
-        for imp in abs_imports:
-            paths += get_imports(imp, paths)
-
-    return paths
 
 
 def gql_file_to_string(path: str) -> [str]:
@@ -107,10 +63,3 @@ def gql_file_to_string(path: str) -> [str]:
         string = re.sub(r"\n\n", "", string)
 
     return [string]
-
-
-def write_strawberry_schema(schema_string, output, schema):
-    file = os.path.split(schema)[1]
-    filepath = os.path.join(output, to_snake_case(os.path.splitext(file)[0]) + ".py")
-    with open(abspath(filepath), "w") as f:
-        f.write(schema_string)
--- tests/cli/test_import_schema.py
+++ tests/cli/test_import_schema.py
@@ -20,6 +20,9 @@ def test_cli_cmd_import_specific_object_type(mocker):
 
     runner = CliRunner()
     file = os.path.join(os.getcwd(), "tests", "cli", "helpers", "type", SPECIFIC_TYPE)
-    output = os.path.join(".", "tests", "cli", "helpers", "output")
-    result = runner.invoke(cmd_import_schema, [file, "--output", output])
+    result = runner.invoke(cmd_import_schema, [file])
+    cwd = os.getcwd()
+    # Just so it's easier to read the result it is in the .gitignore file
+    with open(os.path.join(cwd, 'tests', 'cli', 'output', 'test_import_schema.py'), 'w') as f:
+        f.write(result.output)
     assert result.exit_code == 0
