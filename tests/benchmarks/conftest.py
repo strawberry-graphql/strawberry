79a29d1b139d75ba4868fdda070caa8ca3890912
sarendsen <sjoerd@optixdesigns.com>
2020-07-09 20:17:27 +0200

    Apply black

--- tests/benchmarks/conftest.py
+++ tests/benchmarks/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 
-@pytest.yield_fixture(scope='function')
+
+@pytest.yield_fixture(scope="function")
 def aio_benchmark(benchmark):
     import asyncio
     import threading
-    
+
     class Sync2Async:
         def __init__(self, coro, *args, **kwargs):
             self.coro = coro
@@ -12,11 +13,11 @@ def aio_benchmark(benchmark):
             self.kwargs = kwargs
             self.custom_loop = None
             self.thread = None
-        
+
         def start_background_loop(self) -> None:
             asyncio.set_event_loop(self.custom_loop)
             self.custom_loop.run_forever()
-        
+
         def __call__(self):
             evloop = None
             awaitable = self.coro(*self.args, **self.kwargs)
@@ -27,13 +28,21 @@ def aio_benchmark(benchmark):
             if evloop is None:
                 return asyncio.run(awaitable)
             else:
-                if not self.custom_loop or not self.thread or not self.thread.is_alive():
+                if (
+                    not self.custom_loop
+                    or not self.thread
+                    or not self.thread.is_alive()
+                ):
                     self.custom_loop = asyncio.new_event_loop()
-                    self.thread = threading.Thread(target=self.start_background_loop, daemon=True)
+                    self.thread = threading.Thread(
+                        target=self.start_background_loop, daemon=True
+                    )
                     self.thread.start()
-                
-                return asyncio.run_coroutine_threadsafe(awaitable, self.custom_loop).result()
-    
+
+                return asyncio.run_coroutine_threadsafe(
+                    awaitable, self.custom_loop
+                ).result()
+
     def _wrapper(func, *args, **kwargs):
         if asyncio.iscoroutinefunction(func):
             return benchmark(Sync2Async(func, *args, **kwargs))
