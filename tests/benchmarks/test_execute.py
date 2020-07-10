9da50d0eae1d551d696fb110c93b5c3586a3fd26
sarendsen <sjoerd@optixdesigns.com>
2020-07-10 11:38:09 +0200

    Add a more complex bench

--- tests/benchmarks/test_execute.py
+++ tests/benchmarks/test_execute.py
@@ -1,3 +1,5 @@
+import datetime
+import random
 from typing import List
 
 import pytest
@@ -5,6 +7,7 @@ import pytest
 import strawberry
 from asgiref.sync import async_to_sync
 from strawberry.graphql import execute
+from strawberry.types import Date
 
 
 @pytest.mark.asyncio
@@ -12,18 +15,39 @@ from strawberry.graphql import execute
     "items", [25, 100, 250],
 )
 def test_execute(benchmark, items):
+    birthday = datetime.datetime.now()
+    pets = ("cat", "shark", "dog", "lama")
+
+    @strawberry.type
+    class Pet:
+        id: int
+        name: str
+
     @strawberry.type
     class Patron:
         id: int
         name: str
         age: int
+        birthday: Date
+        tags: List[str]
+
+        @strawberry.field
+        def pets(self) -> List[Pet]:
+            return [Pet(id=i, name=random.choice(pets),) for i in range(5)]
 
     @strawberry.type
     class Query:
         @strawberry.field
         def patrons(self, info) -> List[Patron]:
             return [
-                Patron(id=i, name="Patrick", age=100) for i in range(items)
+                Patron(
+                    id=i,
+                    name="Patrick",
+                    age=100,
+                    birthday=birthday,
+                    tags=["go", "ajax"],
+                )
+                for i in range(items)
             ]
 
     schema = strawberry.Schema(query=Query)
@@ -34,9 +58,15 @@ def test_execute(benchmark, items):
             id
             name
             age
+            birthday
+            tags
+            pets {
+                id
+                name
+            }
           }
         }
     """
     result = benchmark(async_to_sync(execute), schema, query)
-    assert len(result.data["patrons"]) == items
     assert not result.errors
+    assert len(result.data["patrons"]) == items
