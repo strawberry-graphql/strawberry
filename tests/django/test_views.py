fca9617d15c27fde96c9664205e228050fd81a92
Patrick Arminio <patrick.arminio@gmail.com>
2020-08-16 12:16:22 +0100

    Fix typo

--- tests/django/test_views.py
+++ tests/django/test_views.py
@@ -45,7 +45,7 @@ schema = strawberry.Schema(query=Query)
 
 
 class GraphQLView(BaseGraphQLView):
-    def get_root_value(self, reques):
+    def get_root_value(self, request):
         return Query()
 
 
