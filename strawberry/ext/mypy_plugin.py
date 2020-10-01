0b5aea8282288ba50f3046433a4a441e8d7d4e6f
Jonathan Kim <jkimbo@gmail.com>
2020-10-01 12:41:34 +0100

    Update mypy plugin to fix errors

--- strawberry/ext/mypy_plugin.py
+++ strawberry/ext/mypy_plugin.py
@@ -12,10 +12,20 @@ def lazy_type_analyze_callback(ctx: AnalyzeTypeContext) -> Type:
     return type_
 
 
+def private_type_analyze_callback(ctx: AnalyzeTypeContext) -> Type:
+    type_name = ctx.type.args[0]
+    type_ = ctx.api.analyze_type(type_name)
+
+    return type_
+
+
 class StrawberryPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str):
         if fullname == "strawberry.lazy_type.LazyType":
             return lazy_type_analyze_callback
+
+        if fullname == "strawberry.private.Private":
+            return private_type_analyze_callback
 
         return None
 
--- tests/types/test_private_field.py
+++ tests/types/test_private_field.py
@@ -37,3 +37,25 @@ def test_private_field_with_strawberry_field():
     instance = Query(name="Luke", age=22)
     assert instance.name == "Luke"
     assert instance.age == 22
+
+
+def test_private_field_access_in_resolver():
+    @strawberry.type
+    class Query:
+        name: str
+        age: strawberry.Private[int] = strawberry.field(description="🤫")
+
+        @strawberry.field
+        def age_in_months(self) -> int:
+            return self.age * 12
+
+    schema = strawberry.Schema(query=Query)
+
+    result = schema.execute_sync(
+        "query { ageInMonths }", root_value=Query(name="Dave", age=7)
+    )
+
+    assert not result.errors
+    assert result.data == {
+        "ageInMonths": 84,
+    }
