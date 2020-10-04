9321037953c3cbd4844ce35915d4004f639f60db
ignormies <bryce.beagle@gmail.com>
2020-10-04 15:37:48 -0700

    Add some more todos

--- strawberry/backends/base_backend.py
+++ strawberry/backends/base_backend.py
@@ -13,15 +13,21 @@ class StrawberryBackend(ABC):
         self.mutation = mutation
         self.subscription = subscription
 
+    # TODO: Other parameters
     @abstractmethod
     def execute(self, query: str) -> ...:
         # TODO: Rename query to document?
         ...
 
+    # TODO: Other parameters
     @abstractmethod
     async def execute_async(self, query: str) -> ...:
         ...
 
+    # TODO: Other parameters
     @abstractmethod
     async def subscribe(self, query: str) -> ...:
+        # TODO: Should you be able to make subscription queries from execute and
+        #       execute_async?
+        # TODO: Should there be a subscribe/subscribe_async combo?
         ...
