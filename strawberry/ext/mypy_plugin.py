04b026ec314ece168f1f5c4131b231ea3a1b26a5
Patrick Arminio <patrick.arminio@gmail.com>
2020-10-05 10:30:20 +0100

    Fix plugin and move tests

--- strawberry/ext/mypy_plugin.py
+++ strawberry/ext/mypy_plugin.py
@@ -1,11 +1,20 @@
 from typing import Callable, Optional
 
-from mypy.nodes import GDEF, SymbolTableNode, TypeAlias
+from mypy.nodes import (
+    GDEF,
+    Expression,
+    IndexExpr,
+    NameExpr,
+    SymbolTableNode,
+    TupleExpr,
+    TypeAlias,
+)
 from mypy.plugin import (
     AnalyzeTypeContext,
     ClassDefContext,
     DynamicClassDefContext,
     Plugin,
+    SemanticAnalyzerPluginInterface,
 )
 from mypy.plugins import dataclasses
 from mypy.types import Type, UnionType
@@ -25,22 +34,35 @@ def private_type_analyze_callback(ctx: AnalyzeTypeContext) -> Type:
     return type_
 
 
+def _get_type_for_expr(expr: Expression, api: SemanticAnalyzerPluginInterface):
+    if isinstance(expr, NameExpr):
+        return api.named_type(expr.name)
+
+    if isinstance(expr, IndexExpr):
+        type_ = _get_type_for_expr(expr.base, api)
+        type_.args = [_get_type_for_expr(expr.index, api)]
+
+        return type_
+
+    raise ValueError(f"Unsupported expression f{type(expr)}")
+
+
 def union_hook(ctx: DynamicClassDefContext) -> None:
     types = ctx.call.args[1]
-    type_ = UnionType(
-        tuple(ctx.api.named_type(x.name) for x in types.items)  # type: ignore
-    )
 
-    type_alias = TypeAlias(
-        type_,
-        fullname=ctx.api.qualified_name(ctx.name),
-        line=ctx.call.line,
-        column=ctx.call.column,
-    )
+    if isinstance(types, TupleExpr):
+        type_ = UnionType(tuple(_get_type_for_expr(x, ctx.api) for x in types.items))
 
-    ctx.api.add_symbol_table_node(
-        ctx.name, SymbolTableNode(GDEF, type_alias, plugin_generated=True)
-    )
+        type_alias = TypeAlias(
+            type_,
+            fullname=ctx.api.qualified_name(ctx.name),
+            line=ctx.call.line,
+            column=ctx.call.column,
+        )
+
+        ctx.api.add_symbol_table_node(
+            ctx.name, SymbolTableNode(GDEF, type_alias, plugin_generated=True)
+        )
 
 
 class StrawberryPlugin(Plugin):
