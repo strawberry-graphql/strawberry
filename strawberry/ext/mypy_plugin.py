b6de04f03db9c3ca5e82162fbe1320185d294aa9
Patrick Arminio <patrick.arminio@gmail.com>
2020-10-05 09:24:52 +0100

    Add initial plugin for union types

--- strawberry/ext/mypy_plugin.py
+++ strawberry/ext/mypy_plugin.py
@@ -1,6 +1,12 @@
 from typing import Callable, Optional
 
-from mypy.plugin import AnalyzeTypeContext, ClassDefContext, Plugin
+from mypy.nodes import GDEF, Block, ClassDef, SymbolTableNode
+from mypy.plugin import (
+    AnalyzeTypeContext,
+    ClassDefContext,
+    DynamicClassDefContext,
+    Plugin,
+)
 from mypy.plugins import dataclasses
 from mypy.types import Type
 
@@ -19,7 +25,27 @@ def private_type_analyze_callback(ctx: AnalyzeTypeContext) -> Type:
     return type_
 
 
+def union_hook(ctx: DynamicClassDefContext) -> None:
+    # TODO: use these types to construct a propert return type (now it is Any)
+    # >>> types = ctx.call.args[1]
+    # >>> type_ = UnionType(tuple(ctx.api.named_type(x.name) for x in types.items))
+
+    class_def = ClassDef(ctx.name, Block([]))
+    class_def.fullname = ctx.api.qualified_name(ctx.name)
+    info = ctx.api.make_empty_type_info(class_def)  # type: ignore
+
+    ctx.api.add_symbol_table_node(ctx.name, SymbolTableNode(GDEF, info))
+
+
 class StrawberryPlugin(Plugin):
+    def get_dynamic_class_hook(
+        self, fullname: str
+    ) -> Optional[Callable[[DynamicClassDefContext], None]]:
+        if "strawberry.union.union" in fullname:
+            return union_hook
+
+        return None
+
     def get_type_analyze_hook(self, fullname: str):
         if fullname == "strawberry.lazy_type.LazyType":
             return lazy_type_analyze_callback
