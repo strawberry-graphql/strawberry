cfe5d3d2337315e532242fe8a974bee9b8e96826
Patrick Arminio <patrick.arminio@gmail.com>
2020-10-21 12:40:44 +0100

    Update API url

--- .github/release-check-action/config.py
+++ .github/release-check-action/config.py
@@ -5,4 +5,4 @@ RELEASE_FILE_PATH = "RELEASE.md"
 GITHUB_SHA = os.environ["GITHUB_SHA"]
 GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
 GITHUB_WORKSPACE = os.environ["GITHUB_WORKSPACE"]
-API_URL = "https://gentle-beach-72400.herokuapp.com/graphql"
+API_URL = "https://strawberry-github-bot.herokuapp.com/graphql"
