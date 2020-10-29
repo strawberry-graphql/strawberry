2e0270a130d413a32f80b68b5669270ad7b53985
srbiotik <srbiotik@gmail.com>
2020-10-29 15:04:34 +0100

    Change tests to handle strings

--- tests/cli/test_import_schema.py
+++ tests/cli/test_import_schema.py
@@ -2,29 +2,60 @@ import os
 
 from click.testing import CliRunner
 
-from strawberry.cli.commands.import_schema import import_schema as cmd_import_schema
+from strawberry.cli.commands.schema_importer import (
+    import_schema,
+    sdl_importer,
+    ast_converter,
+    sdl_transpiler,
+)
 
-SPECIFIC_TYPE = "Film.gql"
-SPECIFIC_SCHEMA = "swapi_schema.gql"
 
-
-def test_cli_cmd_import_schema(mocker):
+def test_cli_import_whole_schema(mocker):
 
     runner = CliRunner()
-    file = os.path.join(os.getcwd(), "tests", "cli", "helpers", SPECIFIC_SCHEMA)
-    result = runner.invoke(cmd_import_schema, [file], "-R")
+    file = os.path.join(os.getcwd(), "utils", "helpers", "swapi_schema.gql")
+    result = runner.invoke(import_schema.import_schema, [file], "-R")
     assert result.exit_code == 0
 
 
 def test_cli_cmd_import_specific_object_type(mocker):
-
     runner = CliRunner()
-    file = os.path.join(os.getcwd(), "tests", "cli", "helpers", "type", SPECIFIC_TYPE)
-    result = runner.invoke(cmd_import_schema, [file])
-    cwd = os.getcwd()
+    string_type = '''
+    """A single film."""
+    type Film implements Node {
+    """The title of this film."""
+    title: String
+
+    """The episode number of this film."""
+    episodeID: Int
+
+    """The opening paragraphs at the beginning of this film."""
+    openingCrawl: String
+
+    """The name of the director of this film."""
+    director: String
+
+    """The name(s) of the producer(s) of this film."""
+    producers: [String]
+
+    """The ISO 8601 date format of film release at original creator country."""
+    releaseDate: String
+    speciesConnection(after: String, first: Int, before: String, last: Int): FilmSpeciesConnection
+    starshipConnection(after: String, first: Int, before: String, last: Int): FilmStarshipsConnection
+    vehicleConnection(after: String, first: Int, before: String, last: Int): FilmVehiclesConnection
+    characterConnection(after: String, first: Int, before: String, last: Int): FilmCharactersConnection
+    planetConnection(after: String, first: Int, before: String, last: Int): FilmPlanetsConnection
+
+    """The ISO 8601 date format of the time that this resource was created."""
+    created: String
+
+    """The ISO 8601 date format of the time that this resource was edited."""
+    edited: String
+
+    """The ID of an object"""
+    id: ID!
+    }
+    '''
+    result = runner.invoke(import_schema.import_schema, [string_type])
     # Just so it's easier to read the result it is in the .gitignore file
-    with open(
-        os.path.join(cwd, "tests", "cli", "output", "test_import_schema.py"), "w"
-    ) as f:
-        f.write(result.output)
     assert result.exit_code == 0
