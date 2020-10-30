323b0b16922ebccab87c8b20f07785079b57dd18
srbiotik <srbiotik@gmail.com>
2020-10-30 14:23:18 +0100

    Added tests for scalars and lists

--- tests/cli/test_import_schema.py
+++ tests/cli/test_import_schema.py
@@ -1,7 +1,5 @@
 import os
 
-from click.testing import CliRunner
-
 from strawberry.cli.commands.schema_importer import (
     import_schema,
     sdl_importer,
@@ -9,18 +7,9 @@ from strawberry.cli.commands.schema_importer import (
     sdl_transpiler,
 )
 
-
-def test_cli_import_whole_schema(mocker):
-
-    runner = CliRunner()
-    file = os.path.join(os.getcwd(), "utils", "helpers", "swapi_schema.gql")
-    result = runner.invoke(import_schema.import_schema, [file], "-R")
-    assert result.exit_code == 0
-
-
+# Complex object
 def test_cli_cmd_import_specific_object_type(mocker):
-    runner = CliRunner()
-    string_type = '''
+    s = '''
     """A single film."""
     type Film implements Node {
     """The title of this film."""
@@ -56,6 +45,340 @@ def test_cli_cmd_import_specific_object_type(mocker):
     id: ID!
     }
     '''
-    result = runner.invoke(import_schema.import_schema, [string_type])
-    # Just so it's easier to read the result it is in the .gitignore file
-    assert result.exit_code == 0
+    output = sdl_importer.import_sdl(s)
+    assert output
+
+
+# region Scalars
+# Boolean
+def test_import_bool_field():
+    """ Test for a required Boolean field type with a field description and case change """
+    s = '''
+    type Woman {
+        """If a woman weighs less than a duck then she is a...?"""
+        isWitch: Boolean!
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class Woman:\n"
+        "    is_witch: bool = strawberry.field(\n"
+        "        name='isWitch',\n"
+        "        description='''If a woman weighs less than a duck then she is a...?'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_optional_bool_field():
+    """ Test for a optional Boolean field type with a field description and case change """
+    s = '''
+    type Woman {
+        """If a woman weighs less than a duck then she is a...?"""
+        isWitch: Boolean
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class Woman:\n"
+        "    is_witch: typing.Optional[bool] = strawberry.field(\n"
+        "        name='isWitch',\n"
+        "        description='''If a woman weighs less than a duck then she is a...?'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+# Int
+def test_import_int_field():
+    """ Test for a required Int field type with multiline description """
+    s = '''
+    type HolyHandGrenade {
+        """
+        First shalt thou take out the Holy Pin.
+        Then shalt thou count to...
+        """
+        numberThouShaltCount: Int!
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HolyHandGrenade:\n"
+        "    number_thou_shalt_count: int = strawberry.field(\n"
+        "        name='numberThouShaltCount',\n"
+        "        description='''First shalt thou take out the Holy Pin.\n"
+        "Then shalt thou count to...'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_optional_int_field():
+    """ Test for a required Int field type with multiline description """
+    s = '''
+    type HolyHandGrenade {
+        """
+        First shalt thou take out the Holy Pin.
+        Then shalt thou count to...
+        """
+        numberThouShaltCount: Int
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HolyHandGrenade:\n"
+        "    number_thou_shalt_count: typing.Optional[int] = strawberry.field(\n"
+        "        name='numberThouShaltCount',\n"
+        "        description='''First shalt thou take out the Holy Pin.\n"
+        "Then shalt thou count to...'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+# String
+def test_import_str_fields():
+    """ Test for String field types both optional and required """
+    s = '''
+    """
+    He who wants to cross the bridge,
+    must answer me these questions three,
+    and the other side he see...
+    """
+    type BridgeOfDeath {
+        """What is your name?"""
+        question: String!
+        answer: String
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type(description='''He who wants to cross the bridge,\n"
+        "must answer me these questions three,\n"
+        "and the other side he see...''')\n"
+        "class BridgeOfDeath:\n"
+        "    question: str = strawberry.field(\n"
+        "        description='''What is your name?'''\n"
+        "    )\n"
+        "    answer: typing.Optional[str]"
+    )
+
+    assert output == what_it_should_be
+
+
+# Float
+def test_import_float_field():
+    """ Test for a Float type """
+    s = '''
+    type Swallow {
+        """What is the airbourn speed of unladen african swallow?"""
+        speed: Float!
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class Swallow:\n"
+        "    speed: float = strawberry.field(\n"
+        "        description='''What is the airbourn speed of unladen african swallow?'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_optional_float_field():
+    """ Test for a Float type """
+    s = '''
+    type Swallow {
+        """What is the airbourn speed of unladen african swallow?"""
+        speed: Float
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class Swallow:\n"
+        "    speed: typing.Optional[float] = strawberry.field(\n"
+        "        description='''What is the airbourn speed of unladen african swallow?'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+# ID
+def test_import_id_field():
+    """ Test for a required ID field type """
+    s = """
+    type ArgumentClinic {
+        id: ID!
+    }
+    """
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class ArgumentClinic:\n"
+        "    id: strawberry.ID"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_id_field():
+    """ Test for a optional ID field type """
+    s = """
+    type ArgumentClinic {
+        id: ID
+    }
+    """
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class ArgumentClinic:\n"
+        "    id: typing.Optional[strawberry.ID]"
+    )
+
+    assert output == what_it_should_be
+
+
+# endregion
+# Enum
+# region List types
+def test_import_opt_list_opt_str_field():
+    """ Test for an optional list of optional strings type with field description """
+    s = '''
+    type HollyHandGrenade {
+        """And the people did feast on:"""
+        animals: [String]
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HollyHandGrenade:\n"
+        "    animals: typing.Optional[typing.List[typing.Optional[str]]] = strawberry.field(\n"
+        "        description='''And the people did feast on:'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_list_opt_str_field():
+    """ Test for a required list of optional strings type with field description """
+    s = '''
+    type HollyHandGrenade {
+        """And the people did feast on:"""
+        animals: [String]!
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HollyHandGrenade:\n"
+        "    animals: typing.List[typing.Optional[str]] = strawberry.field(\n"
+        "        description='''And the people did feast on:'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_opt_list_str_field():
+    """ Test for an optional list of required strings type with field description """
+    s = '''
+    type HollyHandGrenade {
+        """And the people did feast on:"""
+        animals: [String!]
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HollyHandGrenade:\n"
+        "    animals: typing.Optional[typing.List[str]] = strawberry.field(\n"
+        "        description='''And the people did feast on:'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+def test_import_list_str_field():
+    """ Test for an required list of required strings type with field description """
+    s = '''
+    type HollyHandGrenade {
+        """And the people did feast on:"""
+        animals: [String!]!
+    }
+    '''
+    output = sdl_importer.import_sdl(s)
+    what_it_should_be = (
+        "import strawberry\n"
+        "import typing\n"
+        "\n"
+        "\n"
+        "@strawberry.type\n"
+        "class HollyHandGrenade:\n"
+        "    animals: typing.List[str] = strawberry.field(\n"
+        "        description='''And the people did feast on:'''\n"
+        "    )"
+    )
+
+    assert output == what_it_should_be
+
+
+# endregion
