555cdfe357de0438810947ef3b206412e4bca5ec
Ambro17 <ambro17.1@gmail.com>
2020-11-02 21:15:41 -0300

    ✔ Add tests with some questions about possible improvements

--- tests/types/test_interfaces.py
+++ tests/types/test_interfaces.py
@@ -1,4 +1,5 @@
 import strawberry
+import textwrap
 
 
 def test_defining_interface():
@@ -81,3 +82,26 @@ def test_implementing_interface_twice():
 
     assert definition.is_interface is False
     assert definition.interfaces == [Node._type_definition]
+
+
+def test_interfaces_can_implement_other_interfaces():
+    @strawberry.interface
+    class Node:
+        id: strawberry.ID
+
+    @strawberry.interface
+    class UserNodeInterface(Node):
+        id: strawberry.ID
+        name: str
+
+    @strawberry.type
+    class Person(UserNodeInterface):
+        id: strawberry.ID
+        name: str
+
+    assert UserNodeInterface._type_definition.is_interface is True
+    assert UserNodeInterface._type_definition.interfaces == [Node._type_definition]
+
+    definition = Person._type_definition
+    assert definition.is_interface is False
+    assert definition.interfaces == [UserNodeInterface._type_definition]  # Should Node be here too?
