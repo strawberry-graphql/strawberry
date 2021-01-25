3e727b74c79681a5fea484d0c88883a570d95a87
Patrick Arminio <patrick.arminio@gmail.com>
2021-01-25 23:36:15 +0100

    Update API url

--- .github/release-check-action/config.py
+++ .github/release-check-action/config.py
@@ -5,4 +5,4 @@ RELEASE_FILE_PATH = "RELEASE.md"
 GITHUB_SHA = os.environ["GITHUB_SHA"]
 GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
 GITHUB_WORKSPACE = os.environ["GITHUB_WORKSPACE"]
-API_URL = "https://strawberry-github-bot.herokuapp.com/graphql"
+API_URL = "https://strawberry-bot-r3o3etjz6a-ew.a.run.app/graphql"
