33b2cbf7cb57d77bab26999b8e8bfd7074e2294c
Patrick Arminio <patrick.arminio@gmail.com>
2021-02-08 16:54:21 +0100

    Add test for info param

---
+++ tests/schema/test_info.py
@@ -0,0 +1,51 @@
+import strawberry
+from strawberry.types import Info
+
+
+def test_info_has_the_correct_shape():
+    my_context = "123"
+    root_value = "ABC"
+
+    @strawberry.type
+    class Result:
+        field_name: str
+        operation: str
+        path: str
+        context_equal: bool
+        root_equal: bool
+
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        def hello(self, info: Info[str, str]) -> Result:
+            return Result(
+                path="".join([str(p) for p in info.path.as_list()]),
+                operation=str(info.operation),
+                field_name=info.field_name,
+                context_equal=info.context == my_context,
+                root_equal=info.root_value == root_value,
+            )
+
+    schema = strawberry.Schema(query=Query)
+
+    query = """{
+        hello {
+            fieldName
+            contextEqual
+            operation
+            path
+            rootEqual
+        }
+    }"""
+
+    result = schema.execute_sync(query, context_value=my_context, root_value=root_value)
+
+    assert not result.errors
+    assert result.data["hello"] == {
+        # TODO: abstract this (in future)
+        "operation": "OperationDefinitionNode at 0:141",
+        "fieldName": "hello",
+        "path": "hello",
+        "contextEqual": True,
+        "rootEqual": True,
+    }
