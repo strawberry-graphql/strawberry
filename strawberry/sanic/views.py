993e9fa67589596a671af733d667885703bf60d7
Shiny Brar <charanjotbrar@gmail.com>
2021-03-01 17:50:45 -0500

    Ignoring dispatch overloading. -SB

--- strawberry/sanic/views.py
+++ strawberry/sanic/views.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from sanic.exceptions import abort
 from sanic.request import Request
-from sanic.response import BaseHTTPResponse, HTTPResponse, html
+from sanic.response import HTTPResponse, html
 from sanic.views import HTTPMethodView
 from strawberry.http import GraphQLHTTPResponse, process_result
 from strawberry.types import ExecutionResult
@@ -49,11 +49,9 @@ class GraphQLView(HTTPMethodView):
     def process_result(self, result: ExecutionResult) -> GraphQLHTTPResponse:
         return process_result(result)
 
-    async def dispatch_request(
-        self, request: Request
-    ) -> BaseHTTPResponse:  # type: ignore
+    async def dispatch_request(self, request: Request):  # type: ignore
         request_method = request.method.lower()
-        if not self.graphiql:
+        if not self.graphiql and request_method == "get":
             abort(404)
 
         show_graphiql = request_method == "get" and self.should_display_graphiql(
