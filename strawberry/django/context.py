24d034d6bde6e57ce0a3e667d964d384b5c0af68
pamaron <ernesto.arbitrio@gmail.com>
2021-03-02 23:46:01 +0100

    Bug fix: allow .get in context request as well as [] notation (#732)

--- strawberry/django/context.py
+++ strawberry/django/context.py
@@ -8,4 +8,10 @@ class StrawberryDjangoContext:
     request: HttpRequest
 
     def __getitem__(self, key):
+        # __getitem__ override needed to avoid issues for who's
+        # using info.context["request"]
+        return super().__getattribute__(key)
+
+    def get(self, key):
+        """Enable .get notation for accessing the request"""
         return super().__getattribute__(key)
