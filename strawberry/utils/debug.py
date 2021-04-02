bc6d43470162ab77e39fc09db275abaa68de992b
Kristóf Gilicze <giliczekristof@gmail.com>
2021-04-02 18:20:11 +0000

    add pretty_print for ExecutionContext and ExecutionResult

--- strawberry/utils/debug.py
+++ strawberry/utils/debug.py
@@ -1,10 +1,17 @@
 import datetime
 import json
+import sys
+from contextlib import redirect_stdout
 from json import JSONEncoder
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional, TextIO
 
 from pygments import highlight, lexers
 from pygments.formatters import Terminal256Formatter
+
+from graphql.error.graphql_error import GraphQLError
+
+from strawberry.http import process_result
+from strawberry.types.execution import ExecutionContext, ExecutionResult
 
 from .graphql_lexer import GraphQLLexer
 
@@ -15,21 +22,103 @@ class StrawberryJSONEncoder(JSONEncoder):
 
 
 def pretty_print_graphql_operation(
-    operation_name: str, query: str, variables: Dict["str", Any]
+    operation_name: Optional[str],
+    query: str,
+    variables: Optional[Dict["str", Any]],
 ):
-    """Pretty print a GraphQL operation using pygments.
-
-    Won't print introspection operation to prevent noise in the output."""
-
-    if operation_name == "IntrospectionQuery":
+    """
+    Deprecated
+    Please use `strawberry.utils.debug.pretty_print_graphql_execution_context` instead!
+    ---
+    Pretty print a GraphQL operation using pygments.
+    Won't print introspection operation to prevent noise in the output.
+    """
+    if "query IntrospectionQuery" in query:
         return
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    pretty_print_graphql_execution_context(
+        operation_name, query, variables, attach_timestamp=True
+    )
 
-    print(f"[{now}]: {operation_name or 'No operation name'}")
-    print(highlight(query, GraphQLLexer(), Terminal256Formatter()))
 
-    if variables:
-        variables_json = json.dumps(variables, indent=4, cls=StrawberryJSONEncoder)
+def pretty_print_graphql_execution_context(
+    operation_name: Optional[str],
+    query: str,
+    variables: Optional[Dict["str", Any]],
+    attach_timestamp=False,
+    stream: TextIO = None,
+):
+    if stream is None:
+        stream = sys.stdout
 
-        print(highlight(variables_json, lexers.JsonLexer(), Terminal256Formatter()))
+    with redirect_stdout(stream):
+
+        operation_name = operation_name or "No operation name"
+
+        if attach_timestamp:
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{now}]: {operation_name}")
+        else:
+            print(operation_name)
+
+        print(highlight(query, GraphQLLexer(), Terminal256Formatter()))
+
+        if variables:
+            variables_json = json.dumps(variables, indent=4, cls=StrawberryJSONEncoder)
+            print(highlight(variables_json, lexers.JsonLexer(), Terminal256Formatter()))
+
+
+def pretty_print_graphql_execution_result(
+    data: Optional[Dict[str, Any]],
+    errors: Optional[List[GraphQLError]],
+    extensions: Optional[Dict[str, Any]] = None,
+    stream: TextIO = None,
+):
+    if stream is None:
+        stream = sys.stdout
+
+    formatted_json = json.dumps(
+        process_result(ExecutionResult(data, errors, extensions)),
+        indent=4,
+        cls=StrawberryJSONEncoder,
+    )
+
+    colorful_json = highlight(
+        formatted_json, lexers.JsonLexer(), Terminal256Formatter()
+    )
+
+    with redirect_stdout(stream):
+        print(colorful_json.rstrip("\n"))
+
+
+def pretty_print_graphql(
+    execution_context: ExecutionContext,
+    execution_result: ExecutionResult,
+    skip_introspection_queries=False,
+    stream: TextIO = None,
+):
+    if stream is None:
+        stream = sys.stdout
+
+    if (
+        "query IntrospectionQuery" in execution_context.query
+        and skip_introspection_queries
+    ):
+        #  IntrospectionQuery is not always set as operation_name
+        #  ( single query, operation_name can be ommited )
+        #  So we have too look for it in the query.
+        return
+
+    pretty_print_graphql_execution_context(
+        execution_context.operation_name,
+        execution_context.query,
+        execution_context.variables,
+        stream=stream,
+    )
+
+    pretty_print_graphql_execution_result(
+        execution_result.data,
+        execution_result.errors,
+        execution_result.extensions,
+        stream=stream,
+    )
--- tests/utils/test_pretty_print.py
+++ tests/utils/test_pretty_print.py
@@ -1,6 +1,13 @@
+import io
 from decimal import Decimal
 
-from strawberry.utils.debug import pretty_print_graphql_operation
+from strawberry.types.execution import ExecutionContext, ExecutionResult
+from strawberry.utils.debug import (
+    pretty_print_graphql,
+    pretty_print_graphql_execution_context,
+    pretty_print_graphql_execution_result,
+    pretty_print_graphql_operation,
+)
 
 
 def test_pretty_print(mocker):
@@ -32,3 +39,72 @@ def test_pretty_print_variables_object(mocker):
         '{\n    \x1b[38;5;28;01m"example"\x1b[39;00m: '
         "\x1b[38;5;124m\"Decimal('1')\"\x1b[39m\n}\n"
     )
+
+
+def test_pretty_print_graphql_execution_context(mocker):
+    mock = mocker.patch("builtins.print")
+
+    pretty_print_graphql_execution_context(
+        "Example", "{ query }", variables={"example": 3}
+    )
+
+    mock.assert_called_with(
+        '{\n    \x1b[38;5;28;01m"example"\x1b[39;00m: \x1b[38;5;241m3\x1b[39m\n}\n'
+    )
+
+
+def test_pretty_print_graphql_execution_result(mocker):
+    mock = mocker.patch("builtins.print")
+
+    pretty_print_graphql_execution_result(data=None, errors=[])
+
+    mock.assert_called_with(
+        '{\n    \x1b[38;5;28;01m"data"\x1b[39;00m: \x1b[38;5;28;01mnull\x1b[39;00m\n}'
+    )
+
+
+def test_pretty_print_graphql_stream():
+
+    execution_context = ExecutionContext(
+        operation_name="Example", query="{ query }", variables={"example": Decimal(1)}
+    )
+
+    execution_result = ExecutionResult(
+        data=None,
+        errors=[],
+    )
+
+    test_stream = io.StringIO()
+
+    pretty_print_graphql(execution_context, execution_result, stream=test_stream)
+
+    assert test_stream.getvalue() == (
+        "Example\n{ \x1b[38;5;125mquery\x1b[39m }\n\n{\n    \x1b"
+        '[38;5;28;01m"example"\x1b[39;00m: \x1b[38;5;124m"Decimal(\'1\')"'
+        '\x1b[39m\n}\n\n{\n    \x1b[38;5;28;01m"data"\x1b'
+        "[39;00m: \x1b[38;5;28;01mnull\x1b[39;00m\n}\n"
+    )
+
+    test_stream.close()
+
+
+def test_pretty_print_graphql_skipping_introspection_queries():
+
+    ec = ExecutionContext(
+        operation_name="Example",
+        query="{ query IntrospectionQuery }",
+        variables={"example": Decimal(1)},
+    )
+
+    er = ExecutionResult(
+        data=None,
+        errors=[],
+    )
+
+    test_stream = io.StringIO()
+
+    pretty_print_graphql(ec, er, skip_introspection_queries=True, stream=test_stream)
+
+    assert test_stream.getvalue() == ""
+
+    test_stream.close()
