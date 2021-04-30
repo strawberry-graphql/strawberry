af759ac53d04d59c5efc06f4cf03f9802708f118
ignormies <bryce.beagle@gmail.com>
2021-04-29 18:51:54 -0700

    Fix issues with info object on fields

--- strawberry/field.py
+++ strawberry/field.py
@@ -233,7 +233,7 @@ class StrawberryField(dataclasses.Field):
                 context=info.context,
                 root_value=info.root_value,
                 variable_values=info.variable_values,
-                return_type=self._get_return_type(),
+                return_type=self.type_annotation,
                 operation=info.operation,
                 path=info.path,
             )
