089d757b8be8273fa8542d87a6254bf51337fec3
Jonathan Ehwald <github@ehwald.info>
2021-05-01 17:27:38 +0200

    Add tests for the export_schema command

---
+++ tests/cli/test_export_schema.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+
+from strawberry.cli.commands.export_schema import export_schema as cmd_export_schema
+
+
+def test_cli_cmd_export_schema():
+    cli_runner = CliRunner()
+    result = cli_runner.invoke(cmd_export_schema, ["tests.cli.helpers.sample_schema"])
+    assert result.exit_code == 0
+
+    assert result.output == (
+        "type Query {\n"
+        "  user: User!\n"
+        "}\n"
+        "\n"
+        "type User {\n"
+        "  name: String!\n"
+        "  age: Int!\n"
+        "}\n"
+    )
