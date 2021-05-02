0be48c1471df6733c9deeee7e756c97dce2737b7
Jonathan Ehwald <github@ehwald.info>
2021-05-02 22:21:59 +0200

    Add a reminder to test a branch once issue #766 has been fixed

--- strawberry/sanic/utils.py
+++ strawberry/sanic/utils.py
@@ -28,6 +28,9 @@ def convert_request_to_files_dict(request: Request) -> dict:
             # Turn single item file lists into a single file object
             files_dict[field_name] = BytesIO(file_list[0].body)
         else:
+            # TODO: Test this branch once file list uploads are supported (see #766).
+            # https://github.com/strawberry-graphql/strawberry/issues/766
+
             # Preserve actual file lists for file list upload support
             file_object_list = [BytesIO(file_item.body) for file_item in file_list]
             files_dict[field_name] = file_object_list
