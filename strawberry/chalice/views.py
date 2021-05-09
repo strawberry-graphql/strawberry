997be85928d9556b3e05fbb8f5fe4c883c82b185
Mark Sheehan <mark.sheehan@stackbadger.net>
2021-05-09 22:19:45 +0100

    added to the chalice view and added unit tests for chalice

--- strawberry/chalice/views.py
+++ strawberry/chalice/views.py
@@ -1,13 +1,11 @@
 import functools
 from http import HTTPStatus
-from typing import Dict
 
-from chalice.app import Request, Response
-
-from strawberry.schema import BaseSchema
-from strawberry.http import GraphQLHTTPResponse, process_result
-from strawberry.types import ExecutionResult
+from chalice.app import CaseInsensitiveMapping, Request, Response
 from strawberry.chalice.graphiql import render_graphiql_page
+from strawberry.http import GraphQLHTTPResponse, process_result
+from strawberry.schema import BaseSchema
+from strawberry.types import ExecutionResult
 
 
 class GraphQLView:
@@ -16,20 +14,19 @@ class GraphQLView:
         self.graphiql: bool = render_graphiql
 
     @staticmethod
-    @functools.lru_cache
+    @functools.lru_cache()
     def render_graphiql() -> str:
         """
-        Returns a string containing the html for the graphiql webpage. It also caches the result using lru cache
-        This saves loading from disk each time it is invoked.
+        Returns a string containing the html for the graphiql webpage. It also caches the
+        result using lru cache. This saves loading from disk each time it is invoked.
         Returns:
 
         """
-        # We use the same method as strawberry flask as subscriptions are not currently enabled
         result = render_graphiql_page()
         return result
 
     @staticmethod
-    def has_html_been_asked_for(headers: Dict) -> bool:
+    def has_html_been_asked_for(headers: CaseInsensitiveMapping) -> bool:
         """
         Do the headers indicate that the invoker has requested html?
         Args:
@@ -40,13 +37,31 @@ class GraphQLView:
         """
         try:
             accept_headers = headers.get("accept")
-        except KeyError:
+        except (KeyError, TypeError):
+            return False
+
+        if accept_headers is None:
             return False
 
         if "text/html" in accept_headers:
             return True
 
         if "*/*" in accept_headers:
+            return True
+
+        return False
+
+    @staticmethod
+    def method_is_post(method: str) -> bool:
+        """
+        If the get method has been called this is True
+        Args:
+            method: A request verb such as GET, POST, PUT etc.
+
+        Returns:
+            True if the method is post
+        """
+        if method == "POST":
             return True
 
         return False
@@ -66,29 +81,64 @@ class GraphQLView:
 
         return False
 
+    @staticmethod
+    def invalid_query_response() -> Response:
+        """
+        A response for malformed queries
+        Returns:
+        An errors response
+        """
+        return Response(
+            body={"errors": ["Provide a valid query / body to your call"]},
+            status_code=HTTPStatus.OK,
+        )
+
+    @staticmethod
+    def invalid_rest_verb_response() -> Response:
+        """
+        A response for calling the graphql endpoint with a non POST request
+        Returns:
+
+        """
+        return Response(
+            body={"errors": ["GraphQL queries must be of request type POST"]},
+            status_code=HTTPStatus.OK,
+        )
+
     def execute_request(self, request: Request) -> Response:
         """
         Parse the request process it with strawberry and return a response
         Args:
-            request: The chalice request
+            request: The chalice request this contains the headers and body
 
         Returns:
             A chalice response
         """
         if self.graphiql:
-            if self.has_html_been_asked_for(headers=request.headers) and self.method_is_get(request.method):
-                graphiql_page = self.render_graphiql()
-                return Response(body=graphiql_page, headers={"content-type": "text/html"}, status_code=200)
+            if self.has_html_been_asked_for(
+                headers=request.headers
+            ) and self.method_is_get(request.method):
+                graphiql_page: str = self.render_graphiql()
+                return Response(
+                    body=graphiql_page,
+                    headers={"content-type": "text/html"},
+                    status_code=200,
+                )
+
+        if not self.method_is_post(request.method):
+            return self.invalid_rest_verb_response()
 
         request_data = request.json_body
 
+        if request_data is None:
+            return self.invalid_query_response()
         try:
             query = request_data["query"]
             variables = request_data.get("variables")
             operation_name = request_data.get("operationName")
 
-        except KeyError:
-            return Response(body="Provide a valid query", status_code=HTTPStatus.BAD_REQUEST)
+        except (KeyError, TypeError):
+            return self.invalid_query_response()
 
         result: ExecutionResult = self._schema.execute_sync(
             query,
---
+++ tests/chalice/test_views.py
@@ -0,0 +1,68 @@
+import json
+from http import HTTPStatus
+
+from app import app
+
+from chalice.test import Client, HTTPResponse
+
+
+def test_chalice_server_index_route_returns():
+    with Client(app) as client:
+        response = client.http.get("/")
+        assert response.status_code == 200
+        assert response.json_body == {"strawberry": "cake"}
+
+
+def test_graphiql_view_is_returned_if_accept_is_html():
+    with Client(app) as client:
+        headers = {"Accept": "text/html"}
+        response = client.http.get("/graphql", headers=headers)
+        assert response.status_code == 200
+        assert "GraphiQL" in str(response.body)
+
+
+def response_is_of_error_type(response: HTTPResponse):
+    if "errors" in response.json_body.keys():
+        if response.status_code == HTTPStatus.OK:
+            return True
+    return False
+
+
+def test_get_graphql_view_with_json_accept_type_is_rejected():
+    with Client(app) as client:
+        headers = {"Accept": "application/json"}
+        response = client.http.get("/graphql", headers=headers)
+        assert response_is_of_error_type(response)
+
+
+def test_malformed_unparsable_json_query_returns_error():
+    with Client(app) as client:
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+        # The query key in the json dict is missing
+        query = "I am a malformed query"
+        response = client.http.post("/graphql", headers=headers, body=json.dumps(query))
+        assert response_is_of_error_type(response)
+
+
+# Tests from this points are checking that graphql is getting routed through the endpoint
+# correctly to the strawberry execute_sync command, so just test one happy path case of a
+# query and a mutation as execute_sync command is tested elsewhere.
+def test_graphiql_query():
+    with Client(app) as client:
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+        query = {"query": "query GreetMe {greetings}"}
+        response = client.http.post("/graphql", headers=headers, body=json.dumps(query))
+        assert response.status_code == HTTPStatus.OK
+        assert "hello" == response.json_body["data"]["greetings"]
+
+
+def test_graphiql_mutation():
+    with Client(app) as client:
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+        query = {"query": """ mutation EchoMutation { echo(stringToEcho: "mark")} """}
+        response = client.http.post("/graphql", headers=headers, body=json.dumps(query))
+        assert response.status_code == HTTPStatus.OK
+        assert "mark" == response.json_body["data"]["echo"]
