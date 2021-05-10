412dbd1f37cd8f0fcb16023f5565859f8c7882ae
Jonathan Kim <jkimbo@gmail.com>
2021-05-10 12:44:45 +0100

    Add helper function to extra request data from dict

--- strawberry/http.py
+++ strawberry/http.py
@@ -4,6 +4,7 @@ from typing_extensions import TypedDict
 
 from graphql.error import format_error as format_graphql_error
 
+from strawberry.exceptions import MissingQueryError
 from strawberry.types import ExecutionResult
 
 
@@ -19,3 +20,22 @@ def process_result(result: ExecutionResult) -> GraphQLHTTPResponse:
         data["errors"] = [format_graphql_error(err) for err in result.errors]
 
     return data
+
+
+class GraphQLRequestData(TypedDict, total=False):
+    query: str
+    variables: Optional[Dict[str, Any]]
+    operation_name: Optional[str]
+
+
+def parse_request_data(data: Dict) -> GraphQLRequestData:
+    result: GraphQLRequestData = {}
+
+    if "query" not in data:
+        raise MissingQueryError()
+
+    result["query"] = data["query"]
+    result["variables"] = data.get("variables")
+    result["operation_name"] = data.get("operationName")
+
+    return result
