0ca9d57c0dc17cb7e61e684f1525dd5c05cfeea0
ignormies <bryce.beagle@gmail.com>
2021-07-18 12:24:33 -0700

    Fix LazyType test in pytest path system

--- tests/types/resolving/test_lazy_type.py
+++ tests/types/resolving/test_lazy_type.py
@@ -4,19 +4,21 @@ from strawberry.lazy_type import LazyType
 
 
 @strawberry.type
-class LazierType:
+class LaziestType:
     something: bool
 
 
 def test_lazy_type():
     # This type is in the same file but should adequately test the logic
-    LaziestType = LazyType["LazierType", "tests.types.resolving.test_lazy_type"]
+    # Module path is short and relative because of the way pytest runs the file
+    LazierType = LazyType["LaziestType", "test_lazy_type"]
 
-    annotation = StrawberryAnnotation(LaziestType)
+    annotation = StrawberryAnnotation(LazierType)
     resolved = annotation.resolve()
 
     # Currently StrawberryAnnotation(LazyType).resolve() returns the unresolved
     # LazyType. We may want to find a way to directly return the referenced object
     # without a second resolving step.
     assert isinstance(resolved, LazyType)
-    assert resolved.resolve_type() is LazierType
+    assert resolved is LazierType
+    assert resolved.resolve_type() is LaziestType
