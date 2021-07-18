15ecfebae9a9da783394fa2a32723fc06c43e351
Object905 <Object905@gmail.com>
2021-07-18 21:40:20 +0500

    Use utils.get_graphiql_html for django impl

--- strawberry/django/views.py
+++ strawberry/django/views.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-import os
 from typing import Any, Dict, Optional
 
 from django.core.exceptions import SuspiciousOperation
@@ -14,7 +13,6 @@ from django.utils.decorators import classonlymethod, method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
-import strawberry
 from strawberry.exceptions import MissingQueryError
 from strawberry.file_uploads.utils import replace_placeholders_with_files
 from strawberry.http import (
@@ -24,6 +22,7 @@ from strawberry.http import (
     process_result,
 )
 from strawberry.types import ExecutionResult
+from strawberry.utils.get_graphiql_html import get_graphiql_html
 
 from ..schema import BaseSchema
 from .context import StrawberryDjangoContext
@@ -78,21 +77,17 @@ class BaseView(View):
         if not self.graphiql:
             raise Http404()
 
-        try:
-            template = Template(render_to_string("graphql/graphiql.html"))
-        except TemplateDoesNotExist:
-            template = Template(
-                open(
-                    os.path.join(
-                        os.path.dirname(os.path.abspath(strawberry.__file__)),
-                        "static/graphiql.html",
-                    ),
-                    "r",
-                ).read()
-            )
-
         context = context or {}
-        context.update({"SUBSCRIPTION_ENABLED": "false"})
+        context.update(
+            {"SUBSCRIPTION_ENABLED": "false", "SUBSCRIPTION_GRAPHQL_WS": "false"}
+        )
+
+        try:
+            template = Template(
+                render_to_string("graphql/graphiql.html", context=context)
+            )
+        except TemplateDoesNotExist:
+            template = Template(get_graphiql_html(raw=True))
 
         response = TemplateResponse(request=request, template=None, context=context)
         response.content = template.render(RequestContext(request, context))
--- strawberry/utils/get_graphiql_html.py
+++ strawberry/utils/get_graphiql_html.py
@@ -4,10 +4,26 @@ import typing
 
 
 def get_graphiql_html(
-    subscription_enabled: bool,
+    subscription_enabled: bool = False,
     subscription_graphql_ws: bool = False,
     graphiql_html_file_path: typing.Optional[os.PathLike] = None,
+    raw: bool = False,
 ) -> str:
+    """
+    Renders graphiql template from `graphiql_html_file_path`,
+    if not provided renders bundled strawberry template.
+
+    If `subscription_enabled` is True,
+    then graphiql will try to connect to ws endpoint on same url path.
+
+    If `subscription_graphql_ws` is True,
+    then graphiql will use new [graphql-ws](https://github.com/enisdenjo/graphql-ws)
+    library for subscription client. Otherwise it will use depricated
+    [subscriptions-transport-ws](https://github.com/apollographql/subscriptions-transport-ws).
+
+    If `raw` is True, then all other options are ignored and template
+    string returned without interpolations.
+    """
     if graphiql_html_file_path is None:
         graphiql_html_file_path = (
             pathlib.Path(__file__).parent.parent / "static" / "graphiql.html"
@@ -16,10 +32,12 @@ def get_graphiql_html(
         graphiql_html_file_path = pathlib.Path(graphiql_html_file_path)
 
     template = graphiql_html_file_path.read_text()
-    template = template.replace(
-        "{{ SUBSCRIPTION_ENABLED }}", str(subscription_enabled).lower()
-    )
-    template = template.replace(
-        "{{ SUBSCRIPTION_GRAPHQL_WS }}", str(subscription_graphql_ws).lower()
-    )
+
+    if not raw:
+        template = template.replace(
+            "{{ SUBSCRIPTION_ENABLED }}", str(subscription_enabled).lower()
+        )
+        template = template.replace(
+            "{{ SUBSCRIPTION_GRAPHQL_WS }}", str(subscription_graphql_ws).lower()
+        )
     return template
