7cab245c179a7ba4ebdaff2ecd5a571d5da58c2b
Jonathan Ehwald <github@ehwald.info>
2021-08-05 00:26:39 +0200

    Refactor debug server tests

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -1,6 +1,9 @@
+import sys
+
 import pytest
 
 from click.testing import CliRunner
+from starlette.testclient import TestClient
 
 
 @pytest.fixture
@@ -9,3 +12,13 @@ def cli_runner(mocker):
     uvicorn_run_patch = mocker.patch("uvicorn.run")
     uvicorn_run_patch.return_value = True
     return CliRunner()
+
+
+@pytest.fixture
+def debug_server_client(mocker):
+    schema_import_path = "tests.fixtures.sample_package.sample_module"
+    mocker.patch.object(sys, "argv", ["strawberry", "server", schema_import_path])
+
+    from strawberry.cli.debug_server import app
+
+    return TestClient(app)
