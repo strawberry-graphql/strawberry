66ecdffc932246dd05467149e8f2af23b677ddf6
Patrick Arminio <patrick.arminio@gmail.com>
2021-08-04 20:31:34 +0100

    Use json

--- .github/release-check-action/check.py
+++ .github/release-check-action/check.py
@@ -59,8 +59,8 @@ print(f"::set-output name=release_status::{status}")
 
 if release_info:
     changelog = release_info["changelog"]
-    changelog = changelog.replace("\n", "\\n")
     changelog = changelog.replace("`", r"\`")
+    changelog = json.dumps(changelog)
 
     print(f"::set-output name=changelog::{changelog}")
 
