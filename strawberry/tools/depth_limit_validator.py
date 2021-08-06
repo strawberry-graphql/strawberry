f2de2afd7616621795cca3a71622978fecd9b0a0
Jonathan Kim <jkimbo@gmail.com>
2021-08-06 09:51:35 +0200

    Add license

--- strawberry/tools/depth_limit_validator.py
+++ strawberry/tools/depth_limit_validator.py
@@ -1,3 +1,30 @@
+# This is a Python port of https://github.com/stems/graphql-depth-limit
+# which is licensed under the terms of the MIT license, reproduced below.
+#
+# -----------
+#
+# MIT License
+#
+# Copyright (c) 2017 Stem
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import re
 from typing import Callable, Dict, List, Optional, Union
 
@@ -26,8 +53,6 @@ def depth_limit_validator(
 ):
     """
     Creates a validator for the GraphQL query depth
-
-    This is a Python port of https://github.com/stems/graphql-depth-limit
 
     - max_depth - The maximum allowed depth for any operation in a GraphQL document.
     - ignore - Stops recursive depth checking based on a field name.
