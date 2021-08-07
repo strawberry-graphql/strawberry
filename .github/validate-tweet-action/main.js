c10d84515e71d2ff0f3692fed66f6a41e033fc7f
Patrick Arminio <patrick.arminio@gmail.com>
2021-08-07 23:29:19 +0100

    Run actual check

--- .github/validate-tweet-action/main.js
+++ .github/validate-tweet-action/main.js
@@ -1,5 +1,13 @@
+const core = require('@actions/core');
 const twitter = require('twitter-text')
 
-const tweet = "This is an example tweet";
+const tweetEncoded = core.getInput('tweet');
 
-console.log(twitter.txt.parseTweet(tweet));
+const buff = Buffer.from(tweetEncoded, "base64");
+const tweet = buff.toString("ascii");
+
+const result = twitter.parseTweet(tweet);
+
+if (!result.valid) {
+    core.setFailed(`Tweet is not valid!`);
+}
