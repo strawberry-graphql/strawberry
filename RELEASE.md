1c3132c1e091a968a9ab72d784e64015aee01315
Jonathan Ehwald <github@ehwald.info>
2021-08-22 17:12:02 +0200

    Make use of the uvicorn reload feature (#1114)
    Co-authored-by: Patrick Arminio <patrick.arminio@gmail.com>

---
+++ RELEASE.md
@@ -0,0 +1,5 @@
+Release type: patch
+
+This release refactors the reload feature of the built-in debug server. The refactor
+made the debug server more responsive and allowed us to remove `hupper` from the
+dependencies.
--- tests/cli/test_server.py
+++ tests/cli/test_server.py
@@ -1,8 +1,12 @@
+import os
+import signal
+import subprocess
 import sys
+import time
 
 import pytest
 
-import hupper
+import requests
 import uvicorn
 
 from strawberry.cli.commands.server import server as cmd_server
@@ -11,12 +15,9 @@ from strawberry.cli.commands.server import server as cmd_server
 def test_cli_cmd_server(cli_runner):
     schema = "tests.fixtures.sample_package.sample_module"
     result = cli_runner.invoke(cmd_server, [schema])
+
     assert result.exit_code == 0
-
-    # We started the reloader
-    assert hupper.start_reloader.call_count == 1
     assert uvicorn.run.call_count == 1
-
     assert result.output == "Running strawberry on http://0.0.0.0:8000/graphql 🍓\n"
 
 
@@ -24,12 +25,9 @@ def test_cli_cmd_server_app_dir_option(cli_runner):
     result = cli_runner.invoke(
         cmd_server, ["--app-dir=./tests/fixtures/sample_package", "sample_module"]
     )
+
     assert result.exit_code == 0
-
-    # We started the reloader
-    assert hupper.start_reloader.call_count == 1
     assert uvicorn.run.call_count == 1
-
     assert result.output == "Running strawberry on http://0.0.0.0:8000/graphql 🍓\n"
 
 
@@ -73,7 +71,7 @@ def test_invalid_schema_instance(cli_runner):
     assert expected_error in result.output
 
 
-@pytest.mark.parametrize("dependency", ["hupper", "uvicorn", "starlette.applications"])
+@pytest.mark.parametrize("dependency", ["uvicorn", "starlette"])
 def test_missing_debug_server_dependencies(cli_runner, mocker, dependency):
     mocker.patch.dict(sys.modules, {dependency: None})
 
@@ -86,3 +84,57 @@ def test_missing_debug_server_dependencies(cli_runner, mocker, dependency):
         "The debug server requires additional packages, install them by running:\n"
         "pip install strawberry-graphql[debug-server]\n"
     )
+
+
+def test_debug_server_routes(debug_server_client):
+    for path in ["/", "/graphql"]:
+        response = debug_server_client.get(path)
+        assert response.status_code == 200
+
+
+def test_automatic_reloading(tmp_path):
+    source = (
+        "import strawberry\n"
+        "@strawberry.type\n"
+        "class Query:\n"
+        "    @strawberry.field\n"
+        "    def number(self) -> int:\n"
+        "        return {}\n"
+        "schema = strawberry.Schema(query=Query)\n"
+    )
+
+    schema_file_path = tmp_path / "schema.py"
+    schema_file_path.touch()
+    schema_file_path.write_text(source.format(42))
+
+    args = ["poetry", "run", "strawberry", "server", "--app-dir", tmp_path, "schema"]
+
+    with subprocess.Popen(
+        args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid
+    ) as proc:
+
+        url = "http://127.0.0.1:8000/graphql"
+        query = {"query": "{ number }"}
+
+        # It takes uvicorn some time to initially start the server
+        for i in range(5):
+            try:
+                response = requests.post(url, json=query)
+                assert response.status_code == 200
+                assert response.json() == {"data": {"number": 42}}
+            except requests.RequestException:
+                time.sleep(0.5)
+
+        schema_file_path.write_text(source.format(1234))
+
+        # It takes uvicorn some time to detect file changes
+        for _ in range(5):
+            try:
+                response = requests.post(url, json=query)
+                assert response.status_code == 200
+                assert response.json() == {"data": {"number": 1234}}
+            except AssertionError:
+                time.sleep(0.5)
+
+        os.killpg(proc.pid, signal.SIGKILL)
+        proc.communicate(timeout=10)
