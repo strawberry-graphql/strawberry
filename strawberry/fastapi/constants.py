b414bd13808686023d696f515c076149db41a9f6
Emil Madsen <emil@magenta.dk>
2021-08-22 13:56:12 +0200

    Restructure code

---
+++ strawberry/fastapi/constants.py
@@ -0,0 +1,13 @@
+NO_GRAPHIQL_MSG: str = "GraphiQL not enabled"
+GRAPHIQL_EXAMPLE: str = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Strawberry GraphiQL</title>
+    ...
+</head>
+<body>
+    ...
+</body>
+</html>
+"""
