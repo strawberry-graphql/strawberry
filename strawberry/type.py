585c4cc9ed3d29a227e92be0a41fd3fb12e354e5
Patrick Arminio <patrick.arminio@gmail.com>
2021-08-24 23:23:24 +0100

    Bad rebase

--- strawberry/type.py
+++ strawberry/type.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from __future__ import annotations
-=======
-import dataclasses
-from typing import List, Optional, Type, cast
->>>>>>> c8d5848 (Update inteface definition)
 
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, List, Mapping, TypeVar, Union
