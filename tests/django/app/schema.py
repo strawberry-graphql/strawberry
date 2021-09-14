49b0d05806c472b68db55323f8d918bd80e14b59
Etty <beltrami.ester@gmail.com>
2021-09-14 21:53:06 +0200

    Failing test

--- tests/django/app/schema.py
+++ tests/django/app/schema.py
@@ -2,11 +2,18 @@ import strawberry
 from strawberry.types.info import Info
 
 
+def function_resolver(root) -> str:
+    return "I'm a function resolver"
+
+
 @strawberry.type
 class Query:
     @strawberry.field
     def user(self, info: Info) -> str:
         return "🍓"
+
+    hello: str = "🍓"
+    hello_field: str = strawberry.field(resolver=function_resolver)
 
 
 schema = strawberry.Schema(query=Query)
--- tests/django/test_graphql_test_client.py
+++ tests/django/test_graphql_test_client.py
@@ -7,3 +7,25 @@ def test_query(graphql_client):
     result = graphql_client.query(query)
 
     assert result.data["user"] == "🍓"
+
+
+def test_hello(graphql_client):
+    query = """query {
+        hello
+    }
+    """
+
+    result = graphql_client.query(query)
+
+    assert result.data["hello"] == "🍓"
+
+
+def test_hello_field(graphql_client):
+    query = """query {
+        helloField
+    }
+    """
+
+    result = graphql_client.query(query)
+
+    assert result.data["helloField"] == "I'm a function resolver"
