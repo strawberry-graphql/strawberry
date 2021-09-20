d116afa61869075cbc59e47c3cf1df0eda11cd2c
Jonathan Kim <jkimbo@gmail.com>
2021-09-20 16:07:22 +0100

    Try something else

--- strawberry/schema/schema_converter.py
+++ strawberry/schema/schema_converter.py
@@ -390,6 +390,11 @@ class GraphQLCoreConverter:
                 _source, info=info, args=field_args, kwargs=field_kwargs
             )
 
+        def _get_simple_result(_source: Any, info: Info, **kwargs):
+            return field.get_result(
+                _source, info=None, args=[], kwargs={}
+            )
+
         def _resolver(_source: Any, info: GraphQLResolveInfo, **kwargs):
             strawberry_info = _strawberry_info_from_graphql(info)
             _check_permissions(_source, strawberry_info, kwargs)
@@ -402,12 +407,15 @@ class GraphQLCoreConverter:
 
             return await await_maybe(_get_result(_source, strawberry_info, **kwargs))
 
+        if not field.base_resolver and not field.permission_classes:
+            return _get_simple_result
+
         if field.is_async:
             _async_resolver._is_default = not field.base_resolver  # type: ignore
             return _async_resolver
-        else:
-            _resolver._is_default = not field.base_resolver  # type: ignore
-            return _resolver
+
+        _resolver._is_default = not field.base_resolver  # type: ignore
+        return _resolver
 
     def from_scalar(self, scalar: Type) -> GraphQLScalarType:
         scalar_definition: ScalarDefinition
