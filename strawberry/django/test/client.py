c109b5f4475991bb8f98f5d9473b94e5a2ecc955
Etty <beltrami.ester@gmail.com>
2021-10-02 19:58:08 +0100

    Use graphql_client to test upload

--- strawberry/django/test/client.py
+++ strawberry/django/test/client.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict, Optional
+import json
+from typing import Any, Dict, Literal, Optional
 
-from strawberry.test import BaseGraphQLTestClient, Body, Response
+from strawberry.test import BaseGraphQLTestClient, Response
 
 
 class GraphQLTestClient(BaseGraphQLTestClient):
@@ -10,16 +11,45 @@ class GraphQLTestClient(BaseGraphQLTestClient):
         variables: Optional[Dict[str, Any]] = None,
         headers: Optional[Dict[str, Any]] = None,
         asserts_errors: Optional[bool] = True,
+        format: Literal["multipart", "json"] = "json",
+        **kwargs,
     ) -> Response:
-        body: Body = {"query": query}
+        body: Any = {"query": query}
 
         if variables:
             body["variables"] = variables
 
-        resp = self._client.post(
-            "/graphql/", data=body, content_type="application/json", headers=headers
-        )
-        data = resp.json()
+        if format == "multipart":
+            ref_variable = next(iter(variables))
+            if isinstance(variables.get(ref_variable), dict):
+                ref_variable += f".{next(iter(variables[ref_variable]))}"
+
+            if len(kwargs) == 1:
+                file_map = json.dumps({ref_variable: [f"variables.{ref_variable}"]})
+            else:
+                file_map = json.dumps(
+                    {
+                        k: [f"variables.{ref_variable}.{index}"]
+                        for index, k in enumerate(kwargs)
+                    }
+                )
+
+            body = {
+                "operations": json.dumps(body),
+                "map": file_map,
+                **kwargs,
+            }
+
+            response = self._client.post(
+                "/graphql/", data=body, format="multipart", headers=headers
+            )
+            data = json.loads(response.content.decode())
+
+        elif format == "json":
+            resp = self._client.post(
+                "/graphql/", data=body, content_type="application/json", headers=headers
+            )
+            data = resp.json()
 
         response = Response(
             errors=data.get("errors"),
