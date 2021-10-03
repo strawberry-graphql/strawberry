a7724945f477d493fc712b4e0fbd16c4200891a6
Etty <beltrami.ester@gmail.com>
2021-10-03 19:50:12 +0100

    Add failling test

--- tests/django/test_upload.py
+++ tests/django/test_upload.py
@@ -48,3 +48,24 @@ def test_nested_file_list(graphql_client):
     assert len(response.data["readFolder"]) == 2
     assert response.data["readFolder"][0] == "strawberry1"
     assert response.data["readFolder"][1] == "strawberry2"
+
+
+def test_upload_single_and_list_file_together(graphql_client):
+    query = "mutation($files: [Upload!]!) { readFiles(files: $files) }"
+    file1 = SimpleUploadedFile("file1.txt", b"strawberry1")
+    file2 = SimpleUploadedFile("file2.txt", b"strawberry2")
+    file3 = SimpleUploadedFile("file3.txt", b"strawberry3")
+
+    response = graphql_client.query(
+        query=query,
+        variables={"files": [None, None], "file3": None},
+        format="multipart",
+        file1=file1,
+        file2=file2,
+        file3=file3,
+    )
+
+    assert len(response.data["readFiles"]) == 2
+    assert response.data["readFiles"][0] == "strawberry1"
+    assert response.data["readFiles"][1] == "strawberry2"
+    assert response.data["readFiles"][2] == "strawberry3"
