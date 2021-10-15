aea3ce2d2c36d613f829d7383465af006af42ce0
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2021-10-15 11:04:16 +0200

    told mypy to ignore missing attr

--- strawberry/tools/merge_types.py
+++ strawberry/tools/merge_types.py
@@ -23,7 +23,9 @@ def merge_types(name: str, types: Tuple[type, ...]) -> type:
     if not types:
         raise ValueError("Can't merge types if none are supplied")
 
-    fields = chain(*(t._type_definition.fields for t in types))
+    fields = chain(
+        *(t._type_definition.fields for t in types)  # type: ignore[attr-defined]
+    )
     counter = Counter(f.name for f in fields)
     dupes = [f for f, c in counter.most_common() if c > 1]
     if dupes:
