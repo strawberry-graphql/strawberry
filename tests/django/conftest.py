dcdb535be4206fbe3fa7ec3d7d4570b42bb8b4dc
Patrick Arminio <patrick.arminio@gmail.com>
2021-10-17 15:58:06 +0100

    Revert "Update tests/django/conftest.py"
    This reverts commit d33d16ae38bbbc2edf0a24a7088d76cb7e5c815c.

--- tests/django/conftest.py
+++ tests/django/conftest.py
@@ -11,5 +11,5 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
 
-        if rel_path.is_relative_to("tests/django"):
+        if str(rel_path).startswith("tests/django"):
             item.add_marker(pytest.mark.django)
