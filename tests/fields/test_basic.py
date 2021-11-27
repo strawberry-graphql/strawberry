22045e4844d00c769e940b7075caeb470f78576d
ignormies <bryce.beagle@gmail.com>
2021-11-27 15:43:17 -0800

    Add field name tests involving schema

--- tests/fields/test_basic.py
+++ tests/fields/test_basic.py
@@ -13,31 +13,9 @@ def test_type_add_type_definition_with_fields():
     assert len(definition.fields) == 2
 
     assert definition.fields[0].python_name == "name"
-    assert definition.fields[0].graphql_name is None
     assert definition.fields[0].type == str
 
     assert definition.fields[1].python_name == "age"
-    assert definition.fields[1].graphql_name is None
-    assert definition.fields[1].type == int
-
-
-def test_passing_custom_names_to_fields():
-    @strawberry.type
-    class Query:
-        x: str = strawberry.field(name="name")
-        y: int = strawberry.field(name="age")
-
-    definition = Query._type_definition
-
-    assert definition.name == "Query"
-    assert len(definition.fields) == 2
-
-    assert definition.fields[0].python_name == "x"
-    assert definition.fields[0].graphql_name == "name"
-    assert definition.fields[0].type == str
-
-    assert definition.fields[1].python_name == "y"
-    assert definition.fields[1].graphql_name == "age"
     assert definition.fields[1].type == int
 
 
@@ -53,11 +31,9 @@ def test_passing_nothing_to_fields():
     assert len(definition.fields) == 2
 
     assert definition.fields[0].python_name == "name"
-    assert definition.fields[0].graphql_name is None
     assert definition.fields[0].type == str
 
     assert definition.fields[1].python_name == "age"
-    assert definition.fields[1].graphql_name is None
     assert definition.fields[1].type == int
 
 
@@ -73,14 +49,3 @@ def test_can_use_types_directly():
     user = User(username="abc")
     assert user.username == "abc"
     assert user.email() == "abc@somesite.com"
-
-
-def test_graphql_name_unchanged():
-    @strawberry.type
-    class Query:
-        the_field: int = strawberry.field(name="some_name")
-
-    definition = Query._type_definition
-
-    assert definition.fields[0].python_name == "the_field"
-    assert definition.fields[0].graphql_name == "some_name"
--- tests/fields/test_field_names.py
+++ tests/fields/test_field_names.py
@@ -1,6 +1,3 @@
-"""These tests cover the functionality of StrawberryField names before they're added
-to the Schema"""
-
 import strawberry
 
 
@@ -11,6 +8,17 @@ def test_field_name_standard():
     assert standard_field.graphql_name is None
 
 
+def test_field_name_standard_on_schema():
+    @strawberry.type()
+    class Query:
+        normal_field: int
+
+    [field] = Query._type_definition.fields
+
+    assert field.python_name == "normal_field"
+    assert field.graphql_name is None
+
+
 def test_field_name_override():
     field_name = "override"
 
@@ -18,3 +26,16 @@ def test_field_name_override():
 
     assert standard_field.python_name is None  # Set once field is added to a Schema
     assert standard_field.graphql_name == field_name
+
+
+def test_field_name_override_with_schema():
+    field_name = "override_name"
+
+    @strawberry.type()
+    class Query:
+        override_field: bool = strawberry.field(name=field_name)
+
+    [field] = Query._type_definition.fields
+
+    assert field.python_name == "override_field"
+    assert field.graphql_name == field_name
