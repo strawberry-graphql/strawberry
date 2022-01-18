c1b12ae31d845d854b633bb374029315d2bc543a
Rishi Kumar Ray <87641376+RishiKumarRay@users.noreply.github.com>
2022-01-18 16:57:34 +0530

    Updated field.py

--- strawberry/field.py
+++ strawberry/field.py
@@ -132,7 +132,7 @@ class StrawberryField(dataclasses.Field):
                     "Union",
                 )
             elif getattr(argument.type, "_type_definition", False):
-                if argument.type._type_definition.is_interface:
+                if argument.type._type_definition.is_interface:  # type: ignore
                     raise InvalidFieldArgument(
                         resolver.name,
                         argument.python_name,
