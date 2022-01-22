e54827cfa212e5bb9fb110167c3fc8b13f32cf10
James Chua <james@leadiq.com>
2022-01-22 23:21:20 +0800

    fix(review): Move StrawberryTypeFromPydantic to new file. Add example to RELEASE.md.

--- RELEASE.md
+++ RELEASE.md
@@ -2,4 +2,19 @@ Release type: minor
 
 Improves the following for types converted from pydantic BaseModel.
 Adds mypy extension support for `to_pydantic` and `from_pydantic` methods.
-Adds typing stubs for IDE support.
+Adds type hints for IDE support.
+
+```python
+from pydantic import BaseModel
+import strawberry
+
+class UserPydantic(BaseModel):
+    age: int
+
+@strawberry.experimental.pydantic.type(UserPydantic)
+class UserStrawberry:
+    age: strawberry.auto
+
+reveal_type(UserStrawberry(age=123).to_pydantic())
+```
+Mypy will infer the type as "UserPydantic". Previously it would be `Any`.
--- strawberry/experimental/pydantic/object_type.py
+++ strawberry/experimental/pydantic/object_type.py
@@ -13,13 +13,12 @@ from typing import (
     Optional,
     Sequence,
     Type,
-    TypeVar,
     cast,
 )
 
 from pydantic import BaseModel
 from pydantic.fields import ModelField
-from typing_extensions import Literal, Protocol
+from typing_extensions import Literal
 
 from graphql import GraphQLResolveInfo
 
@@ -87,31 +86,10 @@ def get_type_for_field(field: ModelField):
 
 
 if TYPE_CHECKING:
-    PydanticModel = TypeVar("PydanticModel", bound=BaseModel)
-
-    class StrawberryTypeFromPydantic(Protocol[PydanticModel]):
-        """This class does not exist in runtime.
-        It only makes the methods below visible for IDEs"""
-
-        def __init__(self, **kwargs):
-            ...
-
-        @staticmethod
-        def from_pydantic(
-            instance: PydanticModel, extra: Dict[str, Any] = None
-        ) -> PydanticModel:
-            ...
-
-        def to_pydantic(self) -> PydanticModel:
-            ...
-
-        @property
-        def _type_definition(self) -> TypeDefinition:
-            ...
-
-        @property
-        def _pydantic_type(self) -> PydanticModel:
-            ...
+    from strawberry.experimental.pydantic.conversion_types import (
+        PydanticModel,
+        StrawberryTypeFromPydantic,
+    )
 
 
 def type(
