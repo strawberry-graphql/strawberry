b212989053c95ce437bfedefa42866d4c337d7b0
Etty <beltrami.ester@gmail.com>
2022-01-30 22:19:22 +0000

    Add GraphQLTestClient for aiohttp

--- tests/aiohttp/conftest.py
+++ tests/aiohttp/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from strawberry.aiohttp.test.client import GraphQLTestClient
 from tests.aiohttp.app import create_app
 
 
@@ -8,3 +9,8 @@ def aiohttp_app_client(loop, aiohttp_client):
     app = create_app(graphiql=True)
     loop.set_debug(True)
     return loop.run_until_complete(aiohttp_client(app))
+
+
+@pytest.fixture
+def graphql_client(aiohttp_app_client):
+    yield GraphQLTestClient(aiohttp_app_client)
