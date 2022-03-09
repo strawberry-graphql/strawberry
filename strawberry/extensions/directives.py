2ef9c60cf3f88d5499821f1a55839b6dcd2c1fea
skilkis <sankilkis@msn.com>
2022-03-09 15:40:10 +0100

    Improve reserved argument injection for resolvers
    This commit closes Issue #1666 by improving the injection of reserved
    arguments by providing an opt-in functionality to match reserved
    arguments by type-annotations. The functionality is added to
    `StrawberryResolver`s which is then reused in `StrawberryDirectives`s
    via a new `StrawberryDirectiveResolver` class. Additionally, A new
    `DirectiveValue` `Annotated` type is defined to allow more flexibility
    when defining directive resolvers. To ensure backwards-compatibility, a
    fallback to argument name-based matching for reserved parameters is used
    for `info` and `value` arguments. Overally, the new `ReservedType`
    specification allows arbitrary names for `info` and `value` arguments,
    provided that they are annotated as `<my_info_name>: Info` and
    `<my_value_name>: DirectiveValue[Any]` respectively.
    
    The new injection functionality is enabled by a
    `ReservedParameterSpecification` `Protocol` which is used to define
    various types of reserved parameters. Each specification is responsible
    for iterating over the `parameters` of the wrapped resolver to `find`
    its targeted reserved parameter. This information is then stored in a
    mapping of specification to reserved parameter. Later, this mapping is
    used to determine which reserved keyword argument(s) to pass to the
    resolver during execution.
    
    Summary of Changes:
    - Improve reserved argument injection for resolvers by providing an
      opt-in functionality to match reserved arguments by type-annotations.
    - Re-use filtering logic of `StrawberryResolver` by defining a
      specialized `StrawberryDirectiveResolver` class
    - Support passing `info` and `root` to a `StrawberryDirective`
    - Deprecate use of bare `info` and `value` arguments to resolvers
    - Allow definition of arbitrarily named keyword arguments to represent
      `info`, and `value` reserved arguments
    - Modify @magicmark's failing test to check if a directive without value
      argument correctly recieves its arguments
    - Add test to verify new reserved argument injection logic

--- strawberry/extensions/directives.py
+++ strawberry/extensions/directives.py
@@ -1,5 +1,8 @@
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Dict, Tuple
 
+from graphql import DirectiveNode
+
+from strawberry.directive import StrawberryDirective
 from strawberry.extensions import Extension
 from strawberry.types import Info
 from strawberry.utils.await_maybe import AwaitableOrValue, await_maybe
@@ -16,56 +19,60 @@ class DirectivesExtension(Extension):
     async def resolve(
         self, _next, root, info: Info, *args, **kwargs
     ) -> AwaitableOrValue[Any]:
-        result = await await_maybe(_next(root, info, *args, **kwargs))
+        value = await await_maybe(_next(root, info, *args, **kwargs))
 
         for directive in info.field_nodes[0].directives:
-            directive_name = directive.name.value
-
-            if directive_name in SPECIFIED_DIRECTIVES:
+            if directive.name.value in SPECIFIED_DIRECTIVES:
                 continue
-
-            # TODO: support converting lists
-            arguments = {
-                argument.name.value: argument.value.value  # type: ignore
-                for argument in directive.arguments
-            }
-
-            schema: Schema = info.schema._strawberry_schema  # type: ignore
-            strawberry_directive = schema.get_directive_by_name(directive_name)
-            assert (
-                strawberry_directive is not None
-            ), f"Directive {directive_name} not found"
-
-            result = await await_maybe(
-                strawberry_directive.resolver(result, **arguments)
+            strawberry_directive, arguments = process_directive(
+                directive, value, root, info
             )
+            value = await await_maybe(strawberry_directive.resolver(**arguments))
 
-        return result
+        return value
 
 
 class DirectivesExtensionSync(Extension):
-    # TODO: we might need the graphql info here
     def resolve(self, _next, root, info, *args, **kwargs) -> AwaitableOrValue[Any]:
-        result = _next(root, info, *args, **kwargs)
+        value = _next(root, info, *args, **kwargs)
 
         for directive in info.field_nodes[0].directives:
-            directive_name = directive.name.value
-
-            if directive_name in SPECIFIED_DIRECTIVES:
+            if directive.name.value in SPECIFIED_DIRECTIVES:
                 continue
+            strawberry_directive, arguments = process_directive(
+                directive, value, root, info
+            )
+            value = strawberry_directive.resolver(**arguments)
 
-            # TODO: support converting lists
-            arguments = {
-                argument.name.value: argument.value.value
-                for argument in directive.arguments
-            }
+        return value
 
-            schema: Schema = info.schema._strawberry_schema
-            strawberry_directive = schema.get_directive_by_name(directive_name)
-            assert (
-                strawberry_directive is not None
-            ), f"Directive {directive_name} not found"
 
-            result = strawberry_directive.resolver(result, **arguments)
+def process_directive(
+    directive: DirectiveNode,
+    value: Any,
+    root: Any,
+    info: Info,
+) -> Tuple[StrawberryDirective, Dict[str, Any]]:
+    """Get a `StrawberryDirective` from ``directive` and prepare its arguments."""
+    directive_name = directive.name.value
+    schema: Schema = info.schema._strawberry_schema  # type: ignore
 
-        return result
+    strawberry_directive = schema.get_directive_by_name(directive_name)
+    assert strawberry_directive is not None, f"Directive {directive_name} not found"
+
+    arguments = {
+        argument.name.value: argument.value.value  # type: ignore
+        for argument in directive.arguments
+    }
+    resolver = strawberry_directive.resolver
+
+    info_parameter = resolver.info_parameter
+    root_parameter = resolver.root_parameter
+    value_parameter = resolver.value_parameter
+    if info_parameter:
+        arguments[info_parameter.name] = info
+    if root_parameter:
+        arguments[root_parameter.name] = root
+    if value_parameter:
+        arguments[value_parameter.name] = value
+    return strawberry_directive, arguments
