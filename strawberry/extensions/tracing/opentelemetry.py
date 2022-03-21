4069637935d6a5bad4745e029e64c4629c6ee2bd
peach lasagna <kir.kud@inbox.ru>
2022-03-21 16:28:33 +0700

    try one

--- strawberry/extensions/tracing/opentelemetry.py
+++ strawberry/extensions/tracing/opentelemetry.py
@@ -3,8 +3,6 @@ from copy import deepcopy
 from inspect import isawaitable
 from typing import Any, Callable, Dict, Optional
 
-from opentelemetry import trace
-from opentelemetry.trace import Span, SpanKind, Tracer
 
 from graphql import GraphQLResolveInfo
 
@@ -14,6 +12,12 @@ from strawberry.types.execution import ExecutionContext
 
 from .utils import should_skip_tracing
 
+is_opentelemetry_installed =True
+try:
+    from opentelemetry import trace
+    from opentelemetry.trace import Span, SpanKind, Tracer
+except ModuleNotFoundError:
+    is_opentelemetry_installed = False
 
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
@@ -145,3 +149,10 @@ class OpenTelemetryExtensionSync(OpenTelemetryExtension):
             result = _next(root, info, *args, **kwargs)
 
             return result
+
+if not is_opentelemetry_installed:
+    class OpenTelemetryExtension(Extension):
+        def __init__(self, *args, **kwargs):
+            raise ImportError('To use OpenTelemetryExtension install opentelemetry')
+    class OpenTelemetryExtensionSync(OpenTelemetryExtension):
+        pass
