88889346c6bcaec32dbf17ce190796f4cf435de9
skilkis <sankilkis@msn.com>
2022-03-28 14:43:07 +0200

    Add test to demonstrate Issue #1769

--- tests/schema/test_subscription.py
+++ tests/schema/test_subscription.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import sys
 import typing
 
 import pytest
@@ -49,3 +52,47 @@ async def test_subscription_with_arguments():
 
     assert not result.errors
     assert result.data["example"] == "Hi Nina"
+
+
+requires_builtin_generics = pytest.mark.skipif(
+    sys.version_info < (3, 9),
+    reason="built-in generic annotations where added in python 3.9",
+)
+
+
+@pytest.mark.parametrize(
+    "return_annotation",
+    (
+        "typing.AsyncGenerator[str, None]",
+        "typing.AsyncIterable[str]",
+        "typing.AsyncIterator[str]",
+        pytest.param("abc.AsyncIterator[str]", marks=requires_builtin_generics),
+        pytest.param("abc.AsyncGenerator[str, None]", marks=requires_builtin_generics),
+        pytest.param("abc.AsyncIterable[str]", marks=requires_builtin_generics),
+    ),
+)
+@pytest.mark.asyncio
+async def test_subscription_return_typing_annotations(return_annotation: str):
+    async def async_resolver():
+        yield "Hi"
+
+    async_resolver.__annotations__["return"] = return_annotation
+
+    @strawberry.type
+    class Query:
+        x: str = "Hello"
+
+    @strawberry.type
+    class Subscription:
+
+        example = strawberry.subscription(resolver=async_resolver)
+
+    schema = strawberry.Schema(query=Query, subscription=Subscription)
+
+    query = "subscription { example }"
+
+    sub = await schema.subscribe(query)
+    result = await sub.__anext__()
+
+    assert not result.errors
+    assert result.data["example"] == "Hi"
