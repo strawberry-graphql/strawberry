009f569457f8fa1ef00fbfab8007d6839ee40443
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-05-29 14:20:14 -0300

    feat: Add channels layer typing protocol

--- strawberry/channels/handlers/base.py
+++ strawberry/channels/handlers/base.py
@@ -1,12 +1,55 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional, Protocol
+
+from typing_extensions import Literal
 
 from channels.consumer import AsyncConsumer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from strawberry.channels.context import StrawberryChannelsContext
 
 
+class ChannelsLayer(Protocol):
+    """Channels layer spec.
+
+    Based on: https://channels.readthedocs.io/en/stable/channel_layer_spec.html
+    """
+
+    # Default channels API
+
+    extensions: List[Literal["groups", "flush"]]
+
+    async def send(self, channel: str, message: dict) -> None:
+        ...
+
+    async def receive(self, channel: str) -> dict:
+        ...
+
+    async def new_channel(self, prefix: str = ...) -> str:
+        ...
+
+    # If groups extension is supported
+
+    group_expiry: int
+
+    async def group_add(self, group: str, channel: str) -> None:
+        ...
+
+    async def group_discard(self, group: str, channel: str) -> None:
+        ...
+
+    async def group_send(self, group: str, message: dict) -> None:
+        ...
+
+    # If flush extension is supported
+
+    async def flush(self) -> None:
+        ...
+
+
 class ChannelsConsumer(AsyncConsumer):
     """Base channels async consumer."""
+
+    channel_name: str
+    channel_layer: ChannelsLayer
 
     @property
     def headers(self) -> Dict[str, str]:
