47d3e1e058534b63672242b387eabb7be537c4a1
Patrick Arminio <patrick.arminio@gmail.com>
2022-06-29 21:41:13 +0100

    Make tracing imports lazy

---
+++ RELEASE.md
@@ -0,0 +1,4 @@
+Release type: minor
+
+This release fixes an issue that required installing opentelemetry when
+trying to use the ApolloTracing extension
--- strawberry/extensions/tracing/__init__.py
+++ strawberry/extensions/tracing/__init__.py
@@ -1,2 +1,27 @@
-from .apollo import ApolloTracingExtension, ApolloTracingExtensionSync  # noqa
-from .opentelemetry import OpenTelemetryExtension, OpenTelemetryExtensionSync  # noqa
+import importlib
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from . import apollo, opentelemetry
+    from .apollo import ApolloTracingExtension, ApolloTracingExtensionSync  # noqa
+    from .opentelemetry import (  # noqa
+        OpenTelemetryExtension,
+        OpenTelemetryExtensionSync,
+    )
+
+__all__ = [
+    "opentelemetry",
+    "apollo",
+    "ApolloTracingExtension",
+    "ApolloTracingExtensionSync",
+    "OpenTelemetryExtension",
+    "OpenTelemetryExtensionSync",
+]
+
+
+def __getattr__(name):
+    if name in __all__:
+        return importlib.import_module(f".{name}", __name__)
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
