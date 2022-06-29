bd9117a608a49a3c7873c7a6b8d73e575aeebaf1
Patrick Arminio <patrick.arminio@gmail.com>
2022-06-29 22:52:31 +0100

    Add support for direct imports

--- strawberry/extensions/tracing/__init__.py
+++ strawberry/extensions/tracing/__init__.py
@@ -21,7 +21,13 @@ __all__ = [
 
 
 def __getattr__(name):
-    if name in __all__:
+    if name in {"apollo", "opentelemetry"}:
         return importlib.import_module(f".{name}", __name__)
+
+    if name in {"ApolloTracingExtension", "ApolloTracingExtensionSync"}:
+        return getattr(importlib.import_module("apollo", __name__), name)
+
+    if name in {"OpenTelemetryExtension", "OpenTelemetryExtensionSync"}:
+        return getattr(importlib.import_module("opentelemetry", __name__), name)
 
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
