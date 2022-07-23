62676a38f9612e10e51ee6984b818bd37528c69a
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-23 18:05:10 +0100

    Add support for custom default resolvers (#2037)
    * Add support for custom default resolvers
    
    * Fix typo in release notes

--- strawberry/schema/config.py
+++ strawberry/schema/config.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 from dataclasses import InitVar, dataclass, field
+from typing import Any, Callable
 
 from .name_converter import NameConverter
 
 
 @dataclass
 class StrawberryConfig:
-    auto_camel_case: InitVar[bool] = None
+    auto_camel_case: InitVar[bool] = None  # pyright: reportGeneralTypeIssues=false
     name_converter: NameConverter = field(default_factory=NameConverter)
+    default_resolver: Callable[[Any, str], object] = getattr
 
     def __post_init__(
         self,
