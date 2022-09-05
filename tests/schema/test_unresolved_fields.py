b89dd1e1257db74ea42b7fc129999e7677d1e435
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-05 16:00:44 +0100

    add one more test

--- tests/schema/test_unresolved_fields.py
+++ tests/schema/test_unresolved_fields.py
@@ -6,11 +6,31 @@ from strawberry.exceptions.unresolved_field_type import UnresolvedFieldTypeError
 
 @pytest.mark.raises_strawberry_exception(
     UnresolvedFieldTypeError,
-    message="strawberry.enum can only be used with subclasses of Enum. ",
+    match=(
+        "Could not resolve the type of 'user'. Check that "
+        "the class is accessible from the global module scope."
+    ),
 )
 def test_unresolved_field_fails():
     @strawberry.type
     class Query:
         user: "User"  # type: ignore  # noqa: F821
+
+    strawberry.Schema(query=Query)
+
+
+@pytest.mark.raises_strawberry_exception(
+    UnresolvedFieldTypeError,
+    match=(
+        "Could not resolve the type of 'user'. Check that "
+        "the class is accessible from the global module scope."
+    ),
+)
+def test_unresolved_field_with_resolver_fails():
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        def user(self) -> "User":  # type: ignore  # noqa: F821
+            ...
 
     strawberry.Schema(query=Query)
