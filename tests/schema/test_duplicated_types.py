be3c6b5deea1bb810e80b65dc3ddcec175d293ae
Patrick Arminio <patrick.arminio@gmail.com>
2022-12-02 11:58:51 +0000

    Fix duplicated types error when using generics

--- tests/schema/test_duplicated_types.py
+++ tests/schema/test_duplicated_types.py
@@ -1,4 +1,6 @@
+import textwrap
 from enum import Enum
+from typing import Generic, TypeVar
 
 import pytest
 
@@ -108,3 +110,33 @@ def test_schema_has_no_duplicated_types_between_schema_and_extra_types():
         strawberry.Schema(query=Query, types=[B])
 
     assert isinstance(exc_info.value.__cause__, DuplicatedTypeName)
+
+
+def test_allows_multiple_instance_of_same_generic():
+    T = TypeVar("T")
+
+    @strawberry.type
+    class A(Generic[T]):
+        a: T
+
+    @strawberry.type
+    class Query:
+        first: A[int]
+        second: A[int]
+
+    schema = strawberry.Schema(Query)
+
+    expected_schema = textwrap.dedent(
+        """
+        type IntA {
+          a: Int!
+        }
+
+        type Query {
+          first: IntA!
+          second: IntA!
+        }
+        """
+    ).strip()
+
+    assert str(schema) == expected_schema
