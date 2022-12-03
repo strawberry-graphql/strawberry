abdd77f3821fb71d79327836c12094b22d44cab1
Tommy Smith <tommy.smith023@gmail.com>
2022-12-03 10:21:34 +0000

    fix BaseContext exporting

--- strawberry/fastapi/__init__.py
+++ strawberry/fastapi/__init__.py
@@ -1,4 +1,5 @@
-from strawberry.fastapi.router import BaseContext, GraphQLRouter
+from strawberry.fastapi.context import BaseContext
+from strawberry.fastapi.router import GraphQLRouter
 
 
 __all__ = ["BaseContext", "GraphQLRouter"]
