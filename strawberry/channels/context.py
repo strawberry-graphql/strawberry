ff7503c95aac6603a28f4671928dc18a48e0b5fc
Tommy Smith <tommy.smith023@gmail.com>
2022-12-03 09:05:58 +0000

    add connection_params to channels custom context

--- strawberry/channels/context.py
+++ strawberry/channels/context.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
@@ -11,7 +11,7 @@ class StrawberryChannelsContext:
     """
     A Channels context for GraphQL
     """
-
+    connection_params: Any
     request: "ChannelsConsumer"
 
     @property
