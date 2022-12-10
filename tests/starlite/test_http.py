30c50f4f7a7033588c01f198d28b7923eb70a694
gazorby <gazorby@pm.me>
2022-12-10 03:06:09 +0100

    ✅ test(starlite): remove duplicated tests

--- tests/starlite/test_http.py
+++ tests/starlite/test_http.py
@@ -1,5 +1,3 @@
-import pytest
-
 from starlite import status_codes
 
 
@@ -13,10 +11,3 @@ def test_returns_error_when_not_sending_wrong_content_type(test_client):
     response = test_client.post("/graphql", data="Example")
 
     assert response.status_code == status_codes.HTTP_415_UNSUPPORTED_MEDIA_TYPE
-
-
-@pytest.mark.parametrize("method", ("PUT", "DELETE"))
-def test_returns_error_when_method_is_not_allowed(method, test_client):
-    response = test_client.request(method, "/graphql", json={})
-
-    assert response.status_code == status_codes.HTTP_405_METHOD_NOT_ALLOWED
