cc23d50b3b076444a7f3efc6f35a572187dec0d1
gazorby <gazorby@pm.me>
2022-12-10 16:25:11 +0100

    ✅ test(starlite): move more tests to tests/http

---
+++ tests/starlite/test_websockets.py
@@ -0,0 +1,77 @@
+import pytest
+from tests.starlite.app import create_app
+
+from starlite.exceptions import WebSocketDisconnect
+from starlite.testing import TestClient
+from strawberry.subscriptions import GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL
+
+
+def test_turning_off_graphql_ws():
+    app = create_app(subscription_protocols=[GRAPHQL_TRANSPORT_WS_PROTOCOL])
+    test_client = TestClient(app)
+
+    with pytest.raises(WebSocketDisconnect) as exc:
+        with test_client.websocket_connect("/graphql/ws", [GRAPHQL_WS_PROTOCOL]):
+            pass
+
+    assert exc.value.code == 4406
+
+
+def test_turning_off_graphql_transport_ws():
+    app = create_app(subscription_protocols=[GRAPHQL_WS_PROTOCOL])
+    test_client = TestClient(app)
+
+    with pytest.raises(WebSocketDisconnect) as exc:
+        with test_client.websocket_connect(
+            "/graphql/ws", [GRAPHQL_TRANSPORT_WS_PROTOCOL]
+        ):
+            pass
+
+    assert exc.value.code == 4406
+
+
+def test_turning_off_all_ws_protocols():
+    app = create_app(subscription_protocols=[])
+    test_client = TestClient(app)
+
+    with pytest.raises(WebSocketDisconnect) as exc:
+        with test_client.websocket_connect(
+            "/graphql/ws", [GRAPHQL_TRANSPORT_WS_PROTOCOL]
+        ):
+            pass
+
+    assert exc.value.code == 4406
+
+    with pytest.raises(WebSocketDisconnect) as exc:
+        with test_client.websocket_connect("/graphql/ws", [GRAPHQL_WS_PROTOCOL]):
+            pass
+
+    assert exc.value.code == 4406
+
+
+def test_unsupported_ws_protocol():
+    app = create_app(subscription_protocols=[])
+    test_client = TestClient(app)
+
+    with pytest.raises(WebSocketDisconnect) as exc:
+        with test_client.websocket_connect("/graphql/ws", ["imaginary-protocol"]):
+            pass
+
+    assert exc.value.code == 4406
+
+
+def test_clients_can_prefer_protocols():
+    app = create_app(
+        subscription_protocols=[GRAPHQL_WS_PROTOCOL, GRAPHQL_TRANSPORT_WS_PROTOCOL]
+    )
+    test_client = TestClient(app)
+
+    with test_client.websocket_connect(
+        "/graphql/ws", [GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL]
+    ) as ws:
+        assert ws.accepted_subprotocol == GRAPHQL_TRANSPORT_WS_PROTOCOL
+
+    with test_client.websocket_connect(
+        "/graphql/ws", [GRAPHQL_WS_PROTOCOL, GRAPHQL_TRANSPORT_WS_PROTOCOL]
+    ) as ws:
+        assert ws.accepted_subprotocol == GRAPHQL_WS_PROTOCOL
