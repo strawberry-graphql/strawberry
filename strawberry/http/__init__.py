b89c8259134b4652986f2d67d4106e9c841357ce
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-10 12:49:19 +0000

    Add comment about query being optional

--- strawberry/http/__init__.py
+++ strawberry/http/__init__.py
@@ -41,6 +41,8 @@ def parse_query_params(params: Dict[str, str]) -> Dict[str, Any]:
 
 def parse_request_data(data: Mapping[str, Any]) -> GraphQLRequestData:
     return GraphQLRequestData(
+        # query is optional here as it can be added by an extensions
+        # (for example an extension for persisted queries)
         query=data.get("query", ""),
         variables=data.get("variables"),
         operation_name=data.get("operationName"),
