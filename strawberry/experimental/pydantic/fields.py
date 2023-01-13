6ff2de13aad0f889091ba0de027a1b121b03d33f
Etienne Wodey <44871469+airwoodix@users.noreply.github.com>
2023-01-13 16:37:29 +0100

    pydantic: support confloat fields (#2455)

--- strawberry/experimental/pydantic/fields.py
+++ strawberry/experimental/pydantic/fields.py
@@ -76,6 +76,8 @@ FIELDS_MAP = {
 def get_basic_type(type_) -> Type[Any]:
     if lenient_issubclass(type_, pydantic.ConstrainedInt):
         return int
+    if lenient_issubclass(type_, pydantic.ConstrainedFloat):
+        return float
     if lenient_issubclass(type_, pydantic.ConstrainedStr):
         return str
     if lenient_issubclass(type_, pydantic.ConstrainedList):
