7c9ebaa20c26e49d59fce48580bc8490a39b91b3
skilkis <sankilkis@msn.com>
2023-01-17 05:58:49 +0100

    Fix duplicated type name on generics
    This commit addresses #2379 by implementing additional logic to resolve
    `strawberry.lazy` annotated types. Previously, if the same return type
    was declared in one location using `strawberry.lazy` and in another
    location without, then a DuplicatedTypeName exception would be raised.
    
    Summary of Changes:
    - Added logic to handle StrawberryOptional in `schema_converter.py`
    - Added check to determine if `strawberry.lazy` refers to the
      `__main__` module to prevent an import leading to instantiation
      of new module members.

--- tests/schema/test_lazy/test_lazy_generic.py
+++ tests/schema/test_lazy/test_lazy_generic.py
@@ -1,6 +1,11 @@
+import os
+import subprocess
+import sys
 import textwrap
-from typing import TYPE_CHECKING, Generic, TypeVar
+from typing import TYPE_CHECKING, Generic, List, Optional, Sequence, TypeVar
 from typing_extensions import Annotated
+
+import pytest
 
 import strawberry
 
@@ -60,6 +65,63 @@ def test_no_generic_type_duplication_with_lazy():
 
         type TypeBEdge {
           node: TypeB!
+        }
+        """
+    ).strip()
+
+    assert str(schema) == expected_schema
+
+
+@pytest.mark.parametrize(
+    "commands",
+    [
+        pytest.param(["tests/schema/test_lazy/type_c.py"], id="script"),
+        pytest.param(["-m", "tests.schema.test_lazy.type_c"], id="module"),
+    ],
+)
+def test_lazy_types_loaded_from_same_module(commands: Sequence[str]):
+    """Test if lazy types resolved from the same module produce duplication error.
+
+    Note:
+      `subprocess` is used since the test must be run as the main module / script.
+    """
+    result = subprocess.run(
+        args=[sys.executable, *commands],
+        env=os.environ,
+        capture_output=True,
+    )
+    result.check_returncode()
+
+
+def test_lazy_types_declared_within_optional():
+    from tests.schema.test_lazy.type_c import Edge, TypeC
+
+    @strawberry.type
+    class Query:
+
+        normal_edges: List[Edge[Optional[TypeC]]]
+        lazy_edges: List[
+            Edge[
+                Optional[
+                    Annotated["TypeC", strawberry.lazy("tests.schema.test_lazy.type_c")]
+                ]
+            ]
+        ]
+
+    schema = strawberry.Schema(query=Query)
+    expected_schema = textwrap.dedent(
+        """
+        type Query {
+          normalEdges: [TypeCOptionalEdge!]!
+          lazyEdges: [TypeCOptionalEdge!]!
+        }
+
+        type TypeC {
+          name: String!
+        }
+
+        type TypeCOptionalEdge {
+          node: TypeC
         }
         """
     ).strip()
