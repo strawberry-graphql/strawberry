d457acf2225500f061628f56bb26fe7119c215e0
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-18 12:32:44 +0000

    Update strawberry/subscriptions/__init__.py
    Co-authored-by: Jonathan Ehwald <github@ehwald.info>

--- strawberry/subscriptions/__init__.py
+++ strawberry/subscriptions/__init__.py
@@ -3,7 +3,7 @@ from typing_extensions import Literal
 GRAPHQL_TRANSPORT_WS_PROTOCOL = "graphql-transport-ws"
 GRAPHQL_WS_PROTOCOL = "graphql-ws"
 
-SubscriptionProtocolType = Literal["graphql-transport-ws", "graphql-ws"]
+SubscriptionProtocolType = Literal[GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL]
 
 # Code 4406 is "Subprotocol not acceptable"
 WS_4406_PROTOCOL_NOT_ACCEPTABLE = 4406
