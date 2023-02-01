1f9c020fab775cd2c75b6a372554f79ea965d1ad
Tommy Smith <tommy.smith023@gmail.com>
2023-02-01 17:58:30 +0000

    add tests to cover new class

--- strawberry/extensions/__init__.py
+++ strawberry/extensions/__init__.py
@@ -3,13 +3,14 @@ from .base_extension import Extension
 from .disable_validation import DisableValidation
 from .mask_errors import MaskErrors
 from .parser_cache import ParserCache
-from .query_depth_limiter import QueryDepthLimiter
+from .query_depth_limiter import FieldAttributesRule, QueryDepthLimiter
 from .validation_cache import ValidationCache
 
 __all__ = [
     "Extension",
     "AddValidationRules",
     "DisableValidation",
+    "FieldAttributesRule",
     "ParserCache",
     "QueryDepthLimiter",
     "ValidationCache",
