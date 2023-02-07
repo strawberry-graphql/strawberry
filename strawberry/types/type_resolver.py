57721ce1097f225aa96e7d9c6dc57e2e4db12f32
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-02-07 13:13:25 -0300

    chore: improve type_resolver comments

--- strawberry/types/type_resolver.py
+++ strawberry/types/type_resolver.py
@@ -139,13 +139,14 @@ def _get_fields(cls: Type) -> List[StrawberryField]:
             origin = origins.get(field.name, cls)
             module = sys.modules[origin.__module__]
 
-            # If field_type is specialized, copy its type_var_map to the field
             if isinstance(field_type, TypeVar):
                 specialized_type_var_map = get_specialized_type_var_map(cls)
+                # If field_type is specialized and a TypeVar, replace it with its
                 if specialized_type_var_map and field_type in specialized_type_var_map:
                     field_type = specialized_type_var_map[field_type]
             else:
                 specialized_type_var_map = get_specialized_type_var_map(field_type)
+                # If field_type is specialized, copy its type_var_map to the definition
                 if specialized_type_var_map:
                     field_type = field_type._type_definition.copy_with(
                         specialized_type_var_map
