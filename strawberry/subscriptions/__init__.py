4265b75b083456b83d5c54a3d9e1725073058bc9
gazorby <gazorby@pm.me>
2023-02-13 15:38:13 +0100

    ♻️ refactor: drop SubscriptionProtocolType literal

--- strawberry/subscriptions/__init__.py
+++ strawberry/subscriptions/__init__.py
@@ -1,8 +1,2 @@
-from typing_extensions import Literal
-
 GRAPHQL_TRANSPORT_WS_PROTOCOL = "graphql-transport-ws"
 GRAPHQL_WS_PROTOCOL = "graphql-ws"
-
-SubscriptionProtocolType = Literal[  #  type: ignore
-    GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL  #  type: ignore
-]
