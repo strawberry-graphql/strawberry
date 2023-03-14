3aa649e0b9433b2fc96a2498f5bb6abe26440a78
ניר <88795475+nrbnlulu@users.noreply.github.com>
2023-03-15 01:45:53 +0200

    better error on union of `StrawberryList` (#2593)
    * nit: better error on union of `StrawberryList`
    
    * fix mypy
    
    * fix =<py3.8
    
    * fix =<py3.8
    
    * Update RELEASE.md
    
    Co-authored-by: Thiago Bellini Ribeiro <hackedbellini@gmail.com>
    
    * Update RELEASE.md
    
    Co-authored-by: Thiago Bellini Ribeiro <hackedbellini@gmail.com>
    
    * pytest.skip
    
    * fix RELEASE.md
    
    * Update test
    
    ---------
    
    Co-authored-by: Thiago Bellini Ribeiro <hackedbellini@gmail.com>
    Co-authored-by: Patrick Arminio <patrick.arminio@gmail.com>

---
+++ RELEASE.md
@@ -0,0 +1,7 @@
+Release type: patch
+
+This release covers an edge case where the following would not give a nice error.
+```python
+some_field: "Union[list[str], SomeType]]"
+```
+Fixes [#2591](https://github.com/strawberry-graphql/strawberry/issues/2591)
--- tests/schema/test_union.py
+++ tests/schema/test_union.py
@@ -662,3 +662,37 @@ def test_error_with_invalid_annotated_type():
         union: Union[Something, AnnotatedInt]
 
     strawberry.Schema(query=Query)
+
+
+@pytest.mark.raises_strawberry_exception(
+    InvalidUnionTypeError, match="Type `(.*)` cannot be used in a GraphQL Union"
+)
+@pytest.mark.parametrize(
+    "annotation",
+    (
+        "int",
+        "str",
+        "float",
+        pytest.param(
+            "list[str]",
+            marks=pytest.mark.skipif(
+                sys.version_info < (3, 9, 0),
+                reason="list[str] is only available on python 3.9+",
+            ),
+        ),
+        "List[str]",
+    ),
+)
+def test_raises_on_union_of_scalars(annotation: str, monkeypatch):
+    @strawberry.type
+    class ICanBeInUnion:
+        foo: str
+
+    monkeypatch.setitem(globals(), ICanBeInUnion.__name__, ICanBeInUnion)
+    annotation = f"Union[{ICanBeInUnion.__name__}, {annotation}]"
+
+    @strawberry.type
+    class Query:
+        union: annotation  # type: ignore
+
+    strawberry.Schema(query=Query)
