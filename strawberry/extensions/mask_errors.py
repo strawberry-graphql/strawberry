b9e81665ece8c73645a63b18a538bb0c6b0e7cef
Nikolai Maas <nikolai.maas@student.kit.edu>
2023-03-21 17:31:53 +0100

    update MaskErrors extension to use new extension api
    Seems this extension was missed when the new extension api was
    introduced.

--- strawberry/extensions/mask_errors.py
+++ strawberry/extensions/mask_errors.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Iterator
 
 from graphql.error import GraphQLError
 
@@ -32,7 +32,8 @@ class MaskErrors(SchemaExtension):
             original_error=None,
         )
 
-    def on_request_end(self):
+    def on_operation(self) -> Iterator[None]:
+        yield
         result = self.execution_context.result
         if result and result.errors:
             processed_errors = []
