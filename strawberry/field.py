e1c1135de31d5f76bbb541d4f9e34b2bb50e45fa
Patrick Arminio <patrick.arminio@gmail.com>
2023-03-23 21:10:26 +0000

    Update types for latest pyright

--- strawberry/field.py
+++ strawberry/field.py
@@ -47,8 +47,8 @@ _RESOLVER_TYPE = Union[
     # we initially used Awaitable, but that was triggering the following mypy bug:
     # https://github.com/python/mypy/issues/14669
     Callable[..., Coroutine[T, Any, Any]],
-    "staticmethod[T]",
-    "classmethod[T]",
+    "staticmethod[Any, T]",
+    "classmethod[Any, Any, T]",
 ]
 
 UNRESOLVED = object()
