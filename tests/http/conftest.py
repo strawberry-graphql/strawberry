a991cc1a94ff310f0ee0de2bdaf4b599e80c941e
Matthieu MN <10926130+gazorby@users.noreply.github.com>
2023-03-26 21:10:59 +0200

    Starlite integration (#2391)
    * ✨ feat(starlite): starlite integration
    
    * ♻️ refactor(starlite): remove commented code
    
    * 👷 build(ci): add dedicated job for starlite tests
    
    * ♻️ refactor: wip
    
    * ✅ test(http): add starlite client
    
    * ✅ test(starlite): remove duplicated tests
    
    * ♻️ refactor(starlite): support adding background tasks
    
    * ✅ test(starlite): move more tests to tests/http
    
    * ♻️ refactor(starlite): improve openapi typing
    
    * ✅ test(starlite): add controller tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ♻️ refactor(starlite): move 4406 ws code to constant
    
    * ♻️ refactor(starlite): get content_type from request
    
    * 👷 build(starlite): update to v1.48.0
    
    * ♻️ refactor(starlite): stringify typing
    
    * 🐛 fix(starlite): catch msgspec.DecodeError
    
    * ♻️ refactor(starlite): remove websocket_path param
    
    * ♻️ refactor(starlite): always except MsgspecError for ws handlers
    
    * ♻️ refactor(starlite): use set to test accept header
    
    * ♻️ refactor(starlite): use HTTPMethod where applicable
    
    * ✅ test(starlite): fix test_set_cookie_headers
    
    * ♻️ refactor(starlite): fix untyped args
    
    * ♻️ refactor(starlite): fix typing
    
    * Update code after merge
    
    * Update strawberry/subscriptions/__init__.py
    
    Co-authored-by: Jonathan Ehwald <github@ehwald.info>
    
    * ✅ test(starlite): skip tests on 3.7
    
    * ✅ test(starlite): fix http/test_pload
    
    * 🎨 style: fix mypy
    
    * ♻️ refactor: move WS_4406_PROTOCOL_NOT_ACCEPTABLE constant
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * 👷 build(deps): fix poetry.lock
    
    * ✅ test(starlite): fix import
    
    * 🐛 fix(starlite): graphiql not always rendered when it should
    
    * 🎨 style: ruff
    
    * 🐛 fix(starlite): catch error when parsing multipart requests
    
    * ♻️ refactor(starlite): use raise from form
    
    * ✅ test(starlite): fix IS_STARLITE_INSTALLED
    
    * ♻️ refactor: drop SubscriptionProtocolType literal
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix tests
    
    * Remove check for starlite
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Try starlite import
    
    * Fix error type
    
    * Check imports in all starlite tests
    
    * Fix another import
    
    * Use marks for tests
    
    * Remove comments
    
    * Update relase notes and add docs
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Patrick Arminio <patrick.arminio@gmail.com>
    Co-authored-by: Jonathan Ehwald <github@ehwald.info>

--- tests/http/conftest.py
+++ tests/http/conftest.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Type
 
 import pytest
@@ -13,20 +14,30 @@ from .clients import (
     FlaskHttpClient,
     HttpClient,
     SanicHttpClient,
+    StarliteHttpClient,
 )
 
 
 @pytest.fixture(
     params=[
-        AioHttpClient,
-        AsgiHttpClient,
-        AsyncDjangoHttpClient,
-        AsyncFlaskHttpClient,
-        ChaliceHttpClient,
-        DjangoHttpClient,
-        FastAPIHttpClient,
-        FlaskHttpClient,
-        SanicHttpClient,
+        pytest.param(AioHttpClient, marks=pytest.mark.aiohttp),
+        pytest.param(AsgiHttpClient, marks=pytest.mark.asgi),
+        pytest.param(AsyncDjangoHttpClient, marks=pytest.mark.django),
+        pytest.param(AsyncFlaskHttpClient, marks=pytest.mark.flask),
+        pytest.param(ChaliceHttpClient, marks=pytest.mark.chalice),
+        pytest.param(DjangoHttpClient, marks=pytest.mark.django),
+        pytest.param(FastAPIHttpClient, marks=pytest.mark.fastapi),
+        pytest.param(FlaskHttpClient, marks=pytest.mark.flask),
+        pytest.param(SanicHttpClient, marks=pytest.mark.sanic),
+        pytest.param(
+            StarliteHttpClient,
+            marks=[
+                pytest.mark.starlite,
+                pytest.mark.skipif(
+                    sys.version_info < (3, 8), reason="Starlite requires Python 3.8+"
+                ),
+            ],
+        ),
     ]
 )
 def http_client_class(request) -> Type[HttpClient]:
--- tests/starlite/schema.py
+++ tests/starlite/schema.py
@@ -1,13 +1,11 @@
 import asyncio
-import contextlib
 import typing
 from enum import Enum
+from typing import Optional
 
 from graphql import GraphQLError
 
 import strawberry
-from strawberry.channels.context import StrawberryChannelsContext
-from strawberry.extensions import SchemaExtension
 from strawberry.file_uploads import Upload
 from strawberry.permission import BasePermission
 from strawberry.subscriptions.protocols.graphql_transport_ws.types import PingMessage
@@ -17,30 +15,8 @@ from strawberry.types import Info
 class AlwaysFailPermission(BasePermission):
     message = "You are not authorized"
 
-    def has_permission(self, source, info, **kwargs) -> bool:
+    def has_permission(self, source: typing.Any, info: Info, **kwargs) -> bool:
         return False
-
-
-class MyExtension(SchemaExtension):
-    def get_results(self) -> typing.Dict[str, str]:
-        return {"example": "example"}
-
-
-def _read_file(text_file: Upload) -> str:
-    from starlette.datastructures import UploadFile
-
-    # allow to keep this function synchronous, starlette's files have
-    # async methods for reading
-    if isinstance(text_file, UploadFile):
-        text_file = text_file.file._file  # type: ignore
-
-    with contextlib.suppress(ModuleNotFoundError):
-        from starlite import UploadFile as StarliteUploadFile
-
-        if isinstance(text_file, StarliteUploadFile):
-            text_file = text_file.file  # type: ignore
-
-    return text_file.read().decode()
 
 
 @strawberry.enum
@@ -64,81 +40,53 @@ class DebugInfo:
 @strawberry.type
 class Query:
     @strawberry.field
-    def greetings(self) -> str:
-        return "hello"
-
-    @strawberry.field
     def hello(self, name: typing.Optional[str] = None) -> str:
         return f"Hello {name or 'world'}"
 
     @strawberry.field
-    async def async_hello(
-        self, name: typing.Optional[str] = None, delay: float = 0
-    ) -> str:
+    async def async_hello(self, name: str, delay: float = 0) -> str:
         await asyncio.sleep(delay)
         return f"Hello {name or 'world'}"
 
     @strawberry.field(permission_classes=[AlwaysFailPermission])
-    def always_fail(self) -> typing.Optional[str]:
+    def always_fail(self) -> Optional[str]:
         return "Hey"
+
+    @strawberry.field
+    def root_name(root) -> str:
+        return type(root).__name__
 
     @strawberry.field
     async def exception(self, message: str) -> str:
         raise ValueError(message)
         return message
 
-    @strawberry.field
-    def teapot(self, info: Info[typing.Any, None]) -> str:
-        info.context["response"].status_code = 418
-
-        return "🫖"
-
-    @strawberry.field
-    def root_name(self) -> str:
-        return type(self).__name__
-
-    @strawberry.field
-    def value_from_context(self, info: Info) -> str:
-        return info.context["custom_value"]
-
-    @strawberry.field
-    def returns_401(self, info: Info) -> str:
-        response = info.context["response"]
-
-        if hasattr(response, "set_status"):
-            response.set_status(401)
-        else:
-            response.status_code = 401
-
-        return "hey"
-
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def echo(self, string_to_echo: str) -> str:
-        return string_to_echo
-
-    @strawberry.mutation
-    def hello(self) -> str:
+    async def hello(self) -> str:
         return "strawberry"
 
     @strawberry.mutation
-    def read_text(self, text_file: Upload) -> str:
-        return _read_file(text_file)
+    async def read_text(self, text_file: Upload) -> str:
+        return (await text_file.read()).decode()
 
     @strawberry.mutation
-    def read_files(self, files: typing.List[Upload]) -> typing.List[str]:
-        return list(map(_read_file, files))
+    async def read_files(self, files: typing.List[Upload]) -> typing.List[str]:
+        contents = []
+        for file in files:
+            content = (await file.read()).decode()
+            contents.append(content)
+        return contents
 
     @strawberry.mutation
-    def read_folder(self, folder: FolderInput) -> typing.List[str]:
-        return list(map(_read_file, folder.files))
-
-    @strawberry.mutation
-    def match_text(self, text_file: Upload, pattern: str) -> str:
-        text = text_file.read().decode()
-        return pattern if pattern in text else ""
+    async def read_folder(self, folder: FolderInput) -> typing.List[str]:
+        contents = []
+        for file in folder.files:
+            content = (await file.read()).decode()
+            contents.append(content)
+        return contents
 
 
 @strawberry.type
@@ -201,30 +149,5 @@ class Subscription:
             is_connection_init_timeout_task_done=is_connection_init_timeout_task_done,
         )
 
-    @strawberry.subscription
-    async def listener(
-        self,
-        info: Info[StrawberryChannelsContext, typing.Any],
-        timeout: typing.Optional[float] = None,
-        group: typing.Optional[str] = None,
-    ) -> typing.AsyncGenerator[str, None]:
-        yield info.context.request.channel_name
 
-        async for message in info.context.request.channel_listen(
-            type="test.message",
-            timeout=timeout,
-            groups=[group] if group is not None else [],
-        ):
-            yield message["text"]
-
-    @strawberry.subscription
-    async def connection_params(self, info) -> typing.AsyncGenerator[str, None]:
-        yield info.context["connection_params"]["strawberry"]
-
-
-schema = strawberry.Schema(
-    query=Query,
-    mutation=Mutation,
-    subscription=Subscription,
-    extensions=[MyExtension],
-)
+schema = strawberry.Schema(Query, mutation=Mutation, subscription=Subscription)
