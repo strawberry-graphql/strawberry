72089432153e01ad8febeb31e28ca0052ab26ad3
Patrick Arminio <patrick.arminio@gmail.com>
2023-03-31 23:59:24 +0100

    Update emoji

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,8 @@ def pytest_emoji_xfailed(config) -> Tuple[str, str]:
     return "🤷‍♂️ ", "XFAIL 🤷‍♂️ "
 
 
+def pytest_emoji_skipped(config) -> Tuple[str, str]:
+    return "🦘 ", "SKIPPED 🦘"
+
+
 pytest_plugins = ("tests.plugins.strawberry_exceptions",)
