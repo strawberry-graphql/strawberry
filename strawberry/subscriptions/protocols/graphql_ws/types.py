6448d6ab4d7af40a7b2577abcb13ee6b5dab0e0d
matthew-taylor-jones <matthew.jones@crunchyroll.com>
2023-05-14 20:50:27 -0700

    Refactor for the TypedDict usage of the graphql_ws component

--- strawberry/subscriptions/protocols/graphql_ws/types.py
+++ strawberry/subscriptions/protocols/graphql_ws/types.py
@@ -1,7 +1,12 @@
+from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Union
-from typing_extensions import TypedDict
 
 from graphql import GraphQLFormattedError
+
+from strawberry import UNSET
+from strawberry.subscriptions.protocols.graphql_transport_ws.types import (
+    GraphQLTransportMessage,
+)
 
 ConnectionInitPayload = Dict[str, Any]
 
@@ -9,17 +14,19 @@ ConnectionInitPayload = Dict[str, Any]
 ConnectionErrorPayload = Dict[str, Any]
 
 
-class StartPayload(TypedDict, total=False):
+@dataclass
+class StartPayload(GraphQLTransportMessage):
     query: str
-    variables: Optional[Dict[str, Any]]
-    operationName: Optional[str]
+    variables: Optional[Dict[str, Any]] = UNSET
+    operationName: Optional[str] = UNSET
 
 
-class DataPayload(TypedDict, total=False):
+@dataclass
+class DataPayload(GraphQLTransportMessage):
     data: Any
 
     # Optional list of formatted graphql.GraphQLError objects
-    errors: Optional[List[GraphQLFormattedError]]
+    errors: Optional[List[GraphQLFormattedError]] = UNSET
 
 
 ErrorPayload = GraphQLFormattedError
@@ -34,7 +41,8 @@ OperationMessagePayload = Union[
 ]
 
 
-class OperationMessage(TypedDict, total=False):
+@dataclass
+class OperationMessage(GraphQLTransportMessage):
     type: str
     id: str
     payload: OperationMessagePayload
