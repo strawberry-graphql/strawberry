debf8acd7f92a827e2aedf9547bf93af04e1a659
Christian Dröge <mail@cdroege.de>
2023-05-27 11:53:30 +0000

    Channels: Expose ChannelsConsumer, GraphQLHTTPConsumer, ChannelsWSConsumer in strawberry.channels

--- strawberry/channels/__init__.py
+++ strawberry/channels/__init__.py
@@ -1,10 +1,18 @@
+from .handlers.base import ChannelsConsumer, ChannelsWSConsumer
 from .handlers.graphql_transport_ws_handler import GraphQLTransportWSHandler
 from .handlers.graphql_ws_handler import GraphQLWSHandler
-from .handlers.http_handler import GraphQLHTTPConsumer, SyncGraphQLHTTPConsumer
+from .handlers.http_handler import (
+    ChannelsRequest,
+    GraphQLHTTPConsumer,
+    SyncGraphQLHTTPConsumer,
+)
 from .handlers.ws_handler import GraphQLWSConsumer
 from .router import GraphQLProtocolTypeRouter
 
 __all__ = [
+    "ChannelsConsumer",
+    "ChannelsRequest",
+    "ChannelsWSConsumer",
     "GraphQLProtocolTypeRouter",
     "GraphQLWSHandler",
     "GraphQLTransportWSHandler",
