0a6de0c0aeb98902570a861f207616e633d9c311
Patrick Arminio <patrick.arminio@gmail.com>
2023-06-01 10:47:53 +0200

    Use pytest parametrization

--- tests/types/test_annotation.py
+++ tests/types/test_annotation.py
@@ -2,41 +2,49 @@ import itertools
 from enum import Enum
 from typing import Optional, TypeVar, Union
 
+import pytest
+
 import strawberry
 from strawberry.annotation import StrawberryAnnotation
 from strawberry.unset import UnsetType
 
 
-def test_annotation_hash():
-    T = TypeVar("T")
+class Bleh:
+    pass
 
-    class Bleh:
-        pass
 
-    @strawberry.enum
-    class NumaNuma(Enum):
-        MA = "ma"
-        I = "i"  # noqa: E741
-        A = "a"
-        HI = "hi"
+@strawberry.enum
+class NumaNuma(Enum):
+    MA = "ma"
+    I = "i"  # noqa: E741
+    A = "a"
+    HI = "hi"
 
-    types = [
-        int,
-        str,
-        None,
-        Optional[str],
-        UnsetType,
-        Union[int, str],
-        "int",
-        T,
-        Bleh,
-        NumaNuma,
-    ]
-    for type1, type2 in itertools.combinations_with_replacement(types, 2):
-        annotation1 = StrawberryAnnotation(type1)
-        annotation2 = StrawberryAnnotation(type2)
-        assert (
-            hash(annotation1) == hash(annotation2)
-            if annotation1 == annotation2
-            else hash(annotation1) != hash(annotation2)
-        ), "Equal type must imply equal hash"
+
+T = TypeVar("T")
+
+types = [
+    int,
+    str,
+    None,
+    Optional[str],
+    UnsetType,
+    Union[int, str],
+    "int",
+    T,
+    Bleh,
+    NumaNuma,
+]
+
+
+@pytest.mark.parametrize(
+    ("type1", "type2"), itertools.combinations_with_replacement(types, 2)
+)
+def test_annotation_hash(type1: Union[object, str], type2: Union[object, str]):
+    annotation1 = StrawberryAnnotation(type1)
+    annotation2 = StrawberryAnnotation(type2)
+    assert (
+        hash(annotation1) == hash(annotation2)
+        if annotation1 == annotation2
+        else hash(annotation1) != hash(annotation2)
+    ), "Equal type must imply equal hash"
