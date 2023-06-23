1cdd506162bd07a0fbe797ac31252183e70ce73d
Patrick Arminio <patrick.arminio@gmail.com>
2023-06-23 21:55:48 +0100

    Better codemod

--- strawberry/codemods/annotated_unions.py
+++ strawberry/codemods/annotated_unions.py
@@ -1,8 +1,32 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Sequence
+
 import libcst as cst
 import libcst.matchers as m
-from libcst._nodes.expression import BaseExpression, Call
 from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
 from libcst.codemod.visitors import AddImportsVisitor
+
+if TYPE_CHECKING:
+    from libcst._nodes.expression import BaseExpression, Call
+
+
+def _find_named_argument(args: Sequence[cst.Arg], name: str) -> cst.Arg | None:
+    return next(
+        (arg for arg in args if arg.keyword and arg.keyword.value == name),
+        None,
+    )
+
+
+def _find_positional_argument(
+    args: Sequence[cst.Arg], search_index: int
+) -> cst.Arg | None:
+    for index, arg in enumerate(args):
+        if index > search_index:
+            return None
+
+        if index == search_index and arg.keyword is None:
+            return arg
 
 
 class ConvertUnionToAnnotatedUnion(VisitorBasedCodemodCommand):
@@ -14,22 +38,89 @@ class ConvertUnionToAnnotatedUnion(VisitorBasedCodemodCommand):
     def __init__(self, context: CodemodContext) -> None:
         super().__init__(context)
 
-    # TODO: add types
     @m.leave(m.Call(func=m.Attribute(value=m.Name("strawberry"), attr=m.Name("union"))))
     def leave_union_call(
         self, original_node: Call, updated_node: Call
     ) -> BaseExpression:
+        types = _find_named_argument(original_node.args, "types")
+        union_name = _find_named_argument(original_node.args, "name")
+
+        if types is None:
+            types = _find_positional_argument(original_node.args, 1)
+
+        # this is probably a strawberry.union(name="...") so we skip the conversion
+        # as it is going to be used in the new way already 😊
+
+        if types is None:
+            return original_node
+
         AddImportsVisitor.add_needed_import(
             self.context, "typing_extensions", "Annotated"
         )
 
-        types = original_node.args[1].value.elements
-        name = original_node.args[0].value.value
+        if union_name is None:
+            union_name = _find_positional_argument(original_node.args, 0)
 
-        # create Annotated[Union[...], strawberry.type(...)]
-        annotated_union = cst.parse_expression(
-            f"Annotated[Union[{', '.join(str(t.value.value) for t in types)}], "
-            f"strawberry.type(name={name})]"
+        assert union_name
+        assert isinstance(types.value, (cst.Tuple, cst.List))
+
+        types = types.value.elements
+        union_name = union_name.value
+
+        description = _find_named_argument(original_node.args, "description")
+        directives = _find_named_argument(original_node.args, "directives")
+
+        union_node = cst.Subscript(
+            value=cst.Name(value="Union"),
+            slice=[cst.SubscriptElement(slice=cst.Index(value=t.value)) for t in types],
         )
 
-        return annotated_union
+        union_call_args = [
+            cst.Arg(
+                value=union_name,
+                keyword=cst.Name(value="name"),
+                equal=cst.AssignEqual(
+                    whitespace_before=cst.SimpleWhitespace(""),
+                    whitespace_after=cst.SimpleWhitespace(""),
+                ),
+            )
+        ]
+
+        additional_args = {"description": description, "directives": directives}
+
+        union_call_args.extend(
+            cst.Arg(
+                value=arg.value,
+                keyword=cst.Name(name),
+                equal=cst.AssignEqual(
+                    whitespace_before=cst.SimpleWhitespace(""),
+                    whitespace_after=cst.SimpleWhitespace(""),
+                ),
+            )
+            for name, arg in additional_args.items()
+            if arg is not None
+        )
+
+        union_call_node = cst.Call(
+            func=cst.Attribute(
+                value=cst.Name(value="strawberry"),
+                attr=cst.Name(value="union"),
+            ),
+            args=union_call_args,
+        )
+
+        return cst.Subscript(
+            value=cst.Name(value="Annotated"),
+            slice=[
+                cst.SubscriptElement(
+                    slice=cst.Index(
+                        value=union_node,
+                    ),
+                ),
+                cst.SubscriptElement(
+                    slice=cst.Index(
+                        value=union_call_node,
+                    ),
+                ),
+            ],
+        )
--- tests/codemods/test_annotated_unions.py
+++ tests/codemods/test_annotated_unions.py
@@ -18,7 +18,75 @@ class TestConvertConstantCommand(CodemodTest):
         after = """
             from typing_extensions import Annotated
 
-            AUnion = Annotated[Union[Foo, Bar], strawberry.type(name="ABC")]
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_swapped_kwargs(self) -> None:
+        before = """
+            AUnion = strawberry.union(types=(Foo, Bar), name="ABC")
+        """
+
+        after = """
+            from typing_extensions import Annotated
+
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_union_list(self) -> None:
+        before = """
+            AUnion = strawberry.union(name="ABC", types=[Foo, Bar])
+        """
+
+        after = """
+            from typing_extensions import Annotated
+
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_positional_arguments(self) -> None:
+        before = """
+            AUnion = strawberry.union("ABC", (Foo, Bar))
+        """
+
+        after = """
+            from typing_extensions import Annotated
+
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_supports_directives_and_description(self) -> None:
+        before = """
+            AUnion = strawberry.union(
+                "ABC",
+                (Foo, Bar),
+                description="cool union",
+                directives=[object()],
+            )
+        """
+
+        after = """
+            from typing_extensions import Annotated
+
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC", description="cool union", directives=[object()])]
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_noop_with_annotated_unions(self) -> None:
+        before = """
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
+        """
+
+        after = """
+            AUnion = Annotated[Union[Foo, Bar], strawberry.union(name="ABC")]
         """
 
         self.assertCodemod(before, after)
