fdb288fe9cd3c0d4257aecf2fe3f260c0c3ecc65
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-07 21:55:45 +0100

    wip

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
-from nox_poetry import session
+from nox_poetry import Session, session
 
 
-@session(python=["3.10", "3.9"])
-def tests(session):
+@session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
+def tests(session: Session) -> None:
     session.run_always("poetry", "install", external=True)
     session.run("pytest")
