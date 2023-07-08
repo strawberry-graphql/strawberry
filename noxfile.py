64c59ef4dfdec879f124a2c790946689febfd292
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-08 10:28:38 +0100

    update test command

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,25 @@
 from nox_poetry import Session, session
 
 
-# 2
 @session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
 def tests(session: Session) -> None:
     session.run_always("poetry", "install", external=True)
-    session.run("pytest")
+
+    session.run(
+        "pytest",
+        "--cov=strawberry",
+        "--cov-append",
+        "--cov-report=xml",
+        "-n",
+        "auto",
+        "--showlocals",
+        "-vv",
+        "-m",
+        "not starlette",
+        "-m",
+        "not django",
+        "-m",
+        "not starlite",
+        "--ignore=tests/mypy",
+        "--ignore=tests/pyright",
+    )
