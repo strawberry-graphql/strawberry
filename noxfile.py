639e4dfd316c1b73f7184a6bf9f2fd0e82100753
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-08 11:57:42 +0100

    Move more tests to nox

--- noxfile.py
+++ noxfile.py
@@ -45,3 +45,42 @@ def test_django(session: Session, django: str) -> None:
         "-m",
         "django",
     )
+
+
+@session(python=["3.11"])
+@nox.parametrize("starlette", ["0.28.0", "0.27.0", "0.26.1"])
+def test_starlette(session: Session, starlette: str) -> None:
+    session.run_always("poetry", "install", external=True)
+
+    session._session.install(f"starlette=={starlette}")  # type: ignore
+
+    session.run(
+        "pytest",
+        "--cov=strawberry",
+        "--cov-append",
+        "--cov-report=xml",
+        "-n",
+        "auto",
+        "--showlocals",
+        "-vv",
+        "-m",
+        "starlette",
+    )
+
+
+@session(python=["3.11"])
+def test_litestar(session: Session) -> None:
+    session.run_always("poetry", "install", external=True)
+
+    session.run(
+        "pytest",
+        "--cov=strawberry",
+        "--cov-append",
+        "--cov-report=xml",
+        "-n",
+        "auto",
+        "--showlocals",
+        "-vv",
+        "-m",
+        "starlite",
+    )
