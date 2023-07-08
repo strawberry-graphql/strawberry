a3e37a97cbec238682791f3c6a71bc1411c37e87
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-08 10:13:34 +0100

    Update cache path

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 from nox_poetry import Session, session
 
 
+# 2
 @session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
 def tests(session: Session) -> None:
     session.run_always("poetry", "install", external=True)
