3195858c1d6bb3ddabb275ebca1aca688e07f564
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-11 14:01:16 +0100

    More fixes

--- tests/fastapi/test_websockets.py
+++ tests/fastapi/test_websockets.py
@@ -7,10 +7,13 @@ from fastapi import FastAPI
 from strawberry.fastapi.router import GraphQLRouter
 from strawberry.subscriptions import GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL
 from tests.fastapi.app import create_app
+from tests.http.schema import get_schema
 
 
 def test_turning_off_graphql_ws():
-    app = create_app(subscription_protocols=[GRAPHQL_TRANSPORT_WS_PROTOCOL])
+    app = create_app(
+        get_schema(), subscription_protocols=[GRAPHQL_TRANSPORT_WS_PROTOCOL]
+    )
     test_client = TestClient(app)
 
     with pytest.raises(WebSocketDisconnect) as exc:
@@ -21,7 +24,7 @@ def test_turning_off_graphql_ws():
 
 
 def test_turning_off_graphql_transport_ws():
-    app = create_app(subscription_protocols=[GRAPHQL_WS_PROTOCOL])
+    app = create_app(get_schema(), subscription_protocols=[GRAPHQL_WS_PROTOCOL])
     test_client = TestClient(app)
 
     with pytest.raises(WebSocketDisconnect) as exc:
@@ -32,7 +35,7 @@ def test_turning_off_graphql_transport_ws():
 
 
 def test_turning_off_all_ws_protocols():
-    app = create_app(subscription_protocols=[])
+    app = create_app(get_schema(), subscription_protocols=[])
     test_client = TestClient(app)
 
     with pytest.raises(WebSocketDisconnect) as exc:
@@ -49,7 +52,7 @@ def test_turning_off_all_ws_protocols():
 
 
 def test_unsupported_ws_protocol():
-    app = create_app(subscription_protocols=[])
+    app = create_app(get_schema(), subscription_protocols=[])
     test_client = TestClient(app)
 
     with pytest.raises(WebSocketDisconnect) as exc:
@@ -61,7 +64,8 @@ def test_unsupported_ws_protocol():
 
 def test_clients_can_prefer_protocols():
     app = create_app(
-        subscription_protocols=[GRAPHQL_WS_PROTOCOL, GRAPHQL_TRANSPORT_WS_PROTOCOL]
+        get_schema(),
+        subscription_protocols=[GRAPHQL_WS_PROTOCOL, GRAPHQL_TRANSPORT_WS_PROTOCOL],
     )
     test_client = TestClient(app)
 
