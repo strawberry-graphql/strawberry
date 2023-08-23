f1b1c69012b4a5e70e231045853b063be6c5bf21
Patrick Arminio <patrick.arminio@gmail.com>
2023-08-23 23:54:45 +0100

    Test: ignore this commit

--- tests/experimental/pydantic/utils.py
+++ tests/experimental/pydantic/utils.py
@@ -6,3 +6,5 @@ needs_pydantic_v2 = pytest.mark.skipif(
     not IS_PYDANTIC_V2, reason="requires Pydantic v2"
 )
 needs_pydantic_v1 = pytest.mark.skipif(IS_PYDANTIC_V2, reason="requires Pydantic v1")
+
+# test
