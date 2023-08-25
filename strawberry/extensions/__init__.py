b790ea237f46402b8734acffed2583ef624a5187
boesch <boesch@indeed.com>
2023-08-24 21:15:27 -0500

    feat: add extension to support partial errors, response with data and errors

--- strawberry/extensions/__init__.py
+++ strawberry/extensions/__init__.py
@@ -9,6 +9,7 @@ from .mask_errors import MaskErrors
 from .max_aliases import MaxAliasesLimiter
 from .max_tokens import MaxTokensLimiter
 from .parser_cache import ParserCache
+from .partial_results import PartialResultsExtension
 from .query_depth_limiter import IgnoreContext, QueryDepthLimiter
 from .validation_cache import ValidationCache
 
@@ -35,6 +36,7 @@ __all__ = [
     "AddValidationRules",
     "DisableValidation",
     "ParserCache",
+    "PartialResultsExtension",
     "QueryDepthLimiter",
     "IgnoreContext",
     "ValidationCache",
