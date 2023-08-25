a0336243d6ba158b798c7601005f0157a4d86af3
Kristján Valur Jónsson <sweskman@gmail.com>
2023-08-25 15:04:55 +0000

    update benchmark test

--- tests/benchmarks/test_subscriptions.py
+++ tests/benchmarks/test_subscriptions.py
@@ -16,11 +16,8 @@ def test_subscription(benchmark: BenchmarkFixture):
 
     async def _run():
         for _ in range(100):
-            iterator = await schema.subscribe(s)
-
-            value = await iterator.__anext__()  # type: ignore[union-attr]
-
-            assert value.data is not None
-            assert value.data["something"] == "Hello World!"
+            async for value in schema.subscribe(s):
+                assert value.data is not None
+                assert value.data["something"] == "Hello World!"
 
     benchmark(lambda: asyncio.run(_run()))
