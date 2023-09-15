ecba2c3a1e638406f6cea3d708114fdb7507d49f
Matt Gilson <m.gilson1@gmail.com>
2023-09-15 10:44:08 -0400

    Codegen generic types (#3077)
    Co-authored-by: Matt Gilson <mgilson@lat.ai>

---
+++ tests/codegen/snapshots/python/generic_types.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class ListLifeGenericResultListLifeItems1:
+    name: str
+    age: int
+
+class ListLifeGenericResultListLifeItems2:
+    name: str
+    age: int
+
+class ListLifeGenericResultListLife:
+    items1: List[ListLifeGenericResultListLifeItems1]
+    items2: List[ListLifeGenericResultListLifeItems2]
+
+class ListLifeGenericResult:
+    list_life: ListLifeGenericResultListLife
