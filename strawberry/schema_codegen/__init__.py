c9b6fffd557026825fb78035816d7b5419683e26
Patrick Arminio <patrick.arminio@gmail.com>
2023-09-17 12:32:24 +0100

    Add support for scalars

--- strawberry/schema_codegen/__init__.py
+++ strawberry/schema_codegen/__init__.py
@@ -17,6 +17,7 @@ from graphql import (
     ObjectTypeDefinitionNode,
     ObjectTypeExtensionNode,
     OperationType,
+    ScalarTypeDefinitionNode,
     SchemaDefinitionNode,
     TypeNode,
     UnionTypeDefinitionNode,
@@ -34,6 +35,15 @@ _SCALAR_MAP = {
         value=cst.Name("strawberry"),
         attr=cst.Name("ID"),
     ),
+    "JSON": cst.Attribute(
+        value=cst.Name("strawberry"),
+        attr=cst.Name("JSON"),
+    ),
+    "UUID": cst.Name("UUID"),
+    "Decimal": cst.Name("Decimal"),
+    "Date": cst.Name("date"),
+    "Time": cst.Name("time"),
+    "DateTime": cst.Name("datetime"),
 }
 
 
@@ -310,6 +320,90 @@ def _get_union_definition(definition: UnionTypeDefinitionNode) -> cst.Assign:
     )
 
 
+def _get_scalar_definition(
+    definition: ScalarTypeDefinitionNode, imports: set[Import]
+) -> cst.SimpleStatementLine | None:
+    name = definition.name.value
+
+    if name == "Date":
+        imports.add(Import(module="datetime", imports=("date",)))
+        return None
+    if name == "Time":
+        imports.add(Import(module="datetime", imports=("time",)))
+        return None
+    if name == "DateTime":
+        imports.add(Import(module="datetime", imports=("datetime",)))
+        return None
+    if name == "Decimal":
+        imports.add(Import(module="decimal", imports=("Decimal",)))
+        return None
+    if name == "UUID":
+        imports.add(Import(module="uuid", imports=("UUID",)))
+        return None
+    if name == "JSON":
+        return None
+
+    description = definition.description.value if definition.description else None
+
+    specified_by_url = None
+
+    for directive in definition.directives:
+        if directive.name.value == "specifiedBy":
+            specified_by_url = directive.arguments[0].value.value
+
+    imports.add(Import(module="typing", imports=("NewType",)))
+
+    identity_lambda = cst.Lambda(
+        body=cst.Name("v"),
+        params=cst.Parameters(
+            params=[cst.Param(cst.Name("v"))],
+        ),
+    )
+
+    additional_args: list[cst.Arg | None] = [
+        _get_argument("description", description) if description else None,
+        _get_argument("specified_by_url", specified_by_url)
+        if specified_by_url
+        else None,
+        cst.Arg(
+            keyword=cst.Name("serialize"),
+            value=identity_lambda,
+            equal=cst.AssignEqual(cst.SimpleWhitespace(""), cst.SimpleWhitespace("")),
+        ),
+        cst.Arg(
+            keyword=cst.Name("parse_value"),
+            value=identity_lambda,
+            equal=cst.AssignEqual(cst.SimpleWhitespace(""), cst.SimpleWhitespace("")),
+        ),
+    ]
+
+    return cst.SimpleStatementLine(
+        body=[
+            cst.Assign(
+                targets=[cst.AssignTarget(cst.Name(name))],
+                value=cst.Call(
+                    func=cst.Attribute(
+                        value=cst.Name("strawberry"),
+                        attr=cst.Name("scalar"),
+                    ),
+                    args=[
+                        cst.Arg(
+                            cst.Call(
+                                func=cst.Name("NewType"),
+                                args=[
+                                    cst.Arg(cst.SimpleString(f'"{name}"')),
+                                    cst.Arg(cst.Name("object")),
+                                ],
+                            )
+                        ),
+                        *filter(None, additional_args),
+                    ],
+                ),
+            )
+        ]
+    )
+
+
 def codegen(schema: str) -> str:
     document = parse(schema)
 
@@ -324,8 +418,6 @@ def codegen(schema: str) -> str:
     }
 
     for definition in document.definitions:
-        definitions.append(cst.EmptyLine())  # type: ignore - this works :)
-
         if isinstance(
             definition,
             (
@@ -337,11 +429,13 @@ def codegen(schema: str) -> str:
         ):
             class_definition = _get_class_definition(definition)
 
+            definitions.append(cst.EmptyLine())
             definitions.append(class_definition)
 
         elif isinstance(definition, EnumTypeDefinitionNode):
             imports.add(Import(module="enum", imports=("Enum",)))
 
+            definitions.append(cst.EmptyLine())
             definitions.append(_get_enum_definition(definition))
 
         elif isinstance(definition, SchemaDefinitionNode):
@@ -359,8 +453,16 @@ def codegen(schema: str) -> str:
         elif isinstance(definition, UnionTypeDefinitionNode):
             imports.add(Import(module="typing", imports=("Annotated",)))
 
+            definitions.append(cst.EmptyLine())
             definitions.append(_get_union_definition(definition))
-            definitions.append(cst.EmptyLine())  # type: ignore - this works :)
+            definitions.append(cst.EmptyLine())
+        elif isinstance(definition, ScalarTypeDefinitionNode):
+            scalar_definition = _get_scalar_definition(definition, imports)
+
+            if scalar_definition is not None:
+                definitions.append(cst.EmptyLine())
+                definitions.append(scalar_definition)
+                definitions.append(cst.EmptyLine())
         else:
             raise NotImplementedError(f"Unknown definition {definition}")
 
@@ -371,13 +473,16 @@ def codegen(schema: str) -> str:
     )
 
     if schema_definition:
+        definitions.append(cst.EmptyLine())
         definitions.append(schema_definition)
 
     module = cst.Module(
         body=[
             *[
                 cst.SimpleStatementLine(body=[import_.to_cst()])
-                for import_ in sorted(imports, key=lambda i: i.module or "")
+                for import_ in sorted(
+                    imports, key=lambda i: (i.module or "", i.imports)
+                )
             ],
             *definitions,  # type: ignore
         ]
