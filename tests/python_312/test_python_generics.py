f9fb3beb6eb21e6feff30c98eeb14ffc6698b8ff
Patrick Arminio <patrick.arminio@gmail.com>
2023-11-06 17:15:57 +0100

    Add test case

--- tests/python_312/test_python_generics.py
+++ tests/python_312/test_python_generics.py
@@ -3,6 +3,7 @@ These tests are for Generics that don't expose any generic parts to the schema.
 """
 
 import sys
+import textwrap
 
 import pytest
 
@@ -67,3 +68,118 @@ def test_does_not_create_a_new_type_when_no_generic_field_exposed_argument():
 
     assert not result.errors
     assert result.data == {"example": {"__typename": "Edge", "cursor": "1"}}
+
+
+def test_with_interface():
+    @strawberry.interface
+    class GenericInterface[T]:
+        data: strawberry.Private[T]
+
+        @strawberry.field
+        def value(self) -> str:
+            raise NotImplementedError()
+
+    @strawberry.type
+    class ImplementationOne(GenericInterface[str]):
+        @strawberry.field
+        def value(self) -> str:
+            return self.data
+
+    @strawberry.type
+    class ImplementationTwo(GenericInterface[bool]):
+        @strawberry.field
+        def value(self) -> str:
+            if self.data is True:
+                return "true"
+            return "false"
+
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        @staticmethod
+        async def generic_field() -> GenericInterface:  # type: ignore
+            return ImplementationOne(data="foo")
+
+    schema = strawberry.Schema(
+        query=Query, types=[ImplementationOne, ImplementationTwo]
+    )
+
+    expected_schema = textwrap.dedent(
+        """
+        interface GenericInterface {
+          value: String!
+        }
+
+        type ImplementationOne implements GenericInterface {
+          value: String!
+        }
+
+        type ImplementationTwo implements GenericInterface {
+          value: String!
+        }
+
+        type Query {
+          genericField: GenericInterface!
+        }
+        """
+    ).strip()
+
+    assert str(schema) == expected_schema
+
+
+@pytest.mark.xfail(reason="TODO: fix this")
+def test_with_interface_and_type():
+    @strawberry.interface
+    class GenericInterface[T]:
+        data: strawberry.Private[T]
+
+        @strawberry.field
+        def value(self) -> str:
+            raise NotImplementedError()
+
+    @strawberry.type
+    class ImplementationOne(GenericInterface[str]):
+        @strawberry.field
+        def value(self) -> str:
+            return self.data
+
+    @strawberry.type
+    class ImplementationTwo(GenericInterface[bool]):
+        @strawberry.field
+        def value(self) -> str:
+            if self.data is True:
+                return "true"
+            return "false"
+
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        @staticmethod
+        async def generic_field() -> GenericInterface[str | bool]:
+            return ImplementationOne(data="foo")
+
+    schema = strawberry.Schema(
+        query=Query, types=[ImplementationOne, ImplementationTwo]
+    )
+
+    expected_schema = textwrap.dedent(
+        """
+        interface GenericInterface {
+          value: String!
+        }
+
+        type ImplementationOne implements GenericInterface {
+          value: String!
+        }
+
+        type ImplementationTwo implements GenericInterface {
+          value: String!
+        }
+
+        type Query {
+          genericField: GenericInterface!
+        }
+        """
+    ).strip()
+
+    assert str(schema) == expected_schema
