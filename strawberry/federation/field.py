5deece8dcb40e75dc19998535599654514a62145
Lukasz Piatkowski <lukaspiatkowski@gmail.com>
2024-02-14 21:09:38 +0100

    Add Resolver type, use it as a return type for field and fix tests

--- strawberry/federation/field.py
+++ strawberry/federation/field.py
@@ -24,6 +24,7 @@ if TYPE_CHECKING:
     from strawberry.extensions.field_extension import FieldExtension
     from strawberry.field import _RESOLVER_TYPE, StrawberryField
     from strawberry.permission import BasePermission
+    from strawberry.types.types import Resolver
 
     from .schema_directives import Override
 
@@ -55,7 +56,7 @@ def field(
     directives: Sequence[object] = (),
     extensions: Optional[List[FieldExtension]] = None,
     graphql_type: Optional[Any] = None,
-) -> T: ...
+) -> Resolver[T]: ...
 
 
 @overload
