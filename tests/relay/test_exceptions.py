f59c4b976855aa511ba63808e8f00f9088e75219
Take Weiland <diesieben07@gmail.com>
2024-06-05 18:05:00 +0200

    Add a test for non Node types in GlobalID

--- tests/relay/test_exceptions.py
+++ tests/relay/test_exceptions.py
@@ -40,6 +40,29 @@ def test_raises_error_on_unknown_node_type_in_global_id():
     )
 
 
+def test_raises_error_on_non_node_type_in_global_id():
+    @strawberry.type
+    class Query:
+        @strawberry.field()
+        def test(self, info: Info) -> GlobalID:
+            _id = GlobalID("NonNodeType", "bar")
+            _id.resolve_type(info)
+            return _id
+
+    schema = strawberry.Schema(query=Query, types=(NonNodeType, ))
+
+    result = schema.execute_sync("""
+        query TestQuery {
+            test
+        }
+    """)
+    assert len(result.errors) == 1
+    assert (
+        result.errors[0].message == "Cannot resolve. GlobalID requires a GraphQL Node "
+                                    "type, received `NonNodeType`."
+    )
+
+
 @pytest.mark.raises_strawberry_exception(
     NodeIDAnnotationError,
     match='No field annotated with `NodeID` found in "Fruit"',
