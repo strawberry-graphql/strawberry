392778cbc205c961675b78a176b0b0cbb113af78
Patrick Arminio <patrick.arminio@gmail.com>
2024-07-15 14:59:24 +0200

    Update codemod

--- strawberry/codemods/update_imports.py
+++ strawberry/codemods/update_imports.py
@@ -12,8 +12,7 @@ class UpdateImportsCodemod(VisitorBasedCodemodCommand):
         self.add_imports_visitor = AddImportsVisitor(context)
         self.remove_imports_visitor = RemoveImportsVisitor(context)
 
-    # >>> from strawberry.field import something
-    def leave_ImportFrom(
+    def _update_strawberry_field_imports(
         self, node: cst.ImportFrom, updated_node: cst.ImportFrom
     ) -> cst.ImportFrom:
         if m.matches(
@@ -30,5 +29,47 @@ class UpdateImportsCodemod(VisitorBasedCodemodCommand):
                     attr=cst.Name("field"),
                 ),
             )
+
+        return updated_node
+
+    def _update_strawberry_type_imports(
+        self, node: cst.ImportFrom, updated_node: cst.ImportFrom
+    ) -> cst.ImportFrom:
+        if m.matches(
+            node,
+            m.ImportFrom(
+                module=m.Attribute(value=m.Name("strawberry"), attr=m.Name("type"))
+            ),
+        ):
+            has_get_object_definition = any(
+                m.matches(name, m.ImportAlias(name=m.Name("get_object_definition")))
+                for name in node.names
+            )
+
+            updated_node = updated_node.with_changes(
+                module=cst.Attribute(
+                    value=cst.Attribute(
+                        value=cst.Name("strawberry"), attr=cst.Name("types")
+                    ),
+                    attr=cst.Name("base"),
+                ),
+            )
+
+            self.remove_imports_visitor.remove_unused_import(
+                self.context, "strawberry.types.base", "get_object_definition"
+            )
+
+            if has_get_object_definition:
+                self.add_imports_visitor.add_needed_import(
+                    self.context, "strawberry.types", "get_object_definition"
+                )
+
+        return updated_node
+
+    def leave_ImportFrom(
+        self, node: cst.ImportFrom, updated_node: cst.ImportFrom
+    ) -> cst.ImportFrom:
+        updated_node = self._update_strawberry_field_imports(updated_node, updated_node)
+        updated_node = self._update_strawberry_type_imports(updated_node, updated_node)
 
         return updated_node
--- tests/codemods/test_imports.py
+++ tests/codemods/test_imports.py
@@ -6,13 +6,71 @@ from strawberry.codemods.update_imports import UpdateImportsCodemod
 class TestConvertConstantCommand(CodemodTest):
     TRANSFORM = UpdateImportsCodemod
 
-    def test_update_union(self) -> None:
+    def test_update_field(self) -> None:
         before = """
             from strawberry.field import something
         """
 
         after = """
             from strawberry.types.field import something
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_import_strawberry_type(self) -> None:
+        before = """
+            from strawberry.type import (
+                StrawberryContainer,
+                StrawberryList,
+                StrawberryOptional,
+                StrawberryType,
+                WithStrawberryObjectDefinition,
+            )
+        """
+
+        after = """
+            from strawberry.types.base import (
+                StrawberryContainer,
+                StrawberryList,
+                StrawberryOptional,
+                StrawberryType,
+                WithStrawberryObjectDefinition,
+            )
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_import_strawberry_type_object_definition(self) -> None:
+        before = """
+            from strawberry.type import (
+                StrawberryContainer,
+                StrawberryList,
+                StrawberryOptional,
+                StrawberryType,
+                WithStrawberryObjectDefinition,
+                get_object_definition,
+            )
+        """
+
+        after = """
+            from strawberry.types.base import (
+                StrawberryContainer,
+                StrawberryList,
+                StrawberryOptional,
+                StrawberryType,
+                WithStrawberryObjectDefinition)
+            from strawberry.types import get_object_definition
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_update_import_strawberry_type_object_definition_only(self) -> None:
+        before = """
+            from strawberry.type import get_object_definition
+        """
+
+        after = """
+            from strawberry.types import get_object_definition
         """
 
         self.assertCodemod(before, after)
