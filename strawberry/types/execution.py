4d7fa9e96b7cc05a9676eb6a288b17686f77b97e
nir <nrbnlulu@gmail.com>
2024-07-24 21:49:31 +0300

    lints

--- strawberry/types/execution.py
+++ strawberry/types/execution.py
@@ -97,6 +97,7 @@ class ExecutionResult:
 
 class ExecutionResultError(ExecutionResult):
     """Differentiate between a subscription result and an immediate error.
+
     Immediate errors are errors that at the first iteration of the subscription.
     These errors should alert the integration level that the operation should be canceled immediatly.
     """
