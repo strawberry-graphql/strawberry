42749f6568ec495b961694a0718a9f961aa594a1
ניר <88795475+nrbnlulu@users.noreply.github.com>
2024-08-08 13:33:56 +0300

    chore: Add benchmark test for long running subscription (#3593)

--- tests/benchmarks/test_subscriptions.py
+++ tests/benchmarks/test_subscriptions.py
@@ -1,6 +1,8 @@
 import asyncio
+from typing import AsyncIterator
 
 import pytest
+from graphql import ExecutionResult
 from pytest_codspeed.plugin import BenchmarkFixture
 
 from .api import schema
@@ -22,5 +24,27 @@ def test_subscription(benchmark: BenchmarkFixture):
 
             assert value.data is not None
             assert value.data["something"] == "Hello World!"
+
+    benchmark(lambda: asyncio.run(_run()))
+
+
+@pytest.mark.benchmark
+@pytest.mark.parametrize("count", [1000, 20000])
+def test_subscription_long_run(benchmark: BenchmarkFixture, count: int) -> None:
+    s = """#graphql
+    subscription LongRunning($count: Int!) {
+        longRunning(count: $count)
+    }
+    """
+
+    async def _run():
+        i = 0
+        aiterator: AsyncIterator[ExecutionResult] = await schema.subscribe(
+            s, variable_values={"count": count}
+        )  # type: ignore[assignment]
+        async for res in aiterator:
+            assert res.data is not None
+            assert res.data["longRunning"] == i
+            i += 1
 
     benchmark(lambda: asyncio.run(_run()))
