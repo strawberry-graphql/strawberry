d48f3aefbda7f15e43e6186b3b763649221881d8
nir <nrbnlulu@gmail.com>
2024-08-08 11:27:06 +0300

    chore: Add benchmark test for long running subscription

--- tests/benchmarks/test_subscriptions.py
+++ tests/benchmarks/test_subscriptions.py
@@ -24,3 +24,23 @@ def test_subscription(benchmark: BenchmarkFixture):
             assert value.data["something"] == "Hello World!"
 
     benchmark(lambda: asyncio.run(_run()))
+
+
+@pytest.mark.benchmark
+def test_subscription_long_run(benchmark: BenchmarkFixture) -> None:
+    s = """
+    subscription {
+        longRunning
+    }
+    """
+
+    async def _run():
+        i = 0
+        aiterator = await schema.subscribe(s)
+        assert hasattr(aiterator, "__aiter__")
+        async for res in aiterator:
+            assert res.data is not None
+            assert res.data["longRunning"] == i
+            i += 1
+
+    benchmark(lambda: asyncio.run(_run()))
