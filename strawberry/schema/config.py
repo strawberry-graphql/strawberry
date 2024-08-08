3957ca9c1f37930bead99c0438e36d24d61834cc
Ethan Henderson <ethan.henderson.1998@gmail.com>
2024-08-08 12:15:06 +0100

    Use 'Info' class directly instead of default factory for dataclass field

--- strawberry/schema/config.py
+++ strawberry/schema/config.py
@@ -15,7 +15,7 @@ class StrawberryConfig:
     default_resolver: Callable[[Any, str], object] = getattr
     relay_max_results: int = 100
     disable_field_suggestions: bool = False
-    info_class: type[Info] = field(default_factory=lambda: Info)
+    info_class: type[Info] = Info
 
     def __post_init__(
         self,
