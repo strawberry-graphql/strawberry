a6e25b64083c420b8535379ff5afbff9f9133700
Patrick Arminio <patrick.arminio@gmail.com>
2024-09-04 11:42:09 +0200

    Fix method

--- tests/http/clients/base.py
+++ tests/http/clients/base.py
@@ -290,8 +290,7 @@ class WebSocketClient(abc.ABC):
 
 
 class DebuggableGraphQLTransportWSMixin:
-    @staticmethod
-    def on_init() -> None:
+    def on_init(self) -> None:
         """This method can be patched by unit tests to get the instance of the
         transport handler when it is initialized.
         """
