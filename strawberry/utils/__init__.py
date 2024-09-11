171fd1f8d5bf7496bf3808c5932b9f9ab7869a8a
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-11 07:21:36 -0500

    use graphql's built-in version object

--- strawberry/utils/__init__.py
+++ strawberry/utils/__init__.py
@@ -1,5 +1,4 @@
-import graphql
-from packaging.version import Version
+from graphql.version import version_info, VersionInfo
 
-IS_GQL_33 = Version(graphql.__version__) >= Version("3.3.0a")
+IS_GQL_33 = version_info >= VersionInfo.from_str("3.3.0a0")
 IS_GQL_32 = not IS_GQL_33
