07e0c220a9bfe47cdb3dea5c5082eaeb6527c5b4
Patrick Arminio <patrick.arminio@gmail.com>
2024-09-11 20:49:52 +0200

    Remove check

--- tests/schema/types/test_time.py
+++ tests/schema/types/test_time.py
@@ -5,7 +5,6 @@ from graphql import GraphQLError
 
 import strawberry
 from strawberry.types.execution import ExecutionResult
-from tests.conftest import IS_GQL_32
 
 
 def test_serialization():
@@ -105,8 +104,6 @@ def test_serialization_of_incorrect_time_string(value):
     result = execute_mutation(value)
     assert result.errors
     assert isinstance(result.errors[0], GraphQLError)
-    if IS_GQL_32:
-        assert result.errors[0].original_error is None
 
 
 def test_serialization_error_message_for_incorrect_time_string():
