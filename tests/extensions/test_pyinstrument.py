b31d359a4c964fc15da9a226471a9e30356006a9
Patrick Arminio <patrick.arminio@gmail.com>
2024-12-12 15:28:41 +0000

    Fix pyinstrument

--- tests/extensions/test_pyinstrument.py
+++ tests/extensions/test_pyinstrument.py
@@ -6,44 +6,45 @@ import strawberry
 from strawberry.extensions import pyinstrument
 
 
-def a():
+def function_called_by_us_a():
     time.sleep(0.1)
-    return b()
+    return function_called_by_us_b()
 
 
-def b():
+def function_called_by_us_b():
     time.sleep(0.1)
-    return c()
+    return function_called_by_us_c()
 
 
-def c():
+def function_called_by_us_c():
     time.sleep(0.1)
     return 4
 
 
 def test_basic_pyinstrument():
-    REPORT_PATH = tempfile.NamedTemporaryFile().name
+    with tempfile.NamedTemporaryFile() as report_file:
+        report_file_path = Path(report_file.name)
 
-    @strawberry.type
-    class Query:
-        @strawberry.field
-        def the_field(self) -> int:
-            return a()
+        @strawberry.type
+        class Query:
+            @strawberry.field
+            def the_field(self) -> int:
+                return function_called_by_us_a()
 
-    schema = strawberry.Schema(
-        query=Query,
-        extensions=[pyinstrument.PyInstrument(report_path=REPORT_PATH)],
-    )
+        schema = strawberry.Schema(
+            query=Query,
+            extensions=[pyinstrument.PyInstrument(report_path=report_file_path)],
+        )
 
-    # Query the schema
-    result = schema.execute_sync("{ theField }")
+        # Query the schema
+        result = schema.execute_sync("{ theField }")
+        content = report_file_path.read_text("utf-8")
 
     assert not result.errors
     assert result.data["theField"] == 4
 
-    content = Path(REPORT_PATH, encoding="utf-8").read_text()
-    assert '"function": "a"' in content
-    assert '"function": "b"' in content
-    assert '"function": "c"' in content
+    assert "function_called_by_us_a" in content
+    assert "function_called_by_us_b" in content
+    assert "function_called_by_us_c" in content
 
-    assert content.count('"function": "sleep"') == 3
+    assert content.count('"sleep') == 3
