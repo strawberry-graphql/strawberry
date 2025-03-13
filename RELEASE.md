d6a75dfd3d12bb1c1d90f578845f89f3c5cb98f9
Patrick Arminio <patrick.arminio@gmail.com>
2025-03-13 12:48:56 +0000

    Update imports for future pydantic release (#3808)
    * Update imports for future pydantic release
    
    * Add missing types

---
+++ RELEASE.md
@@ -0,0 +1,4 @@
+Release type: patch
+
+
+This release adds support for the upcoming version of Pydantic (2.11)
--- strawberry/experimental/pydantic/_compat.py
+++ strawberry/experimental/pydantic/_compat.py
@@ -91,6 +91,10 @@ ATTR_TO_TYPE_MAP_Pydantic_V2 = {
     "SecretStr": str,
     "SecretBytes": bytes,
     "AnyUrl": str,
+    "AnyHttpUrl": str,
+    "HttpUrl": str,
+    "PostgresDsn": str,
+    "RedisDsn": str,
 }
 
 ATTR_TO_TYPE_MAP_Pydantic_Core_V2 = {
@@ -263,8 +267,8 @@ class PydanticCompat:
 if IS_PYDANTIC_V2:
     from typing_extensions import get_args, get_origin
 
-    from pydantic._internal._typing_extra import is_new_type
-    from pydantic._internal._utils import lenient_issubclass, smart_deepcopy
+    from pydantic.v1.typing import is_new_type
+    from pydantic.v1.utils import lenient_issubclass, smart_deepcopy
 
     def new_type_supertype(type_: Any) -> Any:
         return type_.__supertype__
