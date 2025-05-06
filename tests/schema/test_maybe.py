4aad67713d41e6900649c7cbdd1929a77cf4b164
Patrick Arminio <patrick.arminio@gmail.com>
2025-05-06 16:56:55 +0100

    Remove test for 3.10+

--- tests/schema/test_maybe.py
+++ tests/schema/test_maybe.py
@@ -1,4 +1,3 @@
-import sys
 from textwrap import dedent
 from typing import Optional
 
@@ -147,49 +146,6 @@ def test_optional_argument_maybe() -> None:
             hello(name: null)
         }
     """
-    )
-    assert not result.errors
-    assert result.data == {"hello": "None"}
-
-
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="Pattern matching is required")
-def test_optional_argument_maybe_pattern_matching() -> None:
-    @strawberry.type
-    class Query:
-        @strawberry.field
-        def hello(self, name: strawberry.Maybe[str] = None) -> str:
-            match name:
-                case strawberry.Some(value=value):
-                    return value if value is not None else "None"
-                case None:
-                    return "UNSET"
-
-    schema = strawberry.Schema(query=Query)
-
-    result = schema.execute_sync(
-        """
-        query {
-            hello
-        }
-        """
-    )
-    assert not result.errors
-    assert result.data == {"hello": "UNSET"}
-    result = schema.execute_sync(
-        """
-        query {
-            hello(name: "bar")
-        }
-        """
-    )
-    assert not result.errors
-    assert result.data == {"hello": "bar"}
-    result = schema.execute_sync(
-        """
-        query {
-            hello(name: null)
-        }
-        """
     )
     assert not result.errors
     assert result.data == {"hello": "None"}
