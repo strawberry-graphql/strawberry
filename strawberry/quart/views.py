8b8304ca8d4c417dc2c774ad4c00c4d22cb84b96
Jonathan Ehwald <github@ehwald.info>
2025-05-20 12:41:21 +0200

    Quart subscription support (#3860)
    * Initial Quart Subscription Support
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix dependency on optional arg
    
    * fix keep alive messages interfered with tests
    
    * reuse asgi test client
    
    * fix ws close reasons were not available
    
    * fix imprecise exception handling
    
    * remove slightly unnecesary convenience method
    
    * remove unnecessary check
    
    * fix default param was mutable
    
    * fix missing type annotations
    
    * fix protocol violation could not be checked
    
    * fix context had no debug info
    
    * fix missing mixin
    
    * remove unnecessary diff
    
    * format
    
    * keep request handling aligned with other integrations
    
    * remove apparently unneded checks
    
    * format
    
    * fix types
    
    * add docs
    
    * add release file
    
    * fix typo
    
    * fix small (unrelated) typo
    
    * fix typo
    
    ---------
    
    Co-authored-by: Paul Dubs <paul.dubs@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- strawberry/quart/views.py
+++ strawberry/quart/views.py
@@ -1,15 +1,29 @@
+import asyncio
 import warnings
-from collections.abc import AsyncGenerator, Mapping
-from typing import TYPE_CHECKING, Callable, ClassVar, Optional, cast
+from collections.abc import AsyncGenerator, Mapping, Sequence
+from datetime import timedelta
+from json.decoder import JSONDecodeError
+from typing import TYPE_CHECKING, Callable, ClassVar, Optional, Union, cast
 from typing_extensions import TypeGuard
 
-from quart import Request, Response, request
+from quart import Request, Response, Websocket, request, websocket
+from quart.ctx import has_websocket_context
 from quart.views import View
-from strawberry.http.async_base_view import AsyncBaseHTTPView, AsyncHTTPRequestAdapter
-from strawberry.http.exceptions import HTTPException
+from strawberry.http.async_base_view import (
+    AsyncBaseHTTPView,
+    AsyncHTTPRequestAdapter,
+    AsyncWebSocketAdapter,
+)
+from strawberry.http.exceptions import (
+    HTTPException,
+    NonJsonMessageReceived,
+    NonTextMessageReceived,
+    WebSocketDisconnected,
+)
 from strawberry.http.ides import GraphQL_IDE
 from strawberry.http.types import FormData, HTTPMethod, QueryParams
 from strawberry.http.typevars import Context, RootValue
+from strawberry.subscriptions import GRAPHQL_TRANSPORT_WS_PROTOCOL, GRAPHQL_WS_PROTOCOL
 
 if TYPE_CHECKING:
     from quart.typing import ResponseReturnValue
@@ -46,15 +60,55 @@ class QuartHTTPRequestAdapter(AsyncHTTPRequestAdapter):
         return FormData(files=files, form=form)
 
 
+class QuartWebSocketAdapter(AsyncWebSocketAdapter):
+    def __init__(
+        self, view: AsyncBaseHTTPView, request: Websocket, response: Response
+    ) -> None:
+        super().__init__(view)
+        self.ws = request
+
+    async def iter_json(
+        self, *, ignore_parsing_errors: bool = False
+    ) -> AsyncGenerator[object, None]:
+        try:
+            while True:
+                # Raises asyncio.CancelledError when the connection is closed.
+                # https://quart.palletsprojects.com/en/latest/how_to_guides/websockets.html#detecting-disconnection
+                message = await self.ws.receive()
+
+                if not isinstance(message, str):
+                    raise NonTextMessageReceived
+
+                try:
+                    yield self.view.decode_json(message)
+                except JSONDecodeError as e:
+                    if not ignore_parsing_errors:
+                        raise NonJsonMessageReceived from e
+        except asyncio.CancelledError:
+            pass
+
+    async def send_json(self, message: Mapping[str, object]) -> None:
+        try:
+            # Raises asyncio.CancelledError when the connection is closed.
+            # https://quart.palletsprojects.com/en/latest/how_to_guides/websockets.html#detecting-disconnection
+            await self.ws.send(self.view.encode_json(message))
+        except asyncio.CancelledError as exc:
+            raise WebSocketDisconnected from exc
+
+    async def close(self, code: int, reason: str) -> None:
+        await self.ws.close(code, reason=reason)
+
+
 class GraphQLView(
     AsyncBaseHTTPView[
-        Request, Response, Response, Request, Response, Context, RootValue
+        Request, Response, Response, Websocket, Response, Context, RootValue
     ],
     View,
 ):
     methods: ClassVar[list[str]] = ["GET", "POST"]
     allow_queries_via_get: bool = True
     request_adapter_class = QuartHTTPRequestAdapter
+    websocket_adapter_class = QuartWebSocketAdapter
 
     def __init__(
         self,
@@ -62,10 +116,23 @@ class GraphQLView(
         graphiql: Optional[bool] = None,
         graphql_ide: Optional[GraphQL_IDE] = "graphiql",
         allow_queries_via_get: bool = True,
+        keep_alive: bool = True,
+        keep_alive_interval: float = 1,
+        debug: bool = False,
+        subscription_protocols: Sequence[str] = (
+            GRAPHQL_TRANSPORT_WS_PROTOCOL,
+            GRAPHQL_WS_PROTOCOL,
+        ),
+        connection_init_wait_timeout: timedelta = timedelta(minutes=1),
         multipart_uploads_enabled: bool = False,
     ) -> None:
         self.schema = schema
         self.allow_queries_via_get = allow_queries_via_get
+        self.keep_alive = keep_alive
+        self.keep_alive_interval = keep_alive_interval
+        self.debug = debug
+        self.subscription_protocols = subscription_protocols
+        self.connection_init_wait_timeout = connection_init_wait_timeout
         self.multipart_uploads_enabled = multipart_uploads_enabled
 
         if graphiql is not None:
@@ -88,18 +155,24 @@ class GraphQLView(
 
         return sub_response
 
-    async def get_context(self, request: Request, response: Response) -> Context:
+    async def get_context(
+        self, request: Union[Request, Websocket], response: Response
+    ) -> Context:
         return {"request": request, "response": response}  # type: ignore
 
-    async def get_root_value(self, request: Request) -> Optional[RootValue]:
+    async def get_root_value(
+        self, request: Union[Request, Websocket]
+    ) -> Optional[RootValue]:
         return None
 
     async def get_sub_response(self, request: Request) -> Response:
         return Response(status=200, content_type="application/json")
 
-    async def dispatch_request(self) -> "ResponseReturnValue":  # type: ignore
+    async def dispatch_request(self, **kwargs: object) -> "ResponseReturnValue":
         try:
-            return await self.run(request=request)
+            return await self.run(
+                request=websocket if has_websocket_context() else request
+            )
         except HTTPException as e:
             return Response(
                 response=e.reason,
@@ -122,16 +195,22 @@ class GraphQLView(
             },
         )
 
-    def is_websocket_request(self, request: Request) -> TypeGuard[Request]:
-        return False
+    def is_websocket_request(
+        self, request: Union[Request, Websocket]
+    ) -> TypeGuard[Websocket]:
+        return has_websocket_context()
 
-    async def pick_websocket_subprotocol(self, request: Request) -> Optional[str]:
-        raise NotImplementedError
+    async def pick_websocket_subprotocol(self, request: Websocket) -> Optional[str]:
+        protocols = request.requested_subprotocols
+        intersection = set(protocols) & set(self.subscription_protocols)
+        sorted_intersection = sorted(intersection, key=protocols.index)
+        return next(iter(sorted_intersection), None)
 
     async def create_websocket_response(
-        self, request: Request, subprotocol: Optional[str]
+        self, request: Websocket, subprotocol: Optional[str]
     ) -> Response:
-        raise NotImplementedError
+        await request.accept(subprotocol=subprotocol)
+        return Response()
 
 
 __all__ = ["GraphQLView"]
--- tests/http/clients/quart.py
+++ tests/http/clients/quart.py
@@ -1,12 +1,18 @@
+import contextlib
 import json
 import urllib.parse
+from collections.abc import AsyncGenerator
 from io import BytesIO
-from typing import Any, Optional
+from typing import Any, Optional, Union
 from typing_extensions import Literal
+
+from starlette.testclient import TestClient
+from starlette.types import Receive, Scope, Send
 
 from quart import Quart
 from quart import Request as QuartRequest
 from quart import Response as QuartResponse
+from quart import Websocket as QuartWebsocket
 from quart.datastructures import FileStorage
 from strawberry.http import GraphQLHTTPResponse
 from strawberry.http.ides import GraphQL_IDE
@@ -14,25 +20,38 @@ from strawberry.quart.views import GraphQLView as BaseGraphQLView
 from strawberry.types import ExecutionResult
 from tests.http.context import get_context
 from tests.views.schema import Query, schema
+from tests.websockets.views import OnWSConnectMixin
 
-from .base import JSON, HttpClient, Response, ResultOverrideFunction
+from .asgi import AsgiWebSocketClient
+from .base import (
+    JSON,
+    DebuggableGraphQLTransportWSHandler,
+    DebuggableGraphQLWSHandler,
+    HttpClient,
+    Response,
+    ResultOverrideFunction,
+    WebSocketClient,
+)
 
 
-class GraphQLView(BaseGraphQLView[dict[str, object], object]):
+class GraphQLView(OnWSConnectMixin, BaseGraphQLView[dict[str, object], object]):
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"]
-
     result_override: ResultOverrideFunction = None
+    graphql_transport_ws_handler_class = DebuggableGraphQLTransportWSHandler
+    graphql_ws_handler_class = DebuggableGraphQLWSHandler
 
     def __init__(self, *args: Any, **kwargs: Any):
-        self.result_override = kwargs.pop("result_override")
+        self.result_override = kwargs.pop("result_override", None)
         super().__init__(*args, **kwargs)
 
-    async def get_root_value(self, request: QuartRequest) -> Query:
+    async def get_root_value(
+        self, request: Union[QuartRequest, QuartWebsocket]
+    ) -> Query:
         await super().get_root_value(request)  # for coverage
         return Query()
 
     async def get_context(
-        self, request: QuartRequest, response: QuartResponse
+        self, request: Union[QuartRequest, QuartWebsocket], response: QuartResponse
     ) -> dict[str, object]:
         context = await super().get_context(request, response)
 
@@ -45,6 +64,21 @@ class GraphQLView(BaseGraphQLView[dict[str, object], object]):
             return self.result_override(result)
 
         return await super().process_result(request, result)
+
+
+class QuartAsgiAppAdapter:
+    def __init__(self, app: Quart):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        scope["asgi"] = scope.get("asgi", {})
+
+        # Our WebSocket tests depend on WebSocket close reasons.
+        # Quart only sends close reason if the ASGI spec version in the scope is => 2.3
+        # https://github.com/pallets/quart/blob/b5593ca4c8c657564cdf2d35c9f0298fce63636b/src/quart/asgi.py#L347-L348
+        scope["asgi"]["spec_version"] = "2.3"
+
+        await self.app(scope, receive, send)  # type: ignore
 
 
 class QuartHttpClient(HttpClient):
@@ -66,6 +100,7 @@ class QuartHttpClient(HttpClient):
             graphql_ide=graphql_ide,
             allow_queries_via_get=allow_queries_via_get,
             result_override=result_override,
+            keep_alive=False,
             multipart_uploads_enabled=multipart_uploads_enabled,
         )
 
@@ -73,6 +108,34 @@ class QuartHttpClient(HttpClient):
             "/graphql",
             view_func=view,
         )
+
+        self.app.add_url_rule(
+            "/graphql",
+            view_func=view,
+            methods=["GET"],
+            websocket=True,
+        )
+
+        self.client = TestClient(QuartAsgiAppAdapter(self.app))
+
+    def create_app(self, **kwargs: Any) -> None:
+        self.app = Quart(__name__)
+        self.app.debug = True
+
+        view = GraphQLView.as_view("graphql_view", schema=schema, **kwargs)
+
+        self.app.add_url_rule(
+            "/graphql",
+            view_func=view,
+        )
+        self.app.add_url_rule(
+            "/graphql",
+            view_func=view,
+            methods=["GET"],
+            websocket=True,
+        )
+
+        self.client = TestClient(QuartAsgiAppAdapter(self.app))
 
     async def _graphql_request(
         self,
@@ -145,3 +208,13 @@ class QuartHttpClient(HttpClient):
         return await self.request(
             url, "post", **{k: v for k, v in kwargs.items() if v is not None}
         )
+
+    @contextlib.asynccontextmanager
+    async def ws_connect(
+        self,
+        url: str,
+        *,
+        protocols: list[str],
+    ) -> AsyncGenerator[WebSocketClient, None]:
+        with self.client.websocket_connect(url, protocols) as ws:
+            yield AsgiWebSocketClient(ws)
