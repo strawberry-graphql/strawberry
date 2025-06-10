3d948dedd9196d0072e02aa0d309f1317d9ff033
Jonathan Ehwald <github@ehwald.info>
2025-06-10 14:06:19 +0200

    Fix spaces got replaced with tabs

--- tests/utils/test_get_first_operation.py
+++ tests/utils/test_get_first_operation.py
@@ -6,10 +6,10 @@ from strawberry.utils.operation import get_first_operation
 def test_document_without_operation_definition_nodes():
     document = parse(
         """
-		fragment Test on Query {
-			hello
-		}
-		"""
+        fragment Test on Query {
+            hello
+        }
+        """
     )
     assert get_first_operation(document) is None
 
@@ -17,10 +17,10 @@ def test_document_without_operation_definition_nodes():
 def test_single_operation_definition_node():
     document = parse(
         """
-		query Operation1 {
-			hello
-		}
-		"""
+        query Operation1 {
+            hello
+        }
+        """
     )
     node = get_first_operation(document)
     assert node is not None
@@ -30,13 +30,13 @@ def test_single_operation_definition_node():
 def test_multiple_operation_definition_nodes():
     document = parse(
         """
-		mutation Operation1 {
-			hello
-		}
-		query Operation2 {
-			hello
-		}
-		"""
+        mutation Operation1 {
+            hello
+        }
+        query Operation2 {
+            hello
+        }
+        """
     )
     node = get_first_operation(document)
     assert node is not None
