0b68daf387270c9a338c65056e8eba2197d938f0
Jonathan Ehwald <github@ehwald.info>
2025-06-10 14:41:59 +0200

    Fix assertions, typos, and formatting (#3894)
    * Fix assertions, typos, and formatting
    
    * Fix spaces got replaced with tabs

--- tests/utils/test_get_first_operation.py
+++ tests/utils/test_get_first_operation.py
@@ -3,30 +3,31 @@ from graphql import OperationType, parse
 from strawberry.utils.operation import get_first_operation
 
 
-def test_document_without_operation_definition_notes():
+def test_document_without_operation_definition_nodes():
     document = parse(
         """
         fragment Test on Query {
             hello
         }
-    """
+        """
     )
     assert get_first_operation(document) is None
 
 
-def test_single_operation_definition_note():
+def test_single_operation_definition_node():
     document = parse(
         """
         query Operation1 {
             hello
         }
-    """
+        """
     )
-    assert get_first_operation(document) is not None
-    assert get_first_operation(document).operation == OperationType.QUERY
+    node = get_first_operation(document)
+    assert node is not None
+    assert node.operation == OperationType.QUERY
 
 
-def test_multiple_operation_definition_notes():
+def test_multiple_operation_definition_nodes():
     document = parse(
         """
         mutation Operation1 {
@@ -35,7 +36,8 @@ def test_multiple_operation_definition_notes():
         query Operation2 {
             hello
         }
-    """
+        """
     )
-    assert get_first_operation(document) is not None
-    assert get_first_operation(document).operation == OperationType.MUTATION
+    node = get_first_operation(document)
+    assert node is not None
+    assert node.operation == OperationType.MUTATION
