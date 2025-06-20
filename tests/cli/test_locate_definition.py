72c2361cbcc58985f340d79b910e994d20e16e87
Sam Millar <sam@millar.io>
2025-06-20 10:13:09 +0100

    add test

--- tests/cli/test_locate_definition.py
+++ tests/cli/test_locate_definition.py
@@ -2,7 +2,7 @@ from typer import Typer
 from typer.testing import CliRunner
 
 
-def test_find_model_name(mocker, cli_app: Typer, cli_runner: CliRunner):
+def test_find_model_name(cli_app: Typer, cli_runner: CliRunner):
     selector = "tests.fixtures.sample_package.sample_module:schema"
     result = cli_runner.invoke(cli_app, ["locate-definition", selector, "User"])
 
@@ -12,7 +12,7 @@ def test_find_model_name(mocker, cli_app: Typer, cli_runner: CliRunner):
     )
 
 
-def test_find_model_field(mocker, cli_app: Typer, cli_runner: CliRunner):
+def test_find_model_field(cli_app: Typer, cli_runner: CliRunner):
     selector = "tests.fixtures.sample_package.sample_module:schema"
     result = cli_runner.invoke(cli_app, ["locate-definition", selector, "User.name"])
 
@@ -22,7 +22,7 @@ def test_find_model_field(mocker, cli_app: Typer, cli_runner: CliRunner):
     )
 
 
-def test_find_missing_model(mocker, cli_app: Typer, cli_runner: CliRunner):
+def test_find_missing_model(cli_app: Typer, cli_runner: CliRunner):
     selector = "tests.fixtures.sample_package.sample_module:schema"
     result = cli_runner.invoke(cli_app, ["locate-definition", selector, "Missing"])
 
@@ -30,7 +30,7 @@ def test_find_missing_model(mocker, cli_app: Typer, cli_runner: CliRunner):
     assert result.stdout.strip() == "Definition not found: Missing"
 
 
-def test_find_missing_model_field(mocker, cli_app: Typer, cli_runner: CliRunner):
+def test_find_missing_model_field(cli_app: Typer, cli_runner: CliRunner):
     selector = "tests.fixtures.sample_package.sample_module:schema"
     result = cli_runner.invoke(
         cli_app, ["locate-definition", selector, "Missing.field"]
@@ -38,3 +38,10 @@ def test_find_missing_model_field(mocker, cli_app: Typer, cli_runner: CliRunner)
 
     assert result.exit_code == 1
     assert result.stdout.strip() == "Definition not found: Missing.field"
+
+
+def test_find_missing_schema(cli_app: Typer, cli_runner: CliRunner):
+    selector = "tests.fixtures.sample_package.sample_module:missing"
+    result = cli_runner.invoke(cli_app, ["locate-definition", selector, "User"])
+
+    assert result.exit_code == 2
