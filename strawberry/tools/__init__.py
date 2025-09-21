168cb62e35bfffe0b71006c420e99e7e525a8132
Muzaffer Cıkay <muzaffercky@gmail.com>
2025-09-21 13:03:38 +0300

    Define PartialType metaclass

--- strawberry/tools/__init__.py
+++ strawberry/tools/__init__.py
@@ -1,7 +1,10 @@
 from .create_type import create_type
 from .merge_types import merge_types
+from .partialtype import PartialType
+
 
 __all__ = [
     "create_type",
     "merge_types",
+    "PartialType",
 ]
