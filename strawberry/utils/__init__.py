c0cabe73e9d1b268b2c13546669d8d63c277840a
Thiago Bellini Ribeiro <thiago@bellini.dev>
2026-02-24 12:31:25 +0100

    fix: prevent async iterable detection for sync execution in gql-core 3.3

--- strawberry/utils/__init__.py
+++ strawberry/utils/__init__.py
@@ -2,3 +2,9 @@ from graphql.version import VersionInfo, version_info
 
 IS_GQL_33 = version_info >= VersionInfo.from_str("3.3.0a0")
 IS_GQL_32 = not IS_GQL_33
+
+if IS_GQL_33 and version_info < VersionInfo.from_str("3.3.0a12"):
+    raise ImportError(
+        f"graphql-core {version_info} is not supported. "
+        "Please upgrade to graphql-core >= 3.3.0a12."
+    )
