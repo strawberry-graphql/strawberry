941de58fb12ba8b15937539175a8ccb9ab188e2c
Patrick Arminio <patrick.arminio@gmail.com>
2026-07-22 23:12:18 +0100

    Fix MaskErrors result typing

--- strawberry/extensions/mask_errors.py
+++ strawberry/extensions/mask_errors.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Iterator
-from typing import Any
+from typing import Protocol, runtime_checkable
 
 from graphql import ExecutionResult as GraphQLExecutionResult
 from graphql.error import GraphQLError
@@ -9,6 +9,11 @@ from strawberry.types.execution import (
     ExecutionResult as StrawberryExecutionResult,
 )
 from strawberry.types.execution import StreamExecutionResult
+
+
+@runtime_checkable
+class _ResultWithErrors(Protocol):
+    errors: list[GraphQLError] | None
 
 
 def default_should_mask_error(_: GraphQLError) -> bool:
@@ -50,11 +55,9 @@ class MaskErrors(SchemaExtension):
 
         return processed_errors
 
-    # TODO: proper typing
-    def _process_result(self, result: Any) -> None:
-        errors = getattr(result, "errors", None)
-        if errors:
-            result.errors = self._process_errors(errors)
+    def _process_result(self, result: object) -> None:
+        if isinstance(result, _ResultWithErrors) and result.errors:
+            result.errors = self._process_errors(result.errors)
 
     def _process_stream_result(self, result: StreamExecutionResult) -> None:
         self._process_result(result)
