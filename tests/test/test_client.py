1786f3b6045aa721c1c0ee5d4709ed8efed9ba29
Egor Poderiagin <egor@crazyrussian.pro>
2026-08-29 23:54:22 +0700

    tests: fix tests via unification

--- tests/test/test_client.py
+++ tests/test/test_client.py
@@ -6,9 +6,9 @@ import pytest
 
 from strawberry.utils.await_maybe import await_maybe
 
-query_to_non_existent_field = "{ nonExistentField { id } }"
+QUERY_TO_NON_EXISTENT_FIELD = "{ nonExistentField { id } }"
 
-NON_EXISTENT_FIELD_ERROR_MESSAGES = [
+NON_EXISTENT_FIELD_ERRORS = [
     {
         "message": "Cannot query field 'nonExistentField' on type 'Query'.",
         "locations": [{"line": 1, "column": 3}],
@@ -31,58 +31,8 @@ async def test_query_with_assert_no_errors_option(
         )
 
 
-@pytest.mark.asgi
-def test_asgi_client_assert_no_errors_verbose_message():
-    from starlette.testclient import TestClient
-
-    from strawberry.asgi import GraphQL
-    from strawberry.asgi.test import GraphQLTestClient
-    from tests.views.schema import schema
-
-    client = GraphQLTestClient(TestClient(GraphQL(schema)))
-
+async def test_assert_no_errors_includes_response_errors_in_message(graphql_client):
     with pytest.raises(AssertionError) as exc_info:
-        client.query(query_to_non_existent_field)
+        await await_maybe(graphql_client.query(QUERY_TO_NON_EXISTENT_FIELD))
 
-    assert exc_info.value.args[0] == NON_EXISTENT_FIELD_ERROR_MESSAGES
-
-
-@pytest.mark.django
-def test_django_client_assert_no_errors_verbose_message():
-    from django.test.client import Client
-
-    from strawberry.django.test import GraphQLTestClient
-
-    client = GraphQLTestClient(Client())
-
-    with pytest.raises(AssertionError) as exc_info:
-        client.query(query_to_non_existent_field)
-
-    assert exc_info.value.args[0] == NON_EXISTENT_FIELD_ERROR_MESSAGES
-
-
-@pytest.mark.aiohttp
-async def test_aiohttp_client_assert_no_errors_verbose_message():
-    try:
-        from aiohttp import web
-        from aiohttp.test_utils import TestClient as AiohttpTestClient
-        from aiohttp.test_utils import TestServer
-
-        from strawberry.aiohttp.test import GraphQLTestClient
-        from strawberry.aiohttp.views import GraphQLView
-    except ImportError:
-        pytest.skip("Aiohttp not installed")
-
-    from tests.views.schema import schema
-
-    view = GraphQLView(schema=schema)
-    app = web.Application()
-    app.router.add_route("*", "/graphql/", view)
-
-    async with AiohttpTestClient(TestServer(app)) as client:
-        graphql_client = GraphQLTestClient(client)
-
-        with pytest.raises(AssertionError) as exc_info:
-            await graphql_client.query(query_to_non_existent_field)
-
-        assert exc_info.value.args[0] == NON_EXISTENT_FIELD_ERROR_MESSAGES
+    assert exc_info.value.args[0] == NON_EXISTENT_FIELD_ERRORS
